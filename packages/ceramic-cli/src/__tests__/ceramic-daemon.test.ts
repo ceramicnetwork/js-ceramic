ddc2aea9b50df05270b42613610109a0023acf94
Joel Torstensson <oed3933@gmail.com>
2020-04-13 09:16:18 +0200

    chore(cli): add interop tests between http-client and daemon

--- packages/ceramic-cli/src/__tests__/ceramic-daemon.test.ts
+++ packages/ceramic-cli/src/__tests__/ceramic-daemon.test.ts
@@ -1,5 +1,110 @@
+import Ceramic from '@ceramicnetwork/ceramic-core'
+import CeramicClient from '@ceramicnetwork/ceramic-http-client'
+import IdentityWallet from 'identity-wallet'
+import tmp from 'tmp-promise'
+import Ipfs from 'ipfs'
+import CeramicDaemon from '../ceramic-daemon'
 
+const seed = '0x5872d6e0ae7347b72c9216db218ebbb9d9d0ae7ab818ead3557e8e78bf944184'
+const genIpfsConf = (path, id): any => {
+  return {
+    repo: `${path}/ipfs${id}/`,
+    config: {
+      Addresses: { Swarm: [] },
+      Discovery: {
+        MDNS: { Enabled: false },
+        webRTCStar: { Enabled: false }
+      },
+      Bootstrap: []
+    },
+  }
+}
+const anchorUpdate = (doc): Promise<void> => new Promise(resolve => doc.on('change', resolve))
+const port = 7777
+const apiUrl = 'http://localhost:' + port
 
-it.skip('null test', async () => {
-  return
+describe('Ceramic interop: core <> http-client', () => {
+  jest.setTimeout(7000)
+  let ipfs
+  let tmpFolder, idWallet
+  let core, daemon, client
+
+  const DOCTYPE_TILE = 'tile'
+  const DOCTYPE_3ID = '3id'
+
+  beforeAll(async () => {
+    idWallet = new IdentityWallet(() => true, { seed })
+    tmpFolder = await tmp.dir({ unsafeCleanup: true })
+    ipfs = await Ipfs.create(genIpfsConf(tmpFolder.path, 0))
+  })
+
+  afterAll(async () => {
+    await ipfs.stop()
+    await tmpFolder.cleanup()
+  })
+
+  beforeEach(async () => {
+    core = await Ceramic.create(ipfs)
+    await core.setDIDProvider(idWallet.get3idProvider())
+    daemon = new CeramicDaemon(core, { port })
+    client = new CeramicClient(apiUrl)
+  })
+
+  afterEach(async () => {
+    await client.close()
+    await daemon.close()
+    await core.close()
+  })
+
+  it('properly creates document', async () => {
+    const doc1 = await core.createDocument({ test: 123 }, DOCTYPE_TILE, { onlyGenesis: true, skipWait: true })
+    const doc2 = await client.createDocument({ test: 123 }, DOCTYPE_TILE, { onlyGenesis: true, skipWait: true })
+    expect(doc1.content).toEqual(doc2.content)
+    expect(doc1.state).toEqual(doc2.state)
+  })
+
+  it('gets anchor record updates', async () => {
+    // Right now there is no pinning in ceramic-core which means that the anchor record
+    // will not be remembered when we create the same document from the client and we
+    // thus get a different anchor record.
+    const doc1 = await core.createDocument({ test: 123 }, DOCTYPE_TILE)
+    await anchorUpdate(doc1)
+    expect(doc1.state.log.length).toEqual(2)
+    expect(doc1.state.anchored).toBeGreaterThan(0)
+    const doc2 = await client.createDocument({ test: 123 }, DOCTYPE_TILE)
+    await anchorUpdate(doc2)
+    expect(doc2.state.log.length).toEqual(2)
+    expect(doc2.state.anchored).toBeGreaterThan(0)
+  })
+
+  it('loads documents correctly', async () => {
+    const doc1 = await core.createDocument({ test: 123 }, DOCTYPE_TILE)
+    await anchorUpdate(doc1)
+    const doc2 = await client.loadDocument(doc1.id)
+    expect(doc1.content).toEqual(doc2.content)
+    expect(doc1.state).toEqual(doc2.state)
+
+    const doc3 = await client.createDocument({ test: 456 }, DOCTYPE_TILE)
+    await anchorUpdate(doc3)
+    const doc4 = await core.loadDocument(doc3.id)
+    expect(doc3.content).toEqual(doc4.content)
+    expect(doc3.state).toEqual(doc4.state)
+  })
+
+  it('makes and gets updates correctly', async () => {
+    const doc1 = await core.createDocument({ test: 123 }, DOCTYPE_TILE)
+    await anchorUpdate(doc1)
+    const doc2 = await client.loadDocument(doc1.id)
+    // change from core viewable in client
+    await doc1.change({test: 123, abc: 987})
+    await anchorUpdate(doc1)
+    await anchorUpdate(doc2)
+    expect(doc1.content).toEqual(doc2.content)
+    expect(doc1.state).toEqual(doc2.state)
+    // change from client viewable in core
+    await doc2.change({test: 456, abc: 654})
+    await anchorUpdate(doc2)
+    expect(doc1.content).toEqual(doc2.content)
+    expect(doc1.state).toEqual(doc2.state)
+  })
 })
--- packages/ceramic-cli/src/ceramic-daemon.ts
+++ packages/ceramic-cli/src/ceramic-daemon.ts
@@ -1,3 +1,4 @@
+import type Ipfs from 'ipfs'
 import ipfsClient from 'ipfs-http-client'
 import express, { Request, Response, NextFunction } from 'express'
 import IdentityWallet from 'identity-wallet'
@@ -5,37 +6,45 @@ import Ceramic from '@ceramicnetwork/ceramic-core'
 import { serializeState } from './utils'
 
 const IPFS_HOST = 'http://localhost:5001'
+const DEFAULT_PORT = 7007
 const toApiPath = (ending: string): string => '/api/v0' + ending
 // TODO - don't hardcode seed lol
 const seed = '0x5872d6e0ae7347b72c9216db218ebbb9d9d0ae7ab818ead3557e8e78bf944184'
 
-class CeramicDaemon {
-  private app: any
+interface CreateOpts {
+  ipfsHost?: string;
+  ipfs?: Ipfs.Ipfs;
+  port?: number;
+}
 
-  constructor (private ceramic: Ceramic) {
-    this.app = express()
-    this.app.use(express.json())
-    this.app.use((req: Request, res: Response, next: NextFunction) => {
+class CeramicDaemon {
+  private server: any
+
+  constructor (private ceramic: Ceramic, opts: CreateOpts) {
+    const app = express()
+    app.use(express.json())
+    app.use((req: Request, res: Response, next: NextFunction) => {
       res.header('Access-Control-Allow-Origin', '*')
       next()
     })
-    this.app.post(toApiPath('/create'), this.createDoc.bind(this))
-    this.app.get(toApiPath('/show/ceramic/:cid'), this.show.bind(this))
-    this.app.get(toApiPath('/state/ceramic/:cid'), this.state.bind(this))
-    this.app.post(toApiPath('/change/ceramic/:cid'), this.change.bind(this))
-    const server = this.app.listen(7007, () => {
-      console.log('Ceramic API running on port 7007')
+    app.post(toApiPath('/create'), this.createDoc.bind(this))
+    app.get(toApiPath('/show/ceramic/:cid'), this.show.bind(this))
+    app.get(toApiPath('/state/ceramic/:cid'), this.state.bind(this))
+    app.post(toApiPath('/change/ceramic/:cid'), this.change.bind(this))
+    const port = opts.port || DEFAULT_PORT
+    this.server = app.listen(port, () => {
+      console.log('Ceramic API running on port ' + port)
       console.log('User DID:', ceramic.user.DID)
     })
-    server.keepAliveTimeout = 60 * 1000
+    this.server.keepAliveTimeout = 60 * 1000
   }
 
-  static async create (ipfsHost: string = IPFS_HOST): Promise<CeramicDaemon> {
-    const ipfs = ipfsClient(ipfsHost)
+  static async create (opts: CreateOpts): Promise<CeramicDaemon> {
+    const ipfs = opts.ipfs || ipfsClient(opts.ipfsHost || IPFS_HOST)
     const ceramic = await Ceramic.create(ipfs)
     const idWallet = new IdentityWallet(async () => true, { seed })
     await ceramic.setDIDProvider(idWallet.get3idProvider())
-    return new CeramicDaemon(ceramic)
+    return new CeramicDaemon(ceramic, opts)
   }
 
   async createDoc (req: Request, res: Response, next: NextFunction): Promise<void> {
@@ -84,11 +93,15 @@ class CeramicDaemon {
     try {
       const doc = await this.ceramic.loadDocument(docId)
       await doc.change(content)
-      res.json({ docId: doc.id, state: doc.state })
+      res.json({ docId: doc.id, state: serializeState(doc.state) })
     } catch (e) {
       res.json({ error: e.message })
     }
     next()
+  }
+
+  async close () {
+    return this.server.close()
   }
 }
 
