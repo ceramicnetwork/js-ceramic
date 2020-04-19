debdb2a8d1e545248402f4c5c287ef8f27536d6b
Janko Simonovic <janko.simonovic@sbgenomics.com>
2020-04-19 11:55:00 +0200

    serialize document state

--- packages/ceramic-cli/src/utils.ts
+++ packages/ceramic-cli/src/utils.ts
@@ -1,6 +1,13 @@
+import { AnchorStatus } from "@ceramicnetwork/ceramic-http-client/lib/document";
 
 export function serializeState (state: any): any {
   state.log = state.log.map((cid: any) => cid.toString());
+  if (state.anchorStatus) {
+    state.anchorStatus = AnchorStatus[state.anchorStatus];
+  }
+  if (state.anchorScheduledFor) {
+    state = new Date(state.anchorScheduledFor).toISOString(); // ISO format of the UTC time
+  }
   if (state.anchorProof) {
     state.anchorProof.txHash = state.anchorProof.txHash.toString();
     state.anchorProof.root = state.anchorProof.root.toString();
