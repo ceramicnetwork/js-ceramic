fbe85a1877b86f60465d4977438daef2b6eb93c6
Michael Sterle-Contala <2691351+msterle@users.noreply.github.com>
2020-05-07 14:59:46 +0200

    ceramic-account base

---
+++ packages/ceramic-account/src/index.ts
@@ -0,0 +1,130 @@
+import type { CeramicApi, CeramicDocument } from './types'
+
+import DIDDocument from './did'
+import AccountLinks from './accountLinks'
+
+class ThreeIDAccount {
+  [key: string]: any
+
+  constructor (public ceramicDoc: CeramicDocument, public didDocument: DIDDocument, public accountLinks: AccountLinks, public keychain: Keychain, public profile: Profile, public sources: Sources, public services: Services, private _ceramic: CeramicApi, public provider?: any) { }
+
+  get DID (): string {
+    return this.didDocument.did
+  }
+
+  static async build (did: string, ceramic: CeramicApi, provider?: any): Promise<ThreeIDAccount> {
+    const didDocument = await DIDDocument.get(did, ceramic)
+    
+    // The incremental building of an account tile here is a good example of how creating a buffered
+    // document type would reduce number of writes, speeding up creation, but more importantly, 
+    // all future fetches due to less DAG depth
+
+    let ceramicDoc
+    if (didDocument.ceramicDoc.content.account) {
+      ceramicDoc = await ceramic.loadDocument(didDocument.ceramicDoc.content.account)
+    } else {
+      const genesisContent: Record<string, string> = {}
+      ceramicDoc = await ceramic.createDocument(genesisContent, 'tile', { owners: [didDocument.did] })
+      const prevContent = didDocument.ceramicDoc.content
+      await didDocument.ceramicDoc.change({...prevContent, sources: ceramicDoc.id})
+    }
+
+    // Load/create subtiles
+
+    let accountLinks: AccountLinks
+    if (ceramicDoc.content['account-links']) {
+      const subtileDocument = await ceramic.loadDocument(ceramicDoc.content['account-links'])
+      accountLinks = new AccountLinks(subtileDocument, ceramic, provider)
+    } else {
+      accountLinks = await AccountLinks.build(did, ceramic, provider)
+      ceramicDoc.change({ ...ceramicDoc.content, 'account-links': accountLinks.ceramicDoc.id })
+    }
+
+    let keychain: Keychain
+    if (ceramicDoc.content.keychain) {
+      const subtileDocument = await ceramic.loadDocument(ceramicDoc.content.keychain)
+      keychain = new Keychain(subtileDocument, ceramic)
+    } else {
+      keychain = await Keychain.build(did, ceramic)
+      ceramicDoc.change({ ...ceramicDoc.content, keychain: accountLinks.ceramicDoc.id })
+    }
+
+    let profile: Profile
+    if (ceramicDoc.content.profile) {
+      const subtileDocument = await ceramic.loadDocument(ceramicDoc.content.profile)
+      profile = new Profile(subtileDocument, ceramic)
+    } else {
+      profile = await Profile.build(did, ceramic)
+      ceramicDoc.change({ ...ceramicDoc.content, profile: accountLinks.ceramicDoc.id })
+    }
+
+    let sources: Sources
+    if (ceramicDoc.content.sources) {
+      const subtileDocument = await ceramic.loadDocument(ceramicDoc.content.sources)
+      sources = new Sources(subtileDocument, ceramic)
+    } else {
+      sources = await Sources.build(did, ceramic)
+      ceramicDoc.change({ ...ceramicDoc.content, sources: accountLinks.ceramicDoc.id })
+    }
+
+    let services: Services
+    if (ceramicDoc.content.services) {
+      const subtileDocument = await ceramic.loadDocument(ceramicDoc.content.services)
+      services = new Services(subtileDocument, ceramic)
+    } else {
+      services = await Services.build(did, ceramic)
+      ceramicDoc.change({ ...ceramicDoc.content, services: accountLinks.ceramicDoc.id })
+    }
+
+    return new ThreeIDAccount(ceramicDoc, didDocument, accountLinks, keychain, profile, sources, services, ceramic, provider)
+  }
+}
+
+// All of these could likely inherit from a AccountSubTile abstract parent class or a builder class
+
+class Keychain {
+  constructor (public ceramicDoc: CeramicDocument, private _ceramic: CeramicApi) { }
+
+  static async build(owner: string, ceramic: CeramicApi): Promise<Keychain> {
+    const genesisContent: Record<string, string[]> = {
+      "privacy-policy-read-keys": [],
+      "auth-public-keys": [],
+      "auth-data": [],
+      "legacy-data": []
+    }
+    const ceramicDoc = await ceramic.createDocument(genesisContent, 'tile', { owners: [owner] })
+    return new Keychain(ceramicDoc, ceramic)
+  }
+}
+
+class Profile {
+  constructor (public ceramicDoc: CeramicDocument, private _ceramic: CeramicApi) { }
+
+  static async build(owner: string, ceramic: CeramicApi): Promise<Profile> {
+    const genesisContent: Record<string, any> = {}
+    const ceramicDoc = await ceramic.createDocument(genesisContent, 'tile', { owners: [owner] })
+    return new Profile(ceramicDoc, ceramic)
+  }
+}
+
+class Sources {
+  constructor (public ceramicDoc: CeramicDocument, private _ceramic: CeramicApi) { }
+
+  static async build(owner: string, ceramic: CeramicApi): Promise<Sources> {
+    const genesisContent: string[] = []
+    const ceramicDoc = await ceramic.createDocument(genesisContent, 'tile', { owners: [owner] })
+    return new Sources(ceramicDoc, ceramic)
+  }
+}
+
+class Services {
+  constructor (public ceramicDoc: CeramicDocument, private _ceramic: CeramicApi) { }
+
+  static async build(owner: string, ceramic: CeramicApi): Promise<Services> {
+    const genesisContent: any[] = []
+    const ceramicDoc = await ceramic.createDocument(genesisContent, 'tile', { owners: [owner] })
+    return new Services(ceramicDoc, ceramic)
+  }
+}
+
+export default ThreeIDAccount
