c548680a174d6e03546ced69c38b8112257617f3
Michael Sterle-Contala <2691351+msterle@users.noreply.github.com>
2020-05-13 15:24:56 +0200

    Remove lib and coverage directories

--- packages/account-template/lib/types.d.ts
+++
@@ -1,6 +0,0 @@
-import type Ceramic from '@ceramicnetwork/ceramic-core';
-import type CeramicClient from '@ceramicnetwork/ceramic-http-client';
-import type CoreDocument from '@ceramicnetwork/ceramic-core/lib/document';
-import type ClientDocument from '@ceramicnetwork/ceramic-http-client/lib/document';
-export declare type CeramicApi = Ceramic | CeramicClient;
-export declare type CeramicDocument = CoreDocument | ClientDocument;
--- packages/account-template/lib/utils.d.ts
+++
@@ -1,4 +0,0 @@
-declare function safeSend(provider: any, data: Record<string, any>): Promise<any>;
-declare function encodeRpcCall(method: any, params?: any, fromAddress?: any): Record<string, any>;
-declare function callRpc(provider: any, method: any, params?: any, fromAddress?: any): Promise<any>;
-export { safeSend, encodeRpcCall, callRpc };
