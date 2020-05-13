d602e4be7f3f412034036245ce2eebff0111afe2
Michael Sterle-Contala <2691351+msterle@users.noreply.github.com>
2020-05-13 13:12:50 +0200

    remove test package

--- packages/test/src/@types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import type Ceramic from '@ceramicnetwork/ceramic-core'
-import type CeramicClient from '@ceramicnetwork/ceramic-http-client'
-
-declare global {
-  type CeramicApi = Ceramic | CeramicClient;
-}
