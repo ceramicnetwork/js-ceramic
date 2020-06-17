d0f1a0221cdbca00d21cabae1a528d69e08b9921
Janko Simonovic <simonovic86@gmail.com>
2020-06-17 11:16:39 +0200

    feat(core): add TODO

--- packages/ceramic-core/src/ceramic-api.ts
+++ packages/ceramic-core/src/ceramic-api.ts
@@ -24,6 +24,7 @@ export interface PinApi {
     ls(docId?: string): Promise<AsyncIterable<string>>;
 }
 
+// TODO handle errors as well
 export interface JsonRpc2Response {
     'id': string;
     'json-rpc': string;
