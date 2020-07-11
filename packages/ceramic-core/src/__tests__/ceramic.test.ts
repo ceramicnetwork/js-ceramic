4e391f44dc73bb1569826f53caf2c63bcb90c33b
Janko Simonovic <simonovic86@gmail.com>
2020-07-11 12:13:09 +0200

    fix(core): inc time

--- packages/ceramic-core/src/__tests__/ceramic.test.ts
+++ packages/ceramic-core/src/__tests__/ceramic.test.ts
@@ -18,7 +18,7 @@ const genIpfsConf = (path, id): any => {
 }
 
 describe('Ceramic integration', () => {
-  jest.setTimeout(30000)
+  jest.setTimeout(120000)
   let ipfs1: Ipfs;
   let ipfs2: Ipfs;
   let ipfs3: Ipfs;
