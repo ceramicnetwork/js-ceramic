02cc52246c6fca77215c47f0424d488e0dd476f7
Janko Simonovic <simonovic86@gmail.com>
2020-07-15 10:43:59 +0200

    fix(core): remove wait

--- packages/ceramic-core/src/__tests__/ceramic.test.ts
+++ packages/ceramic-core/src/__tests__/ceramic.test.ts
@@ -114,10 +114,6 @@ describe('Ceramic integration', () => {
     const ceramic3 = await Ceramic.create(ipfs3)
     // ceramic node 2 shouldn't need to have the document open in order to forward the message
     const doctype1 = await ceramic1.createDocument<ThreeIdDoctype>(DOCTYPE_3ID, { content: { test: 321 }, owners: [owner] })
-
-    // wait a bit to propagate
-    await new Promise(resolve => setTimeout(resolve, 1000))
-
     const doctype3 = await ceramic3.createDocument<ThreeIdDoctype>(DOCTYPE_3ID, { content: { test: 321 }, owners: [owner] }, { applyOnly: true })
 
     expect(doctype3.content).toEqual(doctype1.content)
