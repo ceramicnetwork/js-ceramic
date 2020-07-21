49ecda66ca781bcf1e2266cab1f669ca3a1508b6
Janko Simonovic <simonovic86@gmail.com>
2020-07-21 17:22:22 +0200

    fix(core): fix ceramic test

--- packages/ceramic-core/src/__tests__/ceramic.test.ts
+++ packages/ceramic-core/src/__tests__/ceramic.test.ts
@@ -116,8 +116,11 @@ describe('Ceramic integration', () => {
     // ceramic node 2 shouldn't need to have the document open in order to forward the message
     const doctype1 = await ceramic1.createDocument<ThreeIdDoctype>(DOCTYPE_3ID, { content: { test: 321 }, owners: [owner] })
 
-    // wait a bit to propagate
-    await new Promise(resolve => setTimeout(resolve, 1000))
+    await new Promise(resolve => {
+      doctype1.on('change', () => {
+        resolve()
+      })
+    })
 
     const doctype3 = await ceramic3.createDocument<ThreeIdDoctype>(DOCTYPE_3ID, { content: { test: 321 }, owners: [owner] }, { applyOnly: true })
 
