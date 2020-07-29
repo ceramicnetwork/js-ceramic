cbd0ec0ccb0cb936379227ecd914dc70362e19ed
Janko Simonovic <simonovic86@gmail.com>
2020-07-30 01:21:23 +0200

    feat(common): add validate to Doctype

---
+++ packages/ceramic-common/src/__tests__/doctype.test.ts
@@ -0,0 +1,66 @@
+import { mock } from 'jest-mock-extended'
+import { CeramicApi, DocState, Doctype } from ".."
+
+class BasicDoctype extends Doctype {
+    change(): Promise<void> {
+        throw new Error('Not implemented')
+    }
+}
+
+describe('Doctype', () => {
+    let ceramic: any
+    const schema = {
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "title": "StringMap",
+        "type": "object",
+        "additionalProperties": {
+            "type": "string"
+        }
+    }
+
+    beforeAll(() => {
+        const docSchemaState = mock<DocState>()
+        docSchemaState.content = schema
+        docSchemaState.metadata = {
+            schema: 'ceramic://1234567'
+        }
+
+        const schemaDoc = new BasicDoctype(docSchemaState, null)
+
+        ceramic = mock<CeramicApi>()
+        ceramic.loadDocument.mockReturnValue(new Promise<Doctype>((resolve) => {
+            resolve(schemaDoc);
+        }));
+    })
+
+    it('should pass schema validation', async () => {
+        const state = mock<DocState>()
+        state.metadata = {
+            schema: 'ceramic://1234567'
+        }
+        state.content = {
+            'x': 'y'
+        }
+
+        const doc = new BasicDoctype(state, { api: ceramic })
+        await doc.validate()
+    })
+
+    it('should fail schema validation', async () => {
+        const state = mock<DocState>()
+        state.metadata = {
+            schema: 'ceramic://1234567'
+        }
+        state.content = {
+            'x': 1
+        }
+
+        const doc = new BasicDoctype(state, { api: ceramic })
+        try {
+            await doc.validate()
+            throw new Error('Should not be able to validate invalid data')
+        } catch (e) {
+            expect(e.message).toEqual('Validation Error: data[\'x\'] should be string')
+        }
+    })
+})
