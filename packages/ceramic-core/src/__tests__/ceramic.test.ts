39665d5be9e1ae1896bbb636fee15fd4c9bb4618
Janko Simonovic <simonovic86@gmail.com>
2020-07-30 09:12:27 +0200

    fix(core): fix test

--- packages/ceramic-core/src/__tests__/ceramic.test.ts
+++ packages/ceramic-core/src/__tests__/ceramic.test.ts
@@ -3,6 +3,7 @@ import IdentityWallet from 'identity-wallet'
 import tmp from 'tmp-promise'
 import Ipfs from 'ipfs'
 import { ThreeIdDoctype } from "@ceramicnetwork/ceramic-doctype-three-id"
+import { AnchorStatus } from "@ceramicnetwork/ceramic-common"
 
 jest.mock('../store/level-state-store')
 
@@ -113,7 +114,12 @@ describe('Ceramic integration', () => {
     const ceramic2 = await Ceramic.create(ipfs2)
     const ceramic3 = await Ceramic.create(ipfs3)
     // ceramic node 2 shouldn't need to have the document open in order to forward the message
-    const doctype1 = await ceramic1.createDocument<ThreeIdDoctype>(DOCTYPE_3ID, { content: { test: 321 }, metadata: { owners: [owner] } })
+    const doctype1 = await ceramic1.createDocument<ThreeIdDoctype>(DOCTYPE_3ID, { content: { test: 321 }, owners: [owner] })
+    while (doctype1.state.anchorStatus !== AnchorStatus.ANCHORED) {
+      // wait to propagate
+      await new Promise(resolve => setTimeout(resolve, 1000))
+    }
+
     const doctype3 = await ceramic3.createDocument<ThreeIdDoctype>(DOCTYPE_3ID, { content: { test: 321 }, metadata: { owners: [owner] } }, { applyOnly: true })
     expect(doctype3.content).toEqual(doctype1.content)
     expect(doctype3.state).toEqual(doctype1.state)
