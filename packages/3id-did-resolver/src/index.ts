80d81ff3f4880fd0f27d1df088ba2e247acd1864
Joel Thorstensson <joel@oed.earth>
2020-08-18 14:48:02 +0200

    feat(3id-did-resolver): Support new multicodec key format

--- packages/3id-did-resolver/src/index.ts
+++ packages/3id-did-resolver/src/index.ts
@@ -1,3 +1,4 @@
+import bs58 from 'bs58'
 import { Doctype } from "@ceramicnetwork/ceramic-common"
 import type { ParsedDID, DIDResolver, DIDDocument } from 'did-resolver'
 
@@ -9,31 +10,72 @@ interface ResolverRegistry {
   [index: string]: DIDResolver;
 }
 
-export function wrapDocument (content: any, did: string): DIDDocument {
-  // this should be generated in a much more dynamic way based on the content of the doc
-  // keys should be encoded using multicodec, by looking at the codec bits
-  // we can determine the key type.
-  return {
+export function wrapDocument(content: any, did: string): DIDDocument {
+  const startDoc = {
     '@context': 'https://w3id.org/did/v1',
     id: did,
-    publicKey: [{
-      id: `${did}#signingKey`,
-      type: 'Secp256k1VerificationKey2018',
-      owner: did,
-      publicKeyHex: content.publicKeys.signing
-    }, {
-      id: `${did}#encryptionKey`,
-      type: 'Curve25519EncryptionPublicKey',
-      owner: did,
-      publicKeyBase64: content.publicKeys.encryption
-    }],
-    authentication: [{
-      type: 'Secp256k1SignatureAuthentication2018',
-      publicKey: `${did}#signingKey`,
-    }]
+    publicKey: [],
+    authentication: [],
+    keyAgreement: []
   }
+  return Object.entries(content.publicKeys).reduce((diddoc, [keyName, keyValue]) => {
+    if (keyValue.startsWith('z')) { // we got a multicodec encoded key
+      const keyBuf = bs58.decode(keyValue.slice(1))
+      if (keyBuf[0] === 0xe7) { // it's secp256k1
+        diddoc.publicKey.push({
+          id: `${did}#${keyName}`,
+          type: 'Secp256k1VerificationKey2018',
+          owner: did,
+          // remove multicodec varint and encode to hex
+          publicKeyHex: keyBuf.slice(2).toString('hex')
+        })
+        diddoc.authentication.push({
+          type: 'Secp256k1SignatureAuthentication2018',
+          publicKey: `${did}#${keyName}`,
+        })
+      } else if (keyBuf[0] === 0xec) { // it's x25519
+        // old key format, likely not needed in the future
+        diddoc.publicKey.push({
+          id: `${did}#${keyName}`,
+          type: 'Curve25519EncryptionPublicKey',
+          owner: did,
+          publicKeyBase64: keyBuf.slice(2).toString('base64')
+        })
+        // new keyAgreement format for x25519 keys
+        diddoc.keyAgreement.push({
+          id: `${did}#${keyName}`,
+          type: 'X25519KeyAgreementKey2019',
+          controller: did,
+          publicKeyBase58: bs58.encode(keyBuf.slice(2))
+        })
+      }
+    } else { // we need to be backwards compatible (until js-did is used everywhere)
+      if(keyName === 'signing') {
+        diddoc.publicKey.push({
+          id: `${did}#${keyName}`,
+          type: 'Secp256k1VerificationKey2018',
+          owner: did,
+          // remove multicodec varint and encode to hex
+          publicKeyHex: keyValue
+        })
+        diddoc.authentication.push({
+          type: 'Secp256k1SignatureAuthentication2018',
+          publicKey: `${did}#${keyName}`,
+        })
+      } else if (keyName === 'encryption') {
+        diddoc.publicKey.push({
+          id: `${did}#${keyName}`,
+          type: 'Curve25519EncryptionPublicKey',
+          owner: did,
+          publicKeyBase64: keyValue
+        })
+      }
+    }
+    return diddoc
+  }, startDoc)
 }
 
+// TODO - add backwards compatibility for 3IDv0
 export default {
   getResolver: (ceramic: Ceramic): ResolverRegistry => ({
     '3': async (did: string, parsed: ParsedDID): Promise<DIDDocument | null> => {
