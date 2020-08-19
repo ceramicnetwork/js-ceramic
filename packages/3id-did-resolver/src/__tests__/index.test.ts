0b944f7ce03e881c0f715f8e5143f05ee14b8fc3
Joel Thorstensson <oed@3box.io>
2020-08-19 12:43:22 +0200

    feat(3id-did-resolver): Support new multicodec key format (#218)
    * feat(3id-did-resolver): Support new multicodec key format

--- packages/3id-did-resolver/src/__tests__/index.test.ts
+++ packages/3id-did-resolver/src/__tests__/index.test.ts
@@ -5,6 +5,17 @@ const ceramicMock = {
   loadDocument: async (): Promise<any> => ({
     content: {
       publicKeys: {
+        signing: 'zQ3shwsCgFanBax6UiaLu1oGvM7vhuqoW88VBUiUTCeHbTeTV',
+        encryption: 'z6LSfQabSbJzX8WAm1qdQcHCHTzVv8a2u6F7kmzdodfvUCo9'
+      }
+    }
+  })
+}
+
+const ceramicMockOld = { // to be removed
+  loadDocument: async (): Promise<any> => ({
+    content: {
+      publicKeys: {
         signing: 'fake signing key',
         encryption: 'fake encryption key'
       }
@@ -19,7 +30,14 @@ describe('3ID DID Resolver', () => {
     expect(Object.keys(threeIdResolver)).toEqual(['3'])
   })
 
-  it('resolver works correctly', async () => {
+  it('resolver works correctly (old format)', async () => {
+    const threeIdResolver = ThreeIdResolver.getResolver(ceramicMockOld)
+    const resolver = new Resolver(threeIdResolver)
+    const fake3ID = 'did:3:bafyiuh3f97hqef97h'
+    expect(await resolver.resolve(fake3ID)).toMatchSnapshot()
+  })
+
+  it('resolves 3id document correctly', async () => {
     const threeIdResolver = ThreeIdResolver.getResolver(ceramicMock)
     const resolver = new Resolver(threeIdResolver)
     const fake3ID = 'did:3:bafyiuh3f97hqef97h'
