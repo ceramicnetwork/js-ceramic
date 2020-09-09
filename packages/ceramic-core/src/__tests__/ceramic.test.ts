6f199cc4cfa7a4b92c94de3867bbd0bf404f4090
Joel Thorstensson <joel@oed.earth>
2020-09-09 13:53:06 +0200

    fix: test

--- packages/ceramic-core/src/__tests__/ceramic.test.ts
+++ packages/ceramic-core/src/__tests__/ceramic.test.ts
@@ -191,7 +191,8 @@ describe('Ceramic integration', () => {
     })
     await ceramic3.setDIDProvider(idw.getDidProvider())
 
-    const owner = ceramic1.context.did.id
+    //const owner = ceramic1.context.did.id
+    const owner = idw._threeIdx.managementDID
 
     // ceramic node 2 shouldn't need to have the document open in order to forward the message
     const doctype1 = await ceramic1.createDocument<ThreeIdDoctype>(DOCTYPE_3ID, { content: { test: 321 }, metadata: { owners: [owner] } })
