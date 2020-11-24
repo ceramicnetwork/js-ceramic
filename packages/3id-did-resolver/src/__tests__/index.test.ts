eedeb989855540445c8d693c01a5c26e5796e5b4
Zach Ferland <zachferland@gmail.com>
2020-11-24 04:35:59 -0500

    feat(3id-did-resolver): resolve v0 and v1 3ids (#511)
    * feat: 3id resolve v0 and v1 3ids
    
    * feat: remove legacy lib and ipsf did doc lib, ref, errors
    
    * ref: buf to u8a, remove legacy paths, 3idv0 -> 3idv1 key encoding
    
    * feat: publicKeyBase58 keys (vs hex/base64) and remove old key resolve
    
    * test: resolver test dependencies
    
    * feat: v0 to base58, v0 === v1 doc, ref
    
    * fix: properly encode DID documents
    
    Co-authored-by: Joel Thorstensson <joel@oed.earth>

--- packages/3id-did-resolver/src/__tests__/index.test.ts
+++ packages/3id-did-resolver/src/__tests__/index.test.ts
@@ -1,39 +1,57 @@
+jest.mock('cross-fetch', () =>  {
+  return () => ({
+    ok: true, 
+    json: async () => JSON.parse('{"value":{"id":"did:3:GENESIS","@context":"https://w3id.org/did/v1","publicKey":[{"id":"did:3:GENESIS#signingKey","type":"Secp256k1VerificationKey2018","publicKeyHex":"0452fbcde75f7ddd7cff18767e2b5536211f500ad474c15da8e74577a573e7a346f2192ef49a5aa0552c41f181a7950af3afdb93cafcbff18156943e3ba312e5b2"},{"id":"did:3:GENESIS#encryptionKey","type":"Curve25519EncryptionPublicKey","publicKeyBase64":"DFxR24MNHVxEDAdL2f6pPEwNDJ2p0Ldyjoo7y/ItLDc="},{"id":"did:3:GENESIS#managementKey","type":"Secp256k1VerificationKey2018","ethereumAddress":"0x3f0bb6247d647a30f310025662b29e6fa382b61d"}],"authentication":[{"type":"Secp256k1SignatureAuthentication2018","publicKey":"did:3:GENESIS#signingKey"}]}}')
+  })
+})
+
 import ThreeIdResolver from '../index'
 import { Resolver } from 'did-resolver'
+import DocID from '@ceramicnetwork/docid'
 
 const ceramicMock = {
-  loadDocument: async (): Promise<any> => ({
-    content: {
-      publicKeys: {
-        signing: 'zQ3shwsCgFanBax6UiaLu1oGvM7vhuqoW88VBUiUTCeHbTeTV',
-        encryption: 'z6LSfQabSbJzX8WAm1qdQcHCHTzVv8a2u6F7kmzdodfvUCo9'
+  loadDocument: async (): Promise<any> => {
+    const signing = 'zQ3shwsCgFanBax6UiaLu1oGvM7vhuqoW88VBUiUTCeHbTeTV'
+    const encryption = 'z6LSfQabSbJzX8WAm1qdQcHCHTzVv8a2u6F7kmzdodfvUCo9'
+    // mimic how IDW sets the 3ID DID document
+    return {
+      content: {
+        publicKeys: {
+          [signing.slice(-15)]: signing,
+          [encryption.slice(-15)]: encryption,
+        },
       }
     }
-  })
-}
-
-const ceramicMockOld = { // to be removed
-  loadDocument: async (): Promise<any> => ({
-    content: {
-      publicKeys: {
-        signing: 'fake signing key',
-        encryption: 'fake encryption key'
-      }
-    }
+  },
+  createDocument: async (): Promise<any> => ({
+    id: DocID.fromString('k2t6wyfsu4pg0t2n4j8ms3s33xsgqjhtto04mvq8w5a2v5xo48idyz38l7ydki')
   })
 }
 
 const ceramicMockWithIDX = {
-  loadDocument: async (): Promise<any> => ({
-    content: {
-      publicKeys: {
-        signing: 'zQ3shwsCgFanBax6UiaLu1oGvM7vhuqoW88VBUiUTCeHbTeTV',
-        encryption: 'z6LSfQabSbJzX8WAm1qdQcHCHTzVv8a2u6F7kmzdodfvUCo9'
-      },
-      idx: 'ceramic://rootId'
+  loadDocument: async (): Promise<any> => {
+    const signing = 'zQ3shwsCgFanBax6UiaLu1oGvM7vhuqoW88VBUiUTCeHbTeTV'
+    const encryption = 'z6LSfQabSbJzX8WAm1qdQcHCHTzVv8a2u6F7kmzdodfvUCo9'
+    // mimic how IDW sets the 3ID DID document
+    return {
+      content: {
+        publicKeys: {
+          [signing.slice(-15)]: signing,
+          [encryption.slice(-15)]: encryption,
+        },
+        idx: 'ceramic://rootId'
+      }
     }
-  })
+  }
 }
+
+const ceramicMockNull = { // to be removed
+  loadDocument: async (): Promise<any> => { return { } },
+  createDocument: async (): Promise<any> => ({ id: 'k2t6wyfsu4pg0t2n4j8ms3s33xsgqjhtto04mvq8w5a2v5xo48idyz38l7ydki' })
+}
+
+const fake3ID = 'did:3:k2t6wyfsu4pg0t2n4j8ms3s33xsgqjhtto04mvq8w5a2v5xo48idyz38l7ydki'
+const fakeLegacy3ID = 'did:3:bafyreiffkeeq4wq2htejqla2is5ognligi4lvjhwrpqpl2kazjdoecmugi'
 
 describe('3ID DID Resolver', () => {
 
@@ -42,24 +60,29 @@ describe('3ID DID Resolver', () => {
     expect(Object.keys(threeIdResolver)).toEqual(['3'])
   })
 
-  it('resolver works correctly (old format)', async () => {
-    const threeIdResolver = ThreeIdResolver.getResolver(ceramicMockOld)
-    const resolver = new Resolver(threeIdResolver)
-    const fake3ID = 'did:3:bafyiuh3f97hqef97h'
-    expect(await resolver.resolve(fake3ID)).toMatchSnapshot()
-  })
-
   it('resolves 3id document correctly', async () => {
     const threeIdResolver = ThreeIdResolver.getResolver(ceramicMock)
     const resolver = new Resolver(threeIdResolver)
-    const fake3ID = 'did:3:bafyiuh3f97hqef97h'
     expect(await resolver.resolve(fake3ID)).toMatchSnapshot()
   })
 
   it('adds IDX root as service', async () => {
     const threeIdResolver = ThreeIdResolver.getResolver(ceramicMockWithIDX)
     const resolver = new Resolver(threeIdResolver)
-    const fake3ID = 'did:3:bafyiuh3f97hqef97h'
     expect(await resolver.resolve(fake3ID)).toMatchSnapshot()
+  })
+})
+
+describe('3ID DID Resolver Legacy (v0)', () => {
+  it('resolves 3id v0', async () => {
+    const threeIdResolver = ThreeIdResolver.getResolver(ceramicMockNull)
+    const resolver = new Resolver(threeIdResolver)
+    expect(await resolver.resolve(fakeLegacy3ID)).toMatchSnapshot()
+  })
+
+  it('resolves 3id v0 which includs ceramic updates', async () => {
+    const threeIdResolver = ThreeIdResolver.getResolver(ceramicMock)
+    const resolver = new Resolver(threeIdResolver)
+    expect(await resolver.resolve(fakeLegacy3ID)).toMatchSnapshot()
   })
 })
--- packages/3id-did-resolver/src/index.ts
+++ packages/3id-did-resolver/src/index.ts
@@ -1,9 +1,13 @@
-import bs58 from 'bs58'
-import { Doctype } from "@ceramicnetwork/common"
 import type { ParsedDID, DIDResolver, DIDDocument } from 'did-resolver'
+import { Doctype } from "@ceramicnetwork/common"
+import LegacyResolver from './legacyResolver'
+import * as u8a from 'uint8arrays'
+import DocID from '@ceramicnetwork/docid'
+import CID from 'cids'
 
 interface Ceramic {
-  loadDocument(docId: string): Promise<Doctype>;
+  loadDocument(docId: DocID): Promise<Doctype>;
+  createDocument(type: string, content: any, opts: any): Promise<Doctype>;
 }
 
 interface ResolverRegistry {
@@ -12,6 +16,7 @@ interface ResolverRegistry {
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function wrapDocument(content: any, did: string): DIDDocument {
+  if (!(content && content.publicKeys)) throw new Error('Not a valid 3ID')
   const startDoc: DIDDocument = {
     '@context': 'https://w3id.org/did/v1',
     id: did,
@@ -20,57 +25,35 @@ export function wrapDocument(content: any, did: string): DIDDocument {
     keyAgreement: []
   }
   const doc = Object.entries(content.publicKeys as string[]).reduce((diddoc, [keyName, keyValue]) => {
-    if (keyValue.startsWith('z')) { // we got a multicodec encoded key
-      const keyBuf = bs58.decode(keyValue.slice(1))
-      if (keyBuf[0] === 0xe7) { // it's secp256k1
-        diddoc.publicKey.push({
-          id: `${did}#${keyName}`,
-          type: 'Secp256k1VerificationKey2018',
-          controller: did,
-          // remove multicodec varint and encode to hex
-          publicKeyHex: keyBuf.slice(2).toString('hex')
-        })
-        diddoc.authentication.push({
-          type: 'Secp256k1SignatureAuthentication2018',
-          publicKey: `${did}#${keyName}`,
-        })
-      } else if (keyBuf[0] === 0xec) { // it's x25519
-        // old key format, likely not needed in the future
-        diddoc.publicKey.push({
-          id: `${did}#${keyName}`,
-          type: 'Curve25519EncryptionPublicKey',
-          controller: did,
-          publicKeyBase64: keyBuf.slice(2).toString('base64')
-        })
-        // new keyAgreement format for x25519 keys
-        diddoc.keyAgreement.push({
-          id: `${did}#${keyName}`,
-          type: 'X25519KeyAgreementKey2019',
-          controller: did,
-          publicKeyBase58: bs58.encode(keyBuf.slice(2))
-        })
-      }
-    } else { // we need to be backwards compatible (until js-did is used everywhere)
-      if(keyName === 'signing') {
-        diddoc.publicKey.push({
-          id: `${did}#${keyName}`,
-          type: 'Secp256k1VerificationKey2018',
-          controller: did,
-          // remove multicodec varint and encode to hex
-          publicKeyHex: keyValue
-        })
-        diddoc.authentication.push({
-          type: 'Secp256k1SignatureAuthentication2018',
-          publicKey: `${did}#${keyName}`,
-        })
-      } else if (keyName === 'encryption') {
-        diddoc.publicKey.push({
-          id: `${did}#${keyName}`,
-          type: 'Curve25519EncryptionPublicKey',
-          controller: did,
-          publicKeyBase64: keyValue
-        })
-      }
+    const keyBuf = u8a.fromString(keyValue.slice(1), 'base58btc')
+    // remove multicodec varint
+    const publicKeyBase58 =  u8a.toString(keyBuf.slice(2), 'base58btc')
+    if (keyBuf[0] === 0xe7) { // it's secp256k1
+      diddoc.publicKey.push({
+        id: `${did}#${keyName}`,
+        type: 'Secp256k1VerificationKey2018',
+        controller: did,
+        publicKeyBase58
+      })
+      diddoc.authentication.push({
+        type: 'Secp256k1SignatureAuthentication2018',
+        publicKey: `${did}#${keyName}`,
+      })
+    } else if (keyBuf[0] === 0xec) { // it's x25519
+      // old key format, likely not needed in the future
+      diddoc.publicKey.push({
+        id: `${did}#${keyName}`,
+        type: 'Curve25519EncryptionPublicKey',
+        controller: did,
+        publicKeyBase58
+      })
+      // new keyAgreement format for x25519 keys
+      diddoc.keyAgreement.push({
+        id: `${did}#${keyName}`,
+        type: 'X25519KeyAgreementKey2019',
+        controller: did,
+        publicKeyBase58
+      })
     }
     return diddoc
   }, startDoc)
@@ -88,13 +71,53 @@ export function wrapDocument(content: any, did: string): DIDDocument {
   return doc
 }
 
-// TODO - add backwards compatibility for 3IDv0
+const isLegacyDid = (didId: string): boolean => {
+  try {
+    new CID(didId)
+    return true
+  } catch(e) {
+    return false
+  }
+}
+
+const getVersion = (query = ''): string | null => {
+  const versionParam = query.split('&').find(e => e.includes('version-id'))
+  return versionParam ? versionParam.split('=')[1] : null
+}
+
+const legacyResolve = async (ceramic: Ceramic, didId: string, version?: string): Promise<DIDDocument | null> => {
+  const legacyPublicKeys = await LegacyResolver(didId) // can add opt to pass ceramic ipfs to resolve
+  if (!legacyPublicKeys) return null
+
+  const legacyDoc = wrapDocument(legacyPublicKeys, `did:3:${didId}`)
+  if (version === '0') return legacyDoc
+
+  const managementKey = `did:key:${legacyPublicKeys.publicKeys.signing}`
+
+  try {
+    const content =  { metadata: { controllers: [managementKey], family: '3id' } }
+    const doc = await ceramic.createDocument('tile', content, { applyOnly: true })
+    const didDoc = await resolve(ceramic, doc.id.toString(), version)
+    return didDoc
+  } catch(e) {
+    if (version) throw new Error('Not a valid 3ID')
+    return legacyDoc
+  }
+}
+
+const resolve = async (ceramic: Ceramic, didId: string, version?: string): Promise<DIDDocument | null> =>  {
+  const docId = DocID.fromString(didId, version)
+  const doctype = await ceramic.loadDocument(docId)
+  return wrapDocument(doctype.content, `did:3:${didId}`)
+}
+
 export default {
-  getResolver: (ceramic: Ceramic): ResolverRegistry => ({
-    '3': async (did: string, parsed: ParsedDID): Promise<DIDDocument | null> => {
-      const version = (parsed.query || '').replace('version-id', '?version')
-      const doctype = await ceramic.loadDocument(`/ceramic/${parsed.id}${version}`);
-      return wrapDocument(doctype.content, did)
+  getResolver: (ceramic: Ceramic): ResolverRegistry => {
+    return {
+      '3': async (did: string, parsed: ParsedDID): Promise<DIDDocument | null> => {
+        const version = getVersion(parsed.query)
+        return isLegacyDid(parsed.id) ? legacyResolve(ceramic, parsed.id, version) : resolve(ceramic, parsed.id, version)
+      }
     }
-  })
+  }
 }
