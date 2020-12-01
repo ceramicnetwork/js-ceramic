8c3867b9f5f74b6e8f2f2ba9dea4614298e5c5d0
val <8445610+valmack@users.noreply.github.com>
2020-12-01 16:28:36 -0500

    feat: write warning if file write is blocked

--- packages/cli/src/ceramic-logger-plugins.ts
+++ packages/cli/src/ceramic-logger-plugins.ts
@@ -19,6 +19,11 @@ interface BlockedFiles {
     [filePath: string]: boolean
 }
 
+interface WriteResult {
+    blocked: boolean
+    filePath: string
+}
+
 /**
  * Plugin for the root logger from the `loglevel` library to write logs to files
  */
@@ -39,6 +44,7 @@ export class LogToFiles {
         pluginOptions: LoggerPluginOptions
     ): void {
         const originalFactory = rootLogger.methodFactory;
+
         let basePath = pluginOptions.logPath
         if ((basePath === undefined) || (basePath === '')) {
             basePath = '/usr/local/var/log/ceramic/'
@@ -54,11 +60,9 @@ export class LogToFiles {
                 fs.mkdir(basePath, { recursive: true }, async (err) => {
                     if (err && (err.code != 'EEXIST')) console.warn('WARNING: Can not write logs to files', err)
                     else {
-                        const filePrefix = basePath + loggerName.toLowerCase()
-                        const filePath = `${filePrefix}.log`
-
-                        await LogToFiles._writeFile(pluginState.blockedFiles, filePath, message, 'a')
-                        await LogToFiles._writeDocId(pluginState.blockedFiles, filePrefix, message)
+                        const filePrefix = loggerName.toLowerCase()
+                        await LogToFiles._write(pluginState.blockedFiles, basePath, filePrefix, message)
+                        await LogToFiles._writeDocId(pluginState.blockedFiles, basePath, filePrefix, message)
                     }
                 })
                 rawMethod(...args)
@@ -68,22 +72,86 @@ export class LogToFiles {
     }
 
     /**
-   * Writes `message` to a file
-   * @notice If the file is currently being written to by an open stream or if
-   * its buffer is full, the call to this method will return without writing
-   * @param filePath Full path of file to write to
-   * @param message Message to write to `filePath`
-   * @param writeFlag Specifies writing method (e.g. "a" for append, "w" for overwrite)
-   */
-    private static async _writeFile (
+     * Appends `message` to file `${basePath}${filePrefix}.log`
+     * @param basePath Base directory for file
+     * @param filePrefix Prefix of file name to write to
+     * @param message Message to write to file
+     */
+    private static async _write (
+        blockedFiles: BlockedFiles,
+        basePath: string,
+        filePrefix: string,
+        message: string
+    ): Promise<void> {
+        const filePath = `${basePath}${filePrefix}.log`
+        const result = await LogToFiles._writeStream(blockedFiles, filePath, message, 'a')
+        if (result.blocked) await LogToFiles._writeBlockedWarning(blockedFiles, basePath, filePath)
+    }
+
+    /**
+     * Writes the docId in `message` to file, if `message` contains a docId
+     * @notice The write operation overwrites the existing file at `${basePath}${filePrefix}.log`
+     * @param basePath Base directory for file
+     * @param filePrefix Prefix of file name to write to
+     * @param message Message to write to file
+     */
+    private static async _writeDocId (
+        blockedFiles: BlockedFiles,
+        basePath: string,
+        filePrefix: string,
+        message: string
+    ): Promise<void> {
+        const lookup = '/ceramic/'
+        const docIdIndex = message.indexOf(lookup)
+
+        if (docIdIndex > -1) {
+            const docIdSubstring = message.substring(docIdIndex)
+            const match = docIdSubstring.match(/\/ceramic\/\w+/)
+
+            if (match !== null) {
+                const docId = match[0]
+                const filePath = `${basePath}${filePrefix}-docids.log`
+                const result = await LogToFiles._writeStream(blockedFiles, filePath, docId, 'w')
+                if (result.blocked) await LogToFiles._writeBlockedWarning(blockedFiles, basePath, filePath)
+            }
+        }
+    }
+
+    /**
+     * Logs and writes warning message about `filePath` being blocked.
+     * @notice If the file that this tries to write to itself is blocked,
+     * its message will be dropped and only appear in the console
+     * @param basePath Base directory for file to write to
+     * @param filePath Path to file that is blocked
+     */
+    private static async _writeBlockedWarning (
+        blockedFiles: BlockedFiles,
+        basePath: string,
+        filePath: string
+    ): Promise<void> {
+        const loggerFilePath = basePath + 'logger.log'
+        const message = `Stream blocked for file: ${filePath}\n`
+        await LogToFiles._writeStream(blockedFiles, loggerFilePath, message, 'a')
+    }
+
+    /**
+      * Writes `message` to a file and returns whether it is blocked or not
+      * @notice If the file is currently being written to by an open stream or if
+      * its buffer is full, the call to this method will return without writing
+      * @param filePath Full path of file to write to
+      * @param message Message to write to `filePath`
+      * @param writeFlag Specifies writing method (e.g. "a" for append, "w" for overwrite)
+      * @returns True if the stream is blocked and false otherwise
+      */
+    private static async _writeStream (
         blockedFiles: BlockedFiles,
         filePath: string,
         message: string,
         writeFlag: string
-    ): Promise<void> {
+    ): Promise<WriteResult> {
         if (blockedFiles[filePath]) {
             console.warn(`Stream busy for ${filePath}. Some logs may be dropped.`)
-            return
+            return { blocked: blockedFiles[filePath], filePath }
         }
 
         blockedFiles[filePath] = true
@@ -108,28 +176,7 @@ export class LogToFiles {
             stream.end()
             return
         })
-    }
-
-    /**
-     * Writes the docId in `message` to `filePath`, if `message` contains a docId.
-     * @notice The write operation overwrites any existing file.
-     * @param filePrefix Prefix of file name to write to
-     * @param message Message to write to `filePath`
-     */
-    private static async _writeDocId (blockedFiles: BlockedFiles, filePrefix: string, message: string): Promise<void> {
-        const lookup = '/ceramic/'
-        const docIdIndex = message.indexOf(lookup)
-
-        if (docIdIndex > -1) {
-            const docIdSubstring = message.substring(docIdIndex)
-            const match = docIdSubstring.match(/\/ceramic\/\w+/)
-
-            if (match !== null) {
-                const docId = match[0]
-                const filePath = filePrefix + '-docids.log'
-                LogToFiles._writeFile(blockedFiles, filePath, docId, 'w')
-            }
-        }
+        return { blocked: blockedFiles[filePath], filePath }
     }
 
     /**
@@ -137,6 +184,7 @@ export class LogToFiles {
      * @notice Returns false if file is not found.
      * Returns true if unable to get file creation datetime for some other reason.
      * @param filePath Full path of file
+     * @returns If the file is expired
      */
     private static async _isExpired (filePath: string): Promise<boolean> {
         try {
