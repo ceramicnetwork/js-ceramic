49dacb8a3461983adb6dd9e7b55d989e20a7bc0d
Joel Thorstensson <joel@oed.earth>
2020-12-03 13:49:28 +0100

    fix(doctype-tile): set controller in a better way

--- packages/doctype-tile/src/tile-doctype.ts
+++ packages/doctype-tile/src/tile-doctype.ts
@@ -65,9 +65,9 @@ export class TileDoctype extends Doctype {
         const unique = params.deterministic ? '0' : base64Encode(randomBytes(12))
 
         const metadata = params.metadata ? params.metadata : { controllers: [] }
-        if (!metadata.controllers || metadata.controllers.length === 0) {
-            if (params.content) {
-                metadata.controllers = [context.did?.id]
+        if (metadata.controllers.length === 0) {
+            if (params.content && content.did) {
+                metadata.controllers = [context.did.id]
             } else {
                 throw new Error('No controllers specified')
             }
