510061c485a177b1cfc1fd27294310f66cf787f5
Spencer T Brody <spencer@3box.io>
2020-12-16 16:41:35 -0500

    feat(core): Update default CAS URL to point to clay infra

--- packages/cli/src/ceramic-daemon.ts
+++ packages/cli/src/ceramic-daemon.ts
@@ -12,7 +12,7 @@ const DEFAULT_PORT = 7007
 const DEFAULT_NETWORK = 'testnet-clay'
 const toApiPath = (ending: string): string => '/api/v0' + ending
 
-const DEFAULT_ANCHOR_SERVICE_URL = "https://cas.3box.io:8081"
+const DEFAULT_ANCHOR_SERVICE_URL = "https://cas-clay.3box.io:8081"
 
 /**
  * Daemon create options
