a3f4a91f4b22652f1e240b8cd314b3b7b2fcdd0b
Spencer T Brody <spencer@3box.io>
2021-01-12 16:58:33 -0500

    put new test at end of test file

--- packages/core/src/store/__tests__/state-store-integration.test.ts
+++ packages/core/src/store/__tests__/state-store-integration.test.ts
@@ -217,41 +217,6 @@ describe('Level data store', () => {
     store = await storeFactory.open()
   })
 
-  it('pins in different networks', async () => {
-    // Create a document to test with
-    const genesis = await TileDoctype.makeGenesis({ content: initialContent, metadata: { controllers, tags: ['3id'] } }, context)
-    const genesisCid = await dispatcher.storeCommit(genesis)
-    const docId = new DocID('tile', genesisCid)
-    const doc = await Document.create(docId, doctypeHandler, dispatcher, store, context)
-    await anchorUpdate(doc.doctype)
-
-    const levelPath = (await tmp.dir({unsafeCleanup: true})).path
-    const storeFactoryLocal = new PinStoreFactory(context, {
-      stateStorePath: levelPath,
-      pinnings: ['ipfs+context'],
-      networkName: "local",
-    })
-    const localStore = await storeFactoryLocal.open()
-
-    await localStore.stateStore.save(doc.doctype)
-    let docState = await localStore.stateStore.load(doc.id)
-    expect(docState).toBeDefined()
-
-    await localStore.close()
-
-    // Now create a net pin store for a different ceramic network
-    const storeFactoryInMemory = new PinStoreFactory(context, {
-      stateStorePath: levelPath,
-      pinnings: ['ipfs+context'],
-      networkName: "inmemory",
-    })
-    const inMemoryStore = await storeFactoryInMemory.open()
-
-    // The new pin store shouldn't be able to see docs that were pinned on the other network
-    docState = await inMemoryStore.stateStore.load(doc.id)
-    expect(docState).toBeNull()
-  })
-
   it('pins document correctly without IPFS pinning', async () => {
     const genesis = await TileDoctype.makeGenesis({ content: initialContent, metadata: { controllers, tags: ['3id'] } }, context)
     const genesisCid = await dispatcher.storeCommit(genesis)
@@ -369,5 +334,40 @@ describe('Level data store', () => {
     }
     expect(pinned.length).toEqual(0)
     expect(dispatcher._ipfs.pin.ls).toHaveBeenCalledTimes(0)
+  })
+
+  it('pins in different networks', async () => {
+    // Create a document to test with
+    const genesis = await TileDoctype.makeGenesis({ content: initialContent, metadata: { controllers, tags: ['3id'] } }, context)
+    const genesisCid = await dispatcher.storeCommit(genesis)
+    const docId = new DocID('tile', genesisCid)
+    const doc = await Document.create(docId, doctypeHandler, dispatcher, store, context)
+    await anchorUpdate(doc.doctype)
+
+    const levelPath = (await tmp.dir({unsafeCleanup: true})).path
+    const storeFactoryLocal = new PinStoreFactory(context, {
+      stateStorePath: levelPath,
+      pinnings: ['ipfs+context'],
+      networkName: "local",
+    })
+    const localStore = await storeFactoryLocal.open()
+
+    await localStore.stateStore.save(doc.doctype)
+    let docState = await localStore.stateStore.load(doc.id)
+    expect(docState).toBeDefined()
+
+    await localStore.close()
+
+    // Now create a net pin store for a different ceramic network
+    const storeFactoryInMemory = new PinStoreFactory(context, {
+      stateStorePath: levelPath,
+      pinnings: ['ipfs+context'],
+      networkName: "inmemory",
+    })
+    const inMemoryStore = await storeFactoryInMemory.open()
+
+    // The new pin store shouldn't be able to see docs that were pinned on the other network
+    docState = await inMemoryStore.stateStore.load(doc.id)
+    expect(docState).toBeNull()
   })
 })
