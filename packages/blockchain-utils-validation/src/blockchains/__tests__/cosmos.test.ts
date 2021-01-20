a4a444d9a990f76c21d5782d6fc0d7ea753c919d
Stanly Johnson <stanlyjohnson@outlook.com>
2021-01-20 18:33:14 +0530

    add validation for cosmos

---
+++ packages/blockchain-utils-validation/src/blockchains/__tests__/cosmos.test.ts
@@ -0,0 +1,36 @@
+import { signTx, Tx, SignMeta, createWalletFromMnemonic, Wallet, StdTx } from '@tendermint/sig';
+import { validateLink } from '../cosmos';
+import * as linking from "@ceramicnetwork/blockchain-utils-linking";
+
+const did = 'did:3:bafysdfwefwe';
+const mnemonic = 'test salon husband push melody usage fine ensure blade deal miss twin';
+const local_provider = createWalletFromMnemonic(mnemonic);
+
+class CosmosMockSigner {
+  readonly provider: Wallet;
+
+  constructor(local_provider: Wallet) {
+    this.provider = local_provider;
+  }
+
+  public async sign(msg : Tx, metadata : SignMeta) : Promise<StdTx>{
+    return new Promise((resolve): void => {
+      const signature = signTx(msg, metadata, this.provider);
+      resolve(signature);
+    }); 
+  }
+}
+
+describe('Blockchain: Cosmos', () => {
+  describe('validateLink', () => {
+    test('validate proof for cosmoshub3', async () => {
+      const provider = new CosmosMockSigner(local_provider);
+      const authProvider = new linking.cosmos.CosmosAuthProvider(
+        provider,
+        local_provider.address
+      );
+      const proof = await authProvider.createLink(did);
+      await expect(validateLink(proof)).resolves.toEqual(proof);
+    });
+  });
+});
---
+++ packages/blockchain-utils-validation/src/blockchains/cosmos.ts
@@ -0,0 +1,59 @@
+import { AccountID } from "caip";
+import { Tx, SignMeta, verifyTx } from '@tendermint/sig';
+import { BlockchainHandler } from "../blockchain-handler";
+import * as linking from "@ceramicnetwork/blockchain-utils-linking";
+import * as uint8arrays from "uint8arrays";
+
+const namespace = "cosmos";
+
+const stringEncode = (str: string): string => uint8arrays.toString(uint8arrays.fromString(str), 'base64pad');
+const stringDecode = (str: string): string => uint8arrays.toString(uint8arrays.fromString(str, 'base64pad'));
+
+// return data in the cosmos unsigned transaction format
+function asTransaction(address: string, message: string): Tx {
+  return {
+    fee: {
+      amount: [{ amount: '0', denom: '' }],
+      gas: '0',
+    },
+    memo: message,
+    msg: [
+      {
+        type: 'cosmos-sdk/MsgSend',
+        value: {
+          from_address: address,
+          to_address: address,
+          amount: [{ amount: '0', denom: '0' }],
+        },
+      },
+    ],
+  };
+}
+
+// generate metadata for signing the transaction
+function getMetaData(): SignMeta {
+  return {
+    account_number: '1',
+    chain_id: 'cosmos',
+    sequence: '0',
+  };
+}
+
+export async function validateLink(
+  proof: linking.LinkProof
+): Promise<linking.LinkProof | null> {
+    const account = new AccountID(proof.account);
+    const encodedMsg = stringEncode(proof.message);
+    const payload = asTransaction(account.address, encodedMsg);
+    const sigObj = JSON.parse(stringDecode(proof.signature));
+    const Tx = { ...payload, ...getMetaData(), signatures: [sigObj] };
+    const is_sig_valid = verifyTx(Tx, getMetaData());
+    return is_sig_valid ? proof : null;
+}
+
+const Handler: BlockchainHandler = {
+  namespace,
+  validateLink,
+};
+
+export default Handler;
