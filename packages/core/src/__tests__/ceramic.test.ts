9dc92fdd326c04075a51cf5c3cec19771b7a8b24
Spencer T Brody <spencer@3box.io>
2021-01-25 13:21:48 -0500

    fix(core): Fix flaky test

--- packages/core/src/__tests__/ceramic.test.ts
+++ packages/core/src/__tests__/ceramic.test.ts
@@ -245,7 +245,6 @@ describe('Ceramic integration', () => {
     }, {
       anchor: false, publish: false, sync: false
     })
-    await syncDoc(doctype3) // sync anchor record for genesis
 
     expect(doctype3.content).toEqual(doctype1.content)
 
