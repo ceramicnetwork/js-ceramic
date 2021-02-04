fd9bd2991aa85df26169032b3b04db64ed34dc97
ukstv <sergey@ukstv.me>
2021-02-04 12:15:13 +0300

    feat: Export auth providers

--- packages/blockchain-utils-linking/src/index.ts
+++ packages/blockchain-utils-linking/src/index.ts
@@ -5,3 +5,9 @@ export * as filecoin from "./filecoin";
 export * as polkadot from "./polkadot";
 export * as eosio from "./eosio";
 export * as cosmos from "./cosmos";
+
+export { EthereumAuthProvider } from "./ethereum";
+export { FilecoinAuthProvider } from "./filecoin";
+export { EosioAuthProvider } from "./eosio";
+export { PolkadotAuthProvider } from "./polkadot";
+export { CosmosAuthProvider } from "./cosmos";
