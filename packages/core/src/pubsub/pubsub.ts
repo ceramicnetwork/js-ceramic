d50da6cf23d2de4c0347a332bacdd4c800f360e8
ukstv <sergey@ukstv.me>
2021-02-24 17:18:53 +0300

    feat(core): Log incoming pubsub messages in Pubsub

--- packages/core/src/pubsub/pubsub.ts
+++ packages/core/src/pubsub/pubsub.ts
@@ -1,21 +1,45 @@
-import { Observable, EMPTY, pipe, of, from, Subscription } from 'rxjs';
+import { Observable, EMPTY, pipe, of, from, Subscription, UnaryFunction } from 'rxjs';
 import { deserialize, PubsubMessage, serialize } from './pubsub-message';
 import { IpfsApi } from '@ceramicnetwork/common';
-import { map, catchError, mergeMap } from 'rxjs/operators';
-import { IncomingChannel, filterOuter } from './incoming-channel';
+import { map, catchError, mergeMap, withLatestFrom } from 'rxjs/operators';
+import { IncomingChannel, filterOuter, IPFSPubsubMessage } from './incoming-channel';
 import { DiagnosticsLogger, ServiceLogger } from '@ceramicnetwork/logger';
 import { Memoize } from 'typescript-memoize';
 
-// Internal observable that does not emit if error happens.
-const ipfsToPubsub = pipe(
-  mergeMap((message) =>
-    of(message).pipe(
-      map(deserialize),
-      catchError(() => EMPTY),
+/**
+ * Deserialize incoming message in an internal observable that does not emit if error happens.
+ * Log a successfully deserialized message.
+ *
+ * @param peerId$ - own IPFS node peer id
+ * @param pubsubLogger - logger that dumps a successfully deserialized message
+ * @param topic - IPFS pubsub topic we listen
+ */
+function ipfsToPubsub(
+  peerId$: Observable<string>,
+  pubsubLogger: ServiceLogger,
+  topic: string,
+): UnaryFunction<Observable<IPFSPubsubMessage>, Observable<PubsubMessage>> {
+  return pipe(
+    withLatestFrom(peerId$),
+    mergeMap(([incoming, peerId]) =>
+      of(incoming).pipe(
+        map((incoming) => {
+          const message = deserialize(incoming);
+          const logMessage = { ...incoming, data: message };
+          delete logMessage.key;
+          delete logMessage.signature;
+          pubsubLogger.log({ peer: peerId, event: 'received', topic: topic, message: logMessage });
+          return message;
+        }),
+        catchError(() => EMPTY),
+      ),
     ),
-  ),
-);
+  );
+}
 
+/**
+ * Receive and publish messages to IPFS pubsub.
+ */
 export class Pubsub extends Observable<PubsubMessage> {
   constructor(
     private readonly ipfs: IpfsApi,
@@ -26,12 +50,15 @@ export class Pubsub extends Observable<PubsubMessage> {
   ) {
     super((subscriber) => {
       const incoming$ = new IncomingChannel(ipfs, topic, resubscribeEvery, pubsubLogger, logger);
-      incoming$.pipe(filterOuter(this.peerId$), ipfsToPubsub).subscribe(subscriber);
+
+      incoming$
+        .pipe(filterOuter(this.peerId$), ipfsToPubsub(this.peerId$, pubsubLogger, topic))
+        .subscribe(subscriber);
     });
   }
 
   @Memoize()
-  get peerId$() {
+  private get peerId$() {
     return from<Promise<string>>(this.ipfs.id().then((_) => _.id));
   }
 
