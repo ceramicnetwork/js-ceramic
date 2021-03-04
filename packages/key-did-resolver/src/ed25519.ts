62565e302cb128aeb6d6992667e2b5f30234e1b5
Joel Thorstensson <joel@oed.earth>
2021-03-04 12:55:30 +0100

    feat(key-did-resolver): upgrade to did-resolver v3

--- packages/key-did-resolver/src/ed25519.ts
+++ packages/key-did-resolver/src/ed25519.ts
@@ -15,28 +15,27 @@ function encodeKey(key: Uint8Array): string {
  * Constructs the document based on the method key
  */
 export function keyToDidDoc (pubKeyBytes: Uint8Array, fingerprint: string): any {
-    const did = `did:key:${fingerprint}`
-    const keyId = `${did}#${fingerprint}`
-    const x25519PubBytes = convertPublicKeyToX25519(pubKeyBytes)
-    const x25519KeyId = `${did}#${encodeKey(x25519PubBytes)}`
-    return {
-        "@context": "https://w3id.org/did/v1",
-        id: did,
-        publicKey: [{
-            id: keyId,
-            type: 'Ed25519VerificationKey2018',
-            controller: did,
-            publicKeyBase58: u8a.toString(pubKeyBytes, 'base58btc'),
-        },],
-        authentication: [keyId],
-        assertionMethod: [keyId],
-        capabilityDelegation: [keyId],
-        capabilityInvocation: [keyId],
-        keyAgreement: [{
-            id: x25519KeyId,
-            type: 'X25519KeyAgreementKey2019',
-            controller: did,
-            publicKeyBase58: u8a.toString(x25519PubBytes, 'base58btc'),
-        }]
-    }
+  const did = `did:key:${fingerprint}`
+  const keyId = `${did}#${fingerprint}`
+  const x25519PubBytes = convertPublicKeyToX25519(pubKeyBytes)
+  const x25519KeyId = `${did}#${encodeKey(x25519PubBytes)}`
+  return {
+    id: did,
+    publicKey: [{
+      id: keyId,
+      type: 'Ed25519VerificationKey2018',
+      controller: did,
+      publicKeyBase58: u8a.toString(pubKeyBytes, 'base58btc'),
+    },],
+    authentication: [keyId],
+    assertionMethod: [keyId],
+    capabilityDelegation: [keyId],
+    capabilityInvocation: [keyId],
+    keyAgreement: [{
+      id: x25519KeyId,
+      type: 'X25519KeyAgreementKey2019',
+      controller: did,
+      publicKeyBase58: u8a.toString(x25519PubBytes, 'base58btc'),
+    }]
+  }
 }
--- packages/key-did-resolver/src/secp256k1.ts
+++ packages/key-did-resolver/src/secp256k1.ts
@@ -4,20 +4,19 @@ import * as u8a from 'uint8arrays'
  * Constructs the document based on the method key
  */
 export function keyToDidDoc (pubKeyBytes: Uint8Array, fingerprint: string): any {
-    const did = `did:key:${fingerprint}`
-    const keyId = `${did}#${fingerprint}`
-    return {
-        "@context": "https://w3id.org/did/v1",
-        id: did,
-        publicKey: [{
-            id: keyId,
-            type: 'Secp256k1VerificationKey2018',
-            controller: did,
-            publicKeyHex: u8a.toString(pubKeyBytes, "base16"),
-        },],
-        authentication: [keyId],
-        assertionMethod: [keyId],
-        capabilityDelegation: [keyId],
-        capabilityInvocation: [keyId],
-    }
+  const did = `did:key:${fingerprint}`
+  const keyId = `${did}#${fingerprint}`
+  return {
+    id: did,
+    publicKey: [{
+      id: keyId,
+      type: 'Secp256k1VerificationKey2018',
+      controller: did,
+      publicKeyHex: u8a.toString(pubKeyBytes, "base16"),
+    }],
+    authentication: [keyId],
+    assertionMethod: [keyId],
+    capabilityDelegation: [keyId],
+    capabilityInvocation: [keyId],
+  }
 }
