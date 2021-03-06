ced45ce7602e18b7a3e0bfb86afd34cfe6d58539
ukstv <sergey@ukstv.me>
2021-03-06 15:12:09 +0300

    chore(core): doc-class and proper generic letter for HandlersMap

--- packages/core/src/handlers-map.ts
+++ packages/core/src/handlers-map.ts
@@ -3,6 +3,9 @@ import { Caip10LinkDoctypeHandler } from '@ceramicnetwork/doctype-caip10-link-ha
 import { Doctype, DoctypeHandler } from '@ceramicnetwork/common';
 import { DiagnosticsLogger } from '@ceramicnetwork/logger';
 
+/**
+ * Container for doctype handlers. Maps doctype name to the handler instance.
+ */
 export class HandlersMap {
   private readonly handlers: Map<string, DoctypeHandler<Doctype>>;
 
@@ -11,15 +14,23 @@ export class HandlersMap {
       handlers || new Map().set('tile', new TileDoctypeHandler()).set('caip10-link', new Caip10LinkDoctypeHandler());
   }
 
-  get<A extends Doctype>(doctypeName: string): DoctypeHandler<A> {
+  /**
+   * Return doctype handler based on its name. Throw error if not found.
+   *
+   * @param doctypeName - name of the handler.
+   */
+  get<T extends Doctype>(doctypeName: string): DoctypeHandler<T> {
     const handler = this.handlers.get(doctypeName);
     if (handler) {
-      return handler as DoctypeHandler<A>;
+      return handler as DoctypeHandler<T>;
     } else {
       throw new Error(doctypeName + ' is not a valid doctype');
     }
   }
 
+  /**
+   * Add doctype handler to the collection.
+   */
   add<T extends Doctype>(doctypeHandler: DoctypeHandler<T>): HandlersMap {
     this.logger.debug(`Registered handler for ${doctypeHandler.name} doctype`);
     this.handlers.set(doctypeHandler.name, doctypeHandler);
