b6a8bd20504911f835acd3b6870f649b3300d9a6
ukstv <sergey@ukstv.me>
2021-03-16 18:08:41 +0300

    chore(core): Use vanilla lru map

--- packages/core/src/state-management/repository.ts
+++ packages/core/src/state-management/repository.ts
@@ -1,6 +1,5 @@
 import { DEFAULT_LOAD_DOCOPTS, Document } from '../document';
 import DocID from '@ceramicnetwork/docid';
-import { AsyncLruMap } from './async-lru-map';
 import { DocumentFactory } from './document-factory';
 import { DocOpts, DocState, DocStateHolder } from '@ceramicnetwork/common';
 import { PinStore } from '../store/pin-store';
@@ -9,22 +8,29 @@ import { NamedTaskQueue } from './named-task-queue';
 import { DiagnosticsLogger } from '@ceramicnetwork/logger';
 import { ExecutionQueue } from './execution-queue';
 import { RunningState } from './running-state';
+import { LRUMap } from 'lru_map';
+import { Subject } from 'rxjs';
 
 export class Repository {
   readonly loadingQ: NamedTaskQueue = new NamedTaskQueue();
   readonly executionQ: ExecutionQueue;
 
-  readonly #map: AsyncLruMap<Document>;
+  readonly feed$: Subject<DocState>
+
+  readonly #map: LRUMap<string, Document>;
   #documentFactory?: DocumentFactory;
   pinStore?: PinStore;
   #networkLoad?: NetworkLoad;
 
   constructor(limit: number, logger: DiagnosticsLogger) {
     this.executionQ = new ExecutionQueue(logger, (docId) => this.get(docId).then((doc) => doc.state$));
-    this.#map = new AsyncLruMap(limit, async (entry) => {
+    this.#map = new LRUMap(100);
+    this.#map.shift = function () {
+      const entry = LRUMap.prototype.shift.call(this);
       entry.value.complete();
-      // await this.executionQ.tasks.lanes.get(entry.value.id.toString()).onIdle();
-    });
+      return entry;
+    };
+    this.feed$ = new Subject();
   }
 
   // Ideally this would be provided in the constructor, but circular dependencies in our initialization process make this necessary for now
@@ -41,7 +47,7 @@ export class Repository {
     this.#networkLoad = networkLoad;
   }
 
-  async fromMemory(docId: DocID): Promise<Document | undefined> {
+  fromMemory(docId: DocID): Document | undefined {
     return this.#map.get(docId.toString());
   }
 
@@ -49,7 +55,7 @@ export class Repository {
     if (this.pinStore && this.#documentFactory) {
       const docState = await this.pinStore.stateStore.load(docId);
       if (docState) {
-        const runningState = new RunningState(docState)
+        const runningState = new RunningState(docState);
         const document = await this.#documentFactory.build(runningState);
         await this.add(document);
         return document;
@@ -72,7 +78,7 @@ export class Repository {
    */
   async load(docId: DocID, opts: DocOpts = {}): Promise<Document> {
     return this.loadingQ.run(docId.toString(), async () => {
-      const fromMemory = await this.fromMemory(docId);
+      const fromMemory = this.fromMemory(docId);
       if (fromMemory) return fromMemory;
       const fromStateStore = await this.fromStateStore(docId);
       if (fromStateStore) return fromStateStore;
@@ -84,7 +90,7 @@ export class Repository {
    * Checks if we can get the document state without having to load it via pubsub (i.e. we have the document state in our in-memory cache or in the state store)
    */
   async has(docId: DocID): Promise<boolean> {
-    const fromMemory = await this.fromMemory(docId);
+    const fromMemory = this.fromMemory(docId);
     if (fromMemory) return true;
     const fromState = await this.pinStore.stateStore.load(docId);
     return Boolean(fromState);
@@ -96,7 +102,7 @@ export class Repository {
    */
   async get(docId: DocID): Promise<Document | undefined> {
     return this.loadingQ.run(docId.toString(), async () => {
-      const fromMemory = await this.fromMemory(docId);
+      const fromMemory = this.fromMemory(docId);
       if (fromMemory) return fromMemory;
       return this.fromStateStore(docId);
     });
@@ -106,7 +112,7 @@ export class Repository {
    * Return a document state, either from cache or from state store.
    */
   async docState(docId: DocID): Promise<DocState | undefined> {
-    const fromMemory = await this.#map.get(docId.toString());
+    const fromMemory = this.#map.get(docId.toString());
     if (fromMemory) {
       return fromMemory.state;
     } else {
@@ -119,8 +125,8 @@ export class Repository {
   /**
    * Stub for adding the document.
    */
-  async add(document: Document): Promise<void> {
-    await this.#map.set(document.id.toString(), document);
+  add(document: Document): void {
+    this.#map.set(document.id.toString(), document);
   }
 
   pin(docStateHolder: DocStateHolder): Promise<void> {
@@ -146,12 +152,10 @@ export class Repository {
   async close(): Promise<void> {
     await this.loadingQ.close();
     await this.executionQ.close();
-    await Promise.all(
-      Array.from(this.#map).map(async ([id, document]) => {
-        await this.#map.delete(id);
-        document.complete();
-      }),
-    );
+    Array.from(this.#map).forEach(([id, document]) => {
+      this.#map.delete(id);
+      document.complete();
+    });
     await this.pinStore.close();
   }
 }
