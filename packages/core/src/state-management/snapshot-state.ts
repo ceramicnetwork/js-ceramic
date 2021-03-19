aefff69a92bfb66134abe51156021241da458212
ukstv <sergey@ukstv.me>
2021-03-19 16:12:32 +0300

    chore(core): Comment for SnapshotState

--- packages/core/src/state-management/snapshot-state.ts
+++ packages/core/src/state-management/snapshot-state.ts
@@ -3,6 +3,11 @@ import { RunningStateLike } from './running-state';
 import { DocState } from '@ceramicnetwork/common';
 import { DocID } from '@ceramicnetwork/docid';
 
+/**
+ * Snapshot of a document state at some commit. Unlike `RunningState` this can not be updated.
+ * Only a subset of operations could be performed with an instance of SnapshotState, like
+ * `StateManager#rewind` or `StateManager#atTime`.
+ */
 export class SnapshotState extends Observable<DocState> implements RunningStateLike {
   readonly id: DocID;
   readonly state: DocState;
