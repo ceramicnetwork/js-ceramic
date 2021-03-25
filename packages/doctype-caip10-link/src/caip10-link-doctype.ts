074c315404becf427052a76e19f69f66c326c485
ukstv <sergey@ukstv.me>
2021-03-25 17:30:32 +0300

    chore(core): remove eslint silencer

--- packages/doctype-caip10-link/src/caip10-link-doctype.ts
+++ packages/doctype-caip10-link/src/caip10-link-doctype.ts
@@ -67,7 +67,7 @@ export class Caip10LinkDoctype extends Doctype {
             throw new Error('Exactly one controller must be specified')
         }
 
-        const [, linkedChainId] = metadata.controllers[0].split('@') // eslint-disable-line @typescript-eslint/no-unused-vars
+        const [, linkedChainId] = metadata.controllers[0].split('@');
         if (!linkedChainId) {
             throw new Error('Chain ID must be specified according to CAIP-10')
         }
