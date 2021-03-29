24404c16ca2af6c71b2f69f86883ea4c86ddd09e
ukstv <sergey@ukstv.me>
2021-03-29 22:22:07 +0300

    chore(core): StateCache set-endure symmetry and doc-comments

--- packages/core/src/state-management/state-cache.ts
+++ packages/core/src/state-management/state-cache.ts
@@ -1,9 +1,17 @@
 import { LRUMap } from 'lru_map';
 
+/**
+ * Cache for RunningStates. Two buckets here: volatile and durable. Volatile is a LRUMap with a definite limit.
+ * Durable bucket is just a simple map. One is expected to free a durable value when it is no longer needed.
+ */
 export class StateCache<T> implements Iterable<[string, T]> {
   readonly volatile: LRUMap<string, T>;
   readonly durable: Map<string, T>;
 
+  /**
+   * @param limit - LRUMap limit
+   * @param onEvicted - what to do when an item is evicted from volatile cache.
+   */
   constructor(limit: number, private readonly onEvicted?: (item: T) => void) {
     this.volatile = new LRUMap(limit);
     this.volatile.shift = function () {
@@ -14,32 +22,40 @@ export class StateCache<T> implements Iterable<[string, T]> {
     this.durable = new Map();
   }
 
+  /**
+   * Retrieve value, be it durable or volatile.
+   */
   get(key: string): T | undefined {
-    const fromPersistent = this.durable.get(key);
-    if (fromPersistent) {
-      return fromPersistent;
-    } else {
-      return this.volatile.get(key);
-    }
+    return this.durable.get(key) || this.volatile.get(key);
   }
 
+  /**
+   * Set value to volatile bucket. Remove from durable.
+   */
   set(key: string, value: T): void {
-    if (this.durable.has(key)) {
-      this.durable.set(key, value);
-    }
     this.volatile.set(key, value);
+    this.durable.delete(key);
   }
 
+  /**
+   * Delete value from both volatile and durable buckers.
+   */
   delete(key: string) {
     this.durable.delete(key);
     this.volatile.delete(key);
   }
 
+  /**
+   * Set value to durable bucket. Remove from volatile.
+   */
   endure(key: string, value: T): void {
     this.durable.set(key, value);
     this.volatile.delete(key);
   }
 
+  /**
+   * Move value from durable to volatile bucket
+   */
   free(key: string) {
     const entry = this.durable.get(key);
     if (entry) {
@@ -48,6 +64,9 @@ export class StateCache<T> implements Iterable<[string, T]> {
     }
   }
 
+  /**
+   * Iterator over entries.
+   */
   *entries(): Generator<[string, T]> {
     for (const entry of this.durable.entries()) {
       yield entry;
@@ -57,12 +76,18 @@ export class StateCache<T> implements Iterable<[string, T]> {
     }
   }
 
+  /**
+   * Iterator over keys.
+   */
   *keys(): Generator<string> {
     for (const entry of this.entries()) {
       yield entry[0];
     }
   }
 
+  /**
+   * Iterator over values.
+   */
   *values(): Generator<T> {
     for (const entry of this.entries()) {
       yield entry[1];
