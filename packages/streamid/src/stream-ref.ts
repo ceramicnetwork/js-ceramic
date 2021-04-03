4b6d2822bee16a84f0a71ec70065fedd51477b7b
Joel Thorstensson <joel@oed.earth>
2021-04-03 15:45:01 +0200

    feat(streamid): Rename DocID to StreamID

--- packages/streamid/src/stream-ref.ts
+++ packages/streamid/src/stream-ref.ts
@@ -1,17 +1,17 @@
 import CID from 'cids';
-import { DocID } from './doc-id';
+import { StreamID } from './stream-id';
 import { CommitID } from './commit-id';
 import * as uint8arrays from 'uint8arrays'
 
 /**
- * Ensure there is some resemblance in CommitID and DocID APIs.
+ * Ensure there is some resemblance in CommitID and StreamID APIs.
  */
-export interface DocRef {
+export interface StreamRef {
   readonly type: number;
   readonly typeName: string;
   readonly cid: CID;
   readonly bytes: Uint8Array;
-  readonly baseID: DocID;
+  readonly baseID: StreamID;
   atCommit(commit: CID | string | number): CommitID;
   equals(other: this): boolean;
   toString(): string;
@@ -38,32 +38,32 @@ function complain(message: string): never {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
-export namespace DocRef {
+export namespace StreamRef {
   /**
-   * Try to parse `input` into CommitID or DocID.
+   * Try to parse `input` into CommitID or StreamID.
    */
   // eslint-disable-next-line no-inner-declarations
-  export function from(input: DocID | CommitID | string | Uint8Array): DocID | CommitID {
-    if (input instanceof DocID) {
+  export function from(input: StreamID | CommitID | string | Uint8Array): StreamID | CommitID {
+    if (input instanceof StreamID) {
       return input;
     } else if (input instanceof CommitID) {
       return input;
     } else if (input instanceof Uint8Array) {
-      // Lazy computation: try CommitID, then DocID, then complain
+      // Lazy computation: try CommitID, then StreamID, then complain
       return (
         tryCatch(() => CommitID.fromBytes(input)) ||
-        tryCatch(() => DocID.fromBytes(input)) ||
-        complain(`Can not build CommitID or DocID from bytes ${uint8arrays.toString(input, 'base36')}`)
+        tryCatch(() => StreamID.fromBytes(input)) ||
+        complain(`Can not build CommitID or StreamID from bytes ${uint8arrays.toString(input, 'base36')}`)
       );
     } else if (typeof input === 'string') {
-      // Lazy computation: try CommitID, then DocID, then complain
+      // Lazy computation: try CommitID, then StreamID, then complain
       return (
         tryCatch(() => CommitID.fromString(input)) ||
-        tryCatch(() => DocID.fromString(input)) ||
-        complain(`Can not build CommitID or DocID from string ${input}`)
+        tryCatch(() => StreamID.fromString(input)) ||
+        complain(`Can not build CommitID or StreamID from string ${input}`)
       );
     } else {
-      throw new Error(`Can not build CommitID or DocID`)
+      throw new Error(`Can not build CommitID or StreamID`)
     }
   }
 }
