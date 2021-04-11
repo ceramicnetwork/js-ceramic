f518045fe0ed98c9a9eb41386f0e39e0f4b6606f
Spencer T Brody <spencer@3box.io>
2021-04-11 14:31:41 -0400

    stabilize test

--- packages/cli/src/__tests__/ceramic-multi-daemon.test.ts
+++ packages/cli/src/__tests__/ceramic-multi-daemon.test.ts
@@ -70,69 +70,55 @@ const makeCeramicCore = async(ipfs: IpfsApi, stateStoreDirectory: string): Promi
 }
 
 describe('Ceramic interop between multiple daemons and http clients', () => {
-    jest.setTimeout(30000)
     let ipfs1: IpfsApi
     let ipfs2: IpfsApi
-    let ipfs3: IpfsApi
     let tmpFolder1: any
     let tmpFolder2: any
-    let tmpFolder3: any
     let core1: Ceramic
     let core2: Ceramic
-    let core3: Ceramic
     let daemon1: CeramicDaemon
     let daemon2: CeramicDaemon
-    let daemon3: CeramicDaemon
     let client1: CeramicClient
     let client2: CeramicClient
-    let client3: CeramicClient
 
     beforeAll(async () => {
         tmpFolder1 = await tmp.dir({ unsafeCleanup: true });
         tmpFolder2 = await tmp.dir({ unsafeCleanup: true });
-        tmpFolder3 = await tmp.dir({ unsafeCleanup: true });
 
-        [ipfs1, ipfs2, ipfs3] = await Promise.all([tmpFolder1, tmpFolder2, tmpFolder3].map((tmpFolder) => createIPFS(tmpFolder)));
+        [ipfs1, ipfs2] = await Promise.all([tmpFolder1, tmpFolder2].map((tmpFolder) => createIPFS(tmpFolder)));
 
-        // Make sure nodes 2 and 3 can load updates from node 1
+        // Make sure the nodes can talk to each other
         await swarmConnect(ipfs1, ipfs2)
-        await swarmConnect(ipfs1, ipfs3)
     })
 
     afterAll(async () => {
-        await Promise.all([ipfs1.stop(), ipfs2.stop(), ipfs3.stop()])
-        await Promise.all([tmpFolder1.clean(), tmpFolder2.cleanup(), tmpFolder3.cleanup()])
+        await Promise.all([ipfs1.stop(), ipfs2.stop()])
+        await Promise.all([tmpFolder1.clean(), tmpFolder2.cleanup()])
     })
 
     beforeEach(async () => {
         core1 = await makeCeramicCore(ipfs1, tmpFolder1.path)
         core2 = await makeCeramicCore(ipfs2, tmpFolder1.path)
-        core3 = await makeCeramicCore(ipfs3, tmpFolder1.path)
         const port1 = await getPort()
         const port2 = await getPort()
-        const port3 = await getPort()
         daemon1 = new CeramicDaemon(core1, { port: port1 })
         daemon2 = new CeramicDaemon(core2, { port: port2 })
-        daemon3 = new CeramicDaemon(core3, { port: port3 })
         client1 = new CeramicClient('http://localhost:' + port1, { docSyncInterval: 500 })
         client2 = new CeramicClient('http://localhost:' + port2, { docSyncInterval: 500 })
-        client3 = new CeramicClient('http://localhost:' + port3, { docSyncInterval: 500 })
 
         await core1.setDID(makeDID(seed, core1))
         await client1.setDID(makeDID(seed, client1))
         await core2.setDID(makeDID(seed, core2))
         await client2.setDID(makeDID(seed, client2))
-        await core3.setDID(makeDID(seed, core3))
-        await client3.setDID(makeDID(seed, client3))
     })
 
     afterEach(async () => {
-        await Promise.all([client1.close(), client2.close(), client3.close()])
-        await Promise.all([daemon1.close(), daemon2.close(), daemon3.close()])
-        await Promise.all([core1.close(), core2.close(), core3.close()])
+        await Promise.all([client1.close(), client2.close()])
+        await Promise.all([daemon1.close(), daemon2.close()])
+        await Promise.all([core1.close(), core2.close()])
     })
 
-    it('respects sync DocOpts', async () => {
+    it("doesn't sync if sync: false", async () => {
         const initialContent = {test: 123}
         const updatedContent = {test: 456}
         // Create a document with updates on the first node so that the updates aren't visible
@@ -146,9 +132,18 @@ describe('Ceramic interop between multiple daemons and http clients', () => {
         // TODO uncomment when sync() is changed to always force sync
         //await doc2.sync()
         //expect(doc2.content).toEqual(updatedContent)
+    })
+
+    it("does sync if sync: true", async () => {
+        const initialContent = {test: 123}
+        const updatedContent = {test: 456}
+        // Create a document with updates on the first node so that the updates aren't visible
+        // on node 2 and 3 without querying pubsub
+        const doc1 = await TileDoctype.create(core1, initialContent, null, {anchor: false});
+        await doc1.update(updatedContent)
 
         // Loading the doc with sync:true should get the current contents
-        const doc3 = await TileDoctype.load(client3, doc1.id, {sync: true})
-        expect(doc3.content).toEqual(updatedContent)
+        const doc2 = await TileDoctype.load(client2, doc1.id, {sync: true})
+        expect(doc2.content).toEqual(updatedContent)
     })
 })
