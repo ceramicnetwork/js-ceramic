dfef4e73a37a65a00dd2149fe9f544c6278e7568
val <8445610+valmack@users.noreply.github.com>
2021-04-28 17:52:50 -0400

    feat(cli): allow daemon hostname to be configured

--- packages/cli/src/ceramic-daemon.ts
+++ packages/cli/src/ceramic-daemon.ts
@@ -22,6 +22,7 @@ import { addAsync, ExpressWithAsync } from '@awaitjs/express'
 import { logRequests } from './daemon/log-requests';
 import type { Server } from 'http';
 
+const DEFAULT_HOSTNAME = 'localhost'
 const DEFAULT_PORT = 7007
 const toApiPath = (ending: string): string => '/api/v0' + ending
 
@@ -31,6 +32,7 @@ const toApiPath = (ending: string): string => '/api/v0' + ending
 export interface CreateOpts {
   ipfsHost?: string;
   port?: number;
+  hostname?: string;
   corsAllowedOrigins?: string | RegExp[];
 
   ethereumRpcUrl?: string;
@@ -137,8 +139,9 @@ class CeramicDaemon {
   async listen(): Promise<void> {
     return new Promise<void>((resolve) => {
       const port = this.opts.port || DEFAULT_PORT
-      this.server = this.app.listen(port, () => {
-        this.diagnosticsLogger.imp('Ceramic API running on port ' + port)
+      const hostname = this.opts.hostname || DEFAULT_HOSTNAME
+      this.server = this.app.listen(port, hostname, () => {
+        this.diagnosticsLogger.imp(`Ceramic API running on ${hostname}:${port}'`)
         resolve()
       })
       this.server.keepAliveTimeout = 60 * 1000
