db80b30d3fadacd4f984992fc8ae25eea937283a
ukstv <sergey@ukstv.me>
2021-05-07 14:34:01 +0300

    chore: Bump powergate client

--- packages/pinning-powergate-backend/src/index.ts
+++ packages/pinning-powergate-backend/src/index.ts
@@ -4,24 +4,14 @@ import type {
     PinningBackend,
     PinningInfo,
 } from "@ceramicnetwork/common";
-import { createPow, Pow, ffsTypes } from "@textile/powergate-client";
+import { createPow, Pow } from "@textile/powergate-client";
 import * as sha256 from "@stablelib/sha256";
 import * as base64 from "@stablelib/base64";
-import * as _ from "lodash";
 
 export class EmptyTokenError extends Error {
     constructor(address: string) {
         super(`No token provided for Powergate at ${address}`);
     }
-}
-
-export enum JobStatus {
-    JOB_STATUS_UNSPECIFIED = 0,
-    JOB_STATUS_QUEUED = 1,
-    JOB_STATUS_EXECUTING = 2,
-    JOB_STATUS_FAILED = 3,
-    JOB_STATUS_CANCELED = 4,
-    JOB_STATUS_SUCCESS = 5,
 }
 
 const textEncoder = new TextEncoder();
@@ -71,7 +61,7 @@ export class PowergatePinningBackend implements PinningBackend {
     async pin(cid: CID): Promise<void> {
         if (this.#pow) {
             try {
-                await this.#pow.ffs.pushStorageConfig(cid.toString(), {
+                await this.#pow.storageConfig.apply(cid.toString(), {
                     override: true,
                 });
             } catch (e) {
@@ -86,55 +76,17 @@ export class PowergatePinningBackend implements PinningBackend {
 
     async unpin(cid: CID): Promise<void> {
         if (this.#pow) {
-            const { config } = await this.#pow.ffs.getStorageConfig(cid.toString());
-            if (config) {
-                const next = Object.assign({}, config, {
-                    ...config, repairable: false, hot: {
-                        ...config.hot, allowUnfreeze: false, enabled: false,
-                    }, cold: {
-                        ...config.cold, enabled: false,
-                    },
-                });
-                const { jobId } = await this.#pow.ffs.pushStorageConfig(cid.toString(), {
-                    override: true, storageConfig: next,
-                });
-                await this.waitForJobStatus(jobId, JobStatus.JOB_STATUS_SUCCESS);
-                await this.#pow.ffs.remove(cid.toString());
-            }
-        }
-    }
-
-    protected async waitForJobStatus(jobId: string, status: ffsTypes.JobStatusMap[keyof ffsTypes.JobStatusMap]): Promise<void> {
-        if (this.#pow) {
-            const pow = this.#pow;
-            return new Promise<void>((resolve, reject) => {
-                const cancel = pow.ffs.watchJobs((job: any) => {
-                    if (job.errCause && job.errCause.length > 0) {
-                        reject(new Error(job.errCause));
-                    }
-                    if (job.status === JobStatus.JOB_STATUS_CANCELED) {
-                        reject(new Error("job canceled"));
-                    }
-                    if (job.status === JobStatus.JOB_STATUS_FAILED) {
-                        reject(new Error("job failed"));
-                    }
-                    if (job.status === status) {
-                        cancel();
-                        resolve();
-                    }
-                }, jobId);
-            });
+            await this.#pow.storageConfig.remove(cid.toString())
         }
     }
 
     async ls(): Promise<CidList> {
         if (this.#pow) {
-            const { info } = await this.#pow.ffs.info();
-            if (info) {
-                const cids = info.pinsList;
+            const { storageInfoList } = await this.#pow.storageInfo.list();
+            if (storageInfoList) {
                 const result: CidList = {};
-                cids.forEach((cid) => {
-                    result[cid] = [this.id];
+                storageInfoList.forEach((info) => {
+                    result[info.cid] = [this.id];
                 });
                 return result;
             } else {
@@ -146,15 +98,8 @@ export class PowergatePinningBackend implements PinningBackend {
     }
 
     async info(): Promise<PinningInfo> {
-        let info: any = {};
-        if (this.#pow) {
-            const { info: ffsInfo } = await this.#pow.ffs.info();
-            if (ffsInfo) {
-                info = _.omit(ffsInfo, "pinsList");
-            }
-        }
         return {
-            [this.id]: info,
+            [this.id]: {},
         };
     }
 }
