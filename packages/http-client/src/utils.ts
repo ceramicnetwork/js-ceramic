ad555c91c21e05cf9a0bd793f6e35f0854ea094c
Spencer T Brody <spencer@3box.io>
2021-05-12 14:08:24 -0400

    fix (core,http-client): Don't parse failed HTTP responses

--- packages/http-client/src/utils.ts
+++ packages/http-client/src/utils.ts
@@ -1,24 +1,4 @@
-import fetch from "cross-fetch"
 import StreamID from "@ceramicnetwork/streamid"
-
-interface FetchOpts {
-  body?: any
-  method?: string
-}
-
-export async function fetchJson(url: string, opts: FetchOpts = {}): Promise<any> {
-    if (opts.body) {
-        Object.assign(opts, {
-            body: JSON.stringify(opts.body),
-            headers: { 'Content-Type': 'application/json' }
-        })
-    }
-    const res = await fetch(url, opts).then(response => response.json())
-    if (res.error) {
-        throw new Error(res.error)
-    }
-    return res
-}
 
 export function typeStreamID(streamId: StreamID | string): StreamID  {
     return (typeof streamId === 'string') ? StreamID.fromString(streamId) : streamId
