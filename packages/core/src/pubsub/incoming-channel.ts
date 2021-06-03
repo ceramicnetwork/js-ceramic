4f5d1df0031ada2c8d2c8a31addd196180531137
Joel Thorstensson <joel@oed.earth>
2021-06-03 11:38:56 +0100

    fix: retry pubsub connection if ipfs node is unreachable

--- packages/core/src/pubsub/incoming-channel.ts
+++ packages/core/src/pubsub/incoming-channel.ts
@@ -69,19 +69,30 @@ export class IncomingChannel extends Observable<IPFSPubsubMessage> {
   }
 
   private async subscribeToIpfs(handler: (message: IPFSPubsubMessage) => void, complete: () => void): Promise<void> {
-    const isRunning = this.ipfs && this.ipfs.pubsub && this.ipfs.pubsub.ls;
+    const isRunning = this.ipfs && this.ipfs.pubsub;
     if (isRunning) {
       const onError = (error) => {
         console.warn(`IPFS pubsub subscription error, resubscribing: ${error}`)
         this.tasks.add(() => this.subscribeToIpfs(handler, complete));
       }
+      try {
+        // For some reason ipfs.id() throws an error directly if the
+        // ipfs node can't be reached, while pubsub.subscribe stalls
+        // for an unknown amount of time. We therefor run ipfs.id()
+        // first to determine if the ipfs node is reachable.
+        const ipfsId = await this.ipfs.id();
+        const peerId = ipfsId.id;
 
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      await this.ipfs.pubsub.subscribe(this.topic, handler, { onError });
-      const ipfsId = await this.ipfs.id();
-      const peerId = ipfsId.id;
-      this.pubsubLogger.log({ peer: peerId, event: 'subscribed', topic: this.topic });
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        await this.ipfs.pubsub.subscribe(this.topic, handler, { onError });
+        this.pubsubLogger.log({ peer: peerId, event: 'subscribed', topic: this.topic });
+      } catch (error) {
+        console.warn(`Can not reach ipfs node, resubscribing: ${error}`)
+        // TODO - should probably have some sort of backoff mechanism here.
+        await new Promise(resolve => setTimeout(resolve, 5000))
+        this.tasks.add(() => this.subscribeToIpfs(handler, complete));
+      }
     } else {
       complete();
     }
