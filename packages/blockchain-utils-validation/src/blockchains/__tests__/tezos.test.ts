c594f1759588e25a5580893b8ce74e9dbd78b54f
Geo25rey <gcasper42@gmail.com>
2021-07-17 19:59:57 -0400

    feat(tezos-blockchain-link): initial support for linking and validating DIDs to Tezos wallets
    - implements the `AuthProvider` interface for Tezos
    - adds a `BlockchainHandler` for Tezos
    - the Tezos `LinkProof` validator uses a RPC client to try to resolve the public key associated with the  wallet address (AKA the public key hash)
    - relevant tests are added, I can add more if needed
    - the `AuthProvider` implementation uses the `WalletProvider` and `Signer` interfaces from [`@taquito/*` packages](https://tezostaquito.io/typedoc/modules.html)

---
+++ packages/blockchain-utils-validation/src/blockchains/__tests__/tezos.test.ts
@@ -0,0 +1,171 @@
+import { TezosAuthProvider, TezosProvider } from '../../../../blockchain-utils-linking/src/tezos' //'@ceramicnetwork/blockchain-utils-linking'
+import { InMemorySigner } from '@taquito/signer'
+import {
+  WalletDelegateParams,
+  WalletOriginateParams,
+  WalletTransferParams,
+  WalletProvider,
+} from '@taquito/taquito'
+import { validateLink } from '../tezos'
+import mockAxios from 'jest-mock-axios'
+import { LinkProof } from '../../../../blockchain-utils-linking/src/util'
+import { HttpResponse } from 'jest-mock-axios/dist/lib/mock-axios-types'
+
+const did = 'did:3:bafysdfwefwe'
+const privateKey = 'p2sk2obfVMEuPUnadAConLWk7Tf4Dt3n4svSgJwrgpamRqJXvaYcg1'
+const chainRef = 'NetXdQprcVkpaWU' // Tezos mainnet
+
+type IoTestCase = {
+  testName?: string
+  error?: any
+  pubkeyObject?(publicKey: string): HttpResponse
+}
+
+type ProofTestCase = {
+  message: string
+  proof: () => LinkProof
+}
+
+// Mock Taquito's WalletProvider
+class TezosMockWallet implements WalletProvider {
+  constructor(private readonly signer: InMemorySigner) {}
+  async getPKH(): Promise<string> {
+    return this.signer.publicKeyHash()
+  }
+
+  async mapTransferParamsToWalletParams(_params: WalletTransferParams) {
+    throw new Error('Should not be called')
+  }
+
+  async mapOriginateParamsToWalletParams(_params: WalletOriginateParams) {
+    throw new Error('Should not be called')
+  }
+
+  async mapDelegateParamsToWalletParams(_params: WalletDelegateParams) {
+    throw new Error('Should not be called')
+  }
+
+  async sendOperations(_params: any[]): Promise<string> {
+    throw new Error('Should not be called')
+  }
+}
+
+let provider: TezosProvider
+let publicKeyHash: string
+let publicKey: string
+let validProof: LinkProof
+let invalidSignatureProof: LinkProof
+
+// cache Date.now() to restore it after all tests
+const dateNow = Date.now
+
+beforeAll(async (done) => {
+  // Mock Date.now() to return a constant value
+  Date.now = () => 666000
+
+  // initialize mock tezos provider
+  const signer = await InMemorySigner.fromSecretKey(privateKey)
+  provider = {
+    signer,
+    wallet: new TezosMockWallet(signer),
+  }
+
+  // create proof for did
+  publicKeyHash = await provider.signer.publicKeyHash()
+  publicKey = await provider.signer.publicKey()
+  const authProvider = new TezosAuthProvider(provider, chainRef)
+  validProof = await authProvider.createLink(did)
+  invalidSignatureProof = Object.assign({}, validProof)
+  invalidSignatureProof.signature = 'invalid'
+  done()
+})
+
+afterEach(() => {
+  mockAxios.reset()
+})
+
+afterAll(() => {
+  // restore Date.now()
+  Date.now = dateNow
+})
+
+describe('Blockchain: Tezos', () => {
+  describe('validateLink', () => {
+    // create test cases
+    const ioTestCases: IoTestCase[] = [
+      // Validation not possible
+      // - public key hash never used the blockchain
+      // - public key can't be found from non-existant wallet (on the blockchain)
+      // - signature can't be verified
+      {
+        testName: 'unable to validate when wallet address has never interacted with the blockchain',
+        error: new Error('Axios 404 response status error'), // use axios 404 error
+      },
+
+      // Validation not possible
+      // - public key is not published
+      // - signature can not be verified
+      {
+        testName: 'unable to validate when wallet address has not been published to the blockchain',
+        pubkeyObject(): HttpResponse {
+          return {
+            data: {
+              pubkey: undefined,
+            },
+          }
+        },
+      },
+
+      // Able to test validation
+      // - the public key is published/found on the blockchain
+      // - the signature can be verified
+      {
+        pubkeyObject(publicKey?: string): HttpResponse {
+          return {
+            data: {
+              pubkey: publicKey,
+            },
+          }
+        },
+      },
+    ]
+
+    const proofTestCases: ProofTestCase[] = [
+      // valid proof
+      {
+        message: 'valid proof',
+        proof: () => validProof,
+      },
+      // invalid proof
+      {
+        message: 'invalid proof',
+        proof: () => invalidSignatureProof,
+      },
+    ]
+
+    // run test cases
+    for (const { testName, pubkeyObject, error } of ioTestCases) {
+      for (const { message, proof } of proofTestCases) {
+        test(testName || message, async (done) => {
+          // start test with the proof from the test case
+          const testPromise = expect(validateLink(proof())).resolves.toMatchSnapshot()
+
+          // mock axios response or error
+          if (pubkeyObject) {
+            mockAxios.mockResponse(pubkeyObject(publicKey))
+          }
+          if (error) {
+            mockAxios.mockError(error)
+          }
+          expect(mockAxios.get).toHaveBeenCalledWith(
+            `https://api.tzstats.com/explorer/account/${publicKeyHash}`
+          )
+
+          // wait for the test to finish
+          await testPromise
+          done()
+        })
+      }
+    }
+  })
+})
