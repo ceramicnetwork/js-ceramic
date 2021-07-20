cfcdb45a1a2ed22130a104584a43f56365c94a3b
Geo25rey <gcasper42@gmail.com>
2021-07-19 20:10:48 -0400

    implemented fixes requested in PR

--- packages/blockchain-utils-validation/src/blockchains/tezos.ts
+++ packages/blockchain-utils-validation/src/blockchains/tezos.ts
@@ -1,9 +1,9 @@
 import { AccountID } from 'caip'
 import { RemoteSigner } from '@taquito/remote-signer'
-import { char2Bytes } from '@taquito/utils'
 import { BlockchainHandler } from '../blockchain-handler'
 import { LinkProof } from '@ceramicnetwork/blockchain-utils-linking'
-import axios from 'axios'
+import fetch from 'cross-fetch'
+import * as uint8arrays from 'uint8arrays'
 
 export const ADDRESS_NOT_FOUND_ERROR = new Error(`Address not found on the Tezos blockchain`)
 export const PUBLIC_KEY_NOT_PUBLISHED_ERROR = new Error(
@@ -13,6 +13,18 @@ export const PUBLIC_KEY_NOT_PUBLISHED_ERROR = new Error(
 const namespace = 'tezos'
 
 const FAKE_API_ENDPOINT = 'https://fake-api.example.com' // this enpoint is never actually called
+
+// should be the same as @taquito/utils char2Bytes()
+// exported for testing to ensure above
+/**
+ * converts a utf8 string to a hex string
+ *
+ * @param {string} input - the string to convert
+ * @returns {string} the converted hex string
+ */
+export function char2Bytes(input: string): string {
+  return uint8arrays.toString(uint8arrays.fromString(input, 'utf8'), 'base16')
+}
 
 /**
  * creates a function which attempts to resolve the public key associated with the wallet address
@@ -24,11 +36,11 @@ const FAKE_API_ENDPOINT = 'https://fake-api.example.com' // this enpoint is neve
  */
 function publicKeyFinder(address: string): () => Promise<string> {
   // request the public key from the Tezos blockchain
-  const request = axios.get(`https://api.tzstats.com/explorer/account/${address}`).catch(() => {
+  const request = fetch(`https://api.tzstats.com/explorer/account/${address}`).catch(() => {
     throw ADDRESS_NOT_FOUND_ERROR
   })
   return async (): Promise<string> => {
-    const result = (await (await request).data)?.pubkey
+    const result = (await (await request).json())?.pubkey
     if (result) {
       return result
     } else {
