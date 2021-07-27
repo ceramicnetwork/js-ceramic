baa6c35a6a72f69380597cd260f14bc227c4fd66
Geoffrey Casper <gcasper42@gmail.com>
2021-07-27 09:56:45 -0400

    feat(tezos-blockchain-link): initial support for linking and validating DIDs to Tezos wallets (#1555)
    * feat(tezos-blockchain-link): initial support for linking and validating DIDs to Tezos wallets
    
    - implements the `AuthProvider` interface for Tezos
    - adds a `BlockchainHandler` for Tezos
    - the Tezos `LinkProof` validator uses a RPC client to try to resolve the public key associated with the  wallet address (AKA the public key hash)
    - relevant tests are added, I can add more if needed
    - the `AuthProvider` implementation uses the `WalletProvider` and `Signer` interfaces from [`@taquito/*` packages](https://tezostaquito.io/typedoc/modules.html)
    
    * implemented fixes requested in PR
    
    * fix: use taquito-remote-signer with stablelib
    
    * Cleanup `getActiveAddress`
    
    Co-authored-by: Sergey Ukustov <sergey@ukstv.me>
    
    * Remove `@taquito/utils` dev dep
    
    * Use proper package names instead of relative pathing for imports
    
    Co-authored-by: Sergey Ukustov <sergey@ukstv.me>

---
+++ packages/blockchain-utils-linking/src/tezos.ts
@@ -0,0 +1,120 @@
+import { AccountID } from 'caip'
+import { AuthProvider } from './auth-provider'
+import { getConsentMessage, LinkProof } from './util'
+import { hash } from '@stablelib/sha256'
+import * as uint8arrays from 'uint8arrays'
+import type { Signer, TezosToolkit, Wallet } from '@taquito/taquito'
+
+export type TezosProvider = TezosToolkit & {
+  // the below are requried for the TezosProvider interface
+  wallet: Wallet
+  signer: Signer
+}
+
+// should be the same as @taquito/utils char2Bytes()
+// exported for testing to ensure above
+/**
+ * converts a utf8 string to a hex string
+ *
+ * @internal
+ *
+ * @param {string} input - the string to convert
+ * @returns {string} the converted hex string
+ */
+function char2Bytes(input: string): string {
+  return uint8arrays.toString(uint8arrays.fromString(input, 'utf8'), 'base16')
+}
+
+/**
+ * signs a message with the active address and returns the signature
+ *
+ * @param {TezosProvider} provider - the provider to use for signing
+ * @param {string} message - the message to sign
+ * @returns {Promise<string>} the signature prefixed with the signing method
+ */
+async function sign(provider: TezosProvider, message: string): Promise<string> {
+  const Tezos = provider
+  // sign the message with the active address and get the signature with the type prefixed
+  const { prefixSig: signature } = await Tezos.signer.sign(char2Bytes(message))
+  return signature
+}
+
+/**
+ * get the active address from the {TezosProvider}
+ *
+ * @param {TezosProvider} provider - the provider to use for getting the active account
+ * @returns {Promise<string>} - a promise that resolves to the active account's address
+ */
+async function getActiveAddress(provider: TezosProvider): Promise<string> {
+  const Tezos = provider
+return Tezos.wallet.pkh({ forceRefetch: true })
+}
+
+/**
+ * This is an implementation of {AuthProvider} for the Tezos network.
+ * It uses the Tezos RPC client to get the active account's address and sign the
+ * message with the active account's address.
+ *
+ * @param {TezosProvider} provider - the provider to use signing the link proof and getting the active account's address
+ * @param {string} address - the address to sign the link proof with
+ * @param {string} chainRef - the chain reference to link to
+ */
+export class TezosAuthProvider implements AuthProvider {
+  readonly isAuthProvider = true
+
+  constructor(private readonly provider: TezosProvider, private readonly chainRef: string) {}
+
+  /**
+   * @inheritdoc
+   */
+  async authenticate(message: string): Promise<string> {
+    const signature = await sign(this.provider, message)
+    const digest = hash(uint8arrays.fromString(signature))
+    return `0x${uint8arrays.toString(digest, 'base16')}`
+  }
+
+  /**
+   * @inheritdoc
+   */
+  async createLink(did: string): Promise<LinkProof> {
+    // get consent message
+    const { message, timestamp } = getConsentMessage(did)
+    // sign the message with the active address
+    const signature = await sign(this.provider, message)
+    const address = await getActiveAddress(this.provider)
+    // generate account ID
+    const caipAccount = new AccountID({
+      address,
+      chainId: `tezos:${this.chainRef}`,
+    })
+    // create link proof
+    const proof: LinkProof = {
+      version: 1,
+      message,
+      signature,
+      account: caipAccount.toString(),
+      timestamp,
+    }
+    return proof
+  }
+
+  /**
+   * @inheritdoc
+   */
+  async accountId(): Promise<AccountID> {
+    const address = await getActiveAddress(this.provider)
+    return new AccountID({
+      address,
+      chainId: `tezos:${this.chainRef}`,
+    })
+  }
+
+  /**
+   * This is unsupported
+   *
+   * @throws {Error} - always throws an error
+   */
+  withAddress(_address: string): AuthProvider {
+    throw new Error('TezosAuthProvider does not support withAddress')
+  }
+}
---
+++ packages/blockchain-utils-validation/src/blockchains/__tests__/tezos.test.ts
@@ -0,0 +1,183 @@
+import { TezosAuthProvider, TezosProvider } from '@ceramicnetwork/blockchain-utils-linking'
+import { InMemorySigner } from '@taquito/signer'
+import {
+  WalletDelegateParams,
+  WalletOriginateParams,
+  WalletTransferParams,
+  WalletProvider,
+  TezosToolkit,
+} from '@taquito/taquito'
+import { char2Bytes, validateLink } from '../tezos'
+import mockFetch from 'jest-fetch-mock'
+import { LinkProof } from '@ceramicnetwork/blockchain-utils-linking'
+import utils from '@taquito/utils'
+
+const did = 'did:3:bafysdfwefwe'
+const privateKey = 'p2sk2obfVMEuPUnadAConLWk7Tf4Dt3n4svSgJwrgpamRqJXvaYcg1'
+const chainRef = 'NetXdQprcVkpaWU' // Tezos mainnet
+
+type HttpResponse = string
+
+type IoTestCase = {
+  testName?: string
+  error?: any
+  pubkeyObject?(publicKey: string): HttpResponse
+}
+
+type ProofTestCase = {
+  message: string
+  proof: () => LinkProof
+}
+
+// Mock Taquito's WalletProvider
+class TezosMockWallet implements WalletProvider {
+  constructor(private readonly signer: InMemorySigner) {}
+  async getPKH(): Promise<string> {
+    return this.signer.publicKeyHash()
+  }
+
+  async mapTransferParamsToWalletParams(_params: WalletTransferParams) {
+    throw new Error('Should not be called')
+  }
+
+  async mapOriginateParamsToWalletParams(_params: WalletOriginateParams) {
+    throw new Error('Should not be called')
+  }
+
+  async mapDelegateParamsToWalletParams(_params: WalletDelegateParams) {
+    throw new Error('Should not be called')
+  }
+
+  async sendOperations(_params: any[]): Promise<string> {
+    throw new Error('Should not be called')
+  }
+}
+
+let provider: TezosProvider
+let publicKeyHash: string
+let publicKey: string
+let validProof: LinkProof
+let invalidSignatureProof: LinkProof
+
+// cache Date.now() to restore it after all tests
+const dateNow = Date.now
+
+beforeAll(async (done) => {
+  // Mock Date.now() to return a constant value
+  Date.now = () => 666000
+
+  // initialize mock tezos provider
+  const signer = await InMemorySigner.fromSecretKey(privateKey)
+  provider = new TezosToolkit('https://mainnet-tezos.giganode.io')
+  provider.setProvider({
+    signer,
+    wallet: new TezosMockWallet(signer),
+  })
+
+  // create proof for did
+  publicKeyHash = await provider.signer.publicKeyHash()
+  publicKey = await provider.signer.publicKey()
+  const authProvider = new TezosAuthProvider(provider, chainRef)
+  validProof = await authProvider.createLink(did)
+  invalidSignatureProof = Object.assign({}, validProof)
+  invalidSignatureProof.signature = 'invalid'
+  done()
+})
+
+afterEach(() => {
+  mockFetch.mockReset()
+})
+
+afterAll(() => {
+  // restore Date.now()
+  Date.now = dateNow
+})
+
+describe('Blockchain: Tezos', () => {
+  describe('validateLink', () => {
+    // create test cases
+    const ioTestCases: IoTestCase[] = [
+      // Validation not possible
+      // - public key hash never used the blockchain
+      // - public key can't be found from non-existant wallet (on the blockchain)
+      // - signature can't be verified
+      {
+        testName: 'unable to validate when wallet address has never interacted with the blockchain',
+        error: new Error('Axios 404 response status error'), // use axios 404 error
+      },
+
+      // Validation not possible
+      // - public key is not published
+      // - signature can not be verified
+      {
+        testName: 'unable to validate when wallet address has not been published to the blockchain',
+        pubkeyObject(): HttpResponse {
+          return JSON.stringify({
+            data: {
+              pubkey: undefined,
+            },
+          })
+        },
+      },
+
+      // Able to test validation
+      // - the public key is published/found on the blockchain
+      // - the signature can be verified
+      {
+        pubkeyObject(publicKey?: string): HttpResponse {
+          return JSON.stringify({
+            data: {
+              pubkey: publicKey,
+            },
+          })
+        },
+      },
+    ]
+
+    const proofTestCases: ProofTestCase[] = [
+      // valid proof
+      {
+        message: 'valid proof',
+        proof: () => validProof,
+      },
+      // invalid proof
+      {
+        message: 'invalid proof',
+        proof: () => invalidSignatureProof,
+      },
+    ]
+
+    // run test cases
+    for (const { testName, pubkeyObject, error } of ioTestCases) {
+      for (const { message, proof } of proofTestCases) {
+        test(testName || message, async (done) => {
+          // start test with the proof from the test case
+          const testPromise = expect(validateLink(proof())).resolves.toMatchSnapshot()
+
+          // mock axios response or error
+          if (pubkeyObject) {
+            mockFetch.mockResponse(pubkeyObject(publicKey))
+          }
+          if (error) {
+            mockFetch.mockReject(error)
+          }
+          expect(mockFetch).toHaveBeenCalledWith(
+            `https://api.tzstats.com/explorer/account/${publicKeyHash}`
+          )
+
+          // wait for the test to finish
+          await testPromise
+          done()
+        })
+      }
+    }
+  })
+
+  describe('char2Bytes', () => {
+    test('should match the @taquito/utils char2Bytes() implementation', () => {
+      // get a random string of length 32
+      const s = 'some random string. this should be fine.'
+      expect(char2Bytes(s)).toBe(utils.char2Bytes(s))
+    })
+  })
+})
