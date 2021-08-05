b1a578e6146faaf0d8e781be91774d1b14d15428
ukstv <sergey@ukstv.me>
2021-08-05 17:19:27 +0300

    feat: Remove rinkeby subgraphs

--- packages/cli/src/ceramic-daemon.ts
+++ packages/cli/src/ceramic-daemon.ts
@@ -186,10 +186,6 @@ export class CeramicDaemon {
         ...NftDidResolver.getResolver({
           ceramic: ceramic,
           subGraphUrls: {
-            'eip155:4': {
-              erc721: 'https://api.thegraph.com/subgraphs/name/sunguru98/erc721-rinkeby-subgraph',
-              erc1155: 'https://api.thegraph.com/subgraphs/name/sunguru98/erc1155-rinkeby-subgraph',
-            },
             'eip155:1': {
               erc1155: 'https://api.thegraph.com/subgraphs/name/sunguru98/mainnet-erc1155-subgraph',
               erc721: 'https://api.thegraph.com/subgraphs/name/sunguru98/mainnet-erc721-subgraph',
