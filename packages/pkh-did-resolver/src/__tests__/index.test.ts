3a09d1b24f37a5b264a9b8816d9b6a6afa848ffb
Joel Thorstensson <joel@oed.earth>
2021-08-06 19:58:15 +0100

    fix: tests

--- packages/pkh-did-resolver/src/__tests__/index.test.ts
+++ packages/pkh-did-resolver/src/__tests__/index.test.ts
@@ -1,8 +1,8 @@
 import index from '../index'
 import { Resolver } from 'did-resolver'
 
-const ethDid = 'eip155:1:0xab16a96d359ec26a11e2c2b3d8f8b8942d5bfcdb'
-const btcDid = 'bip122:000000000019d6689c085ae165831e93:128Lkh3S7CkDTBZ8W7BbpsN3YYizJMp8p6'
+const ethDid = 'did:pkh:eip155:1:0xab16a96d359ec26a11e2c2b3d8f8b8942d5bfcdb'
+const btcDid = 'bdid:pkh:ip122:000000000019d6689c085ae165831e93:128Lkh3S7CkDTBZ8W7BbpsN3YYizJMp8p6'
 
 describe('PKH DID Resolver', () => {
   it('successfully resolves DIDs', async () => {
