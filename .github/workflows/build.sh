9cf76c9e43c620b387210e4c9fb6b8dd76ccff50
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2021-09-16 16:34:03 -0400

    test(ci): publish `js-ceramic` and `ipfs-daemon` Docker images to ECR

---
+++ .github/workflows/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TAGS=""
+for IMAGE_ID in $2
+do
+  for TAG in $3
+  do
+    TAGS="$TAGS --tag $IMAGE_ID:$TAG"
+  done
+done
+
+docker buildx build . --file "$1" --output 'type=image,push=true' "$TAGS"
