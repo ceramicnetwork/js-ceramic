7cadd629b17a557a017aa34dc684dad3e66a2534
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-09-17 07:03:44 -0400

    near improvements

--- packages/blockchain-utils-linking/src/__tests__/near.test.ts
+++ packages/blockchain-utils-linking/src/__tests__/near.test.ts
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import { NearAuthProvider } from '../near';
-import { KeyPair } from 'near-api-js';
-import * as uint8arrays from 'uint8arrays';
-
-const did = 'did:3:bafysdfwefwe';
-const privateKey = 'ed25519:9hB3onqC56qBSHpHJaE6EyxKPyFxCxzRBkmjuVx6UqXwygvAmFbwnsLuZ2YHsYJqkPTCygVBwXpNzssvWvUySbd';
-const local_provider = KeyPair.fromString(privateKey);
-const chainRef = 'near-testnet';
-
-class NearMockSigner {
-  readonly provider: KeyPair;
-
-  constructor(local_provider: KeyPair) {
-    this.provider = local_provider;
-  }
-
-  public async sign(message: String): Promise<{ signature: String, account: String }> {
-    const { signature, publicKey } = await this.provider.sign(
-      uint8arrays.fromString(message)
-    );
-    return {
-      signature: uint8arrays.toString(signature, 'base64pad'),
-      account: uint8arrays.toString(publicKey.data, 'base64pad'),
-    };
-=======
 import { NearAuthProvider } from '../near'
 import { KeyPair } from 'near-api-js'
 import * as uint8arrays from 'uint8arrays'
@@ -32,7 +6,8 @@ const did = 'did:3:bafysdfwefwe'
 const privateKey =
   'ed25519:9hB3onqC56qBSHpHJaE6EyxKPyFxCxzRBkmjuVx6UqXwygvAmFbwnsLuZ2YHsYJqkPTCygVBwXpNzssvWvUySbd'
 const local_provider = KeyPair.fromString(privateKey)
-const chainRef = 'near-mainnet'
+const chainRef = 'testnet'
+const account = 'crustykitty.testnet'
 
 class NearMockSigner {
   readonly provider: KeyPair
@@ -41,58 +16,33 @@ class NearMockSigner {
     this.provider = local_provider
   }
 
-  public async sign(message: String): Promise<{ signature: String; account: String }> {
+  public async sign(message: string): Promise<{ signature: String; account: String }> {
     const { signature, publicKey } = await this.provider.sign(uint8arrays.fromString(message))
     return {
       signature: uint8arrays.toString(signature, 'base64pad'),
       account: uint8arrays.toString(publicKey.data, 'base64pad'),
     }
->>>>>>> 4267d8b31fca38a163bb009198a49e1de22b2a58
   }
 }
 
 beforeAll(() => {
-<<<<<<< HEAD
-  global.Date.now = jest.fn().mockImplementation(() => 666000);
-});
-
-afterAll(() => {
-  jest.clearAllMocks();
-});
-=======
   global.Date.now = jest.fn().mockImplementation(() => 666000)
 })
 
 afterAll(() => {
   jest.clearAllMocks()
 })
->>>>>>> 4267d8b31fca38a163bb009198a49e1de22b2a58
 
 describe('Blockchain: NEAR', () => {
   describe('createLink', () => {
     test(`create proof for ${chainRef}`, async () => {
-<<<<<<< HEAD
-      const provider = new NearMockSigner(local_provider);
-=======
-      const provider = new NearMockSigner(local_provider)
->>>>>>> 4267d8b31fca38a163bb009198a49e1de22b2a58
-      const authProvider = new NearAuthProvider(
-        provider,
-        local_provider.getPublicKey().toString(),
+      const nearAuthProvider = new NearAuthProvider(
+        local_provider,
+        account,
         chainRef
-<<<<<<< HEAD
-      );
-      const proof = await authProvider.createLink(did);
-      expect(proof).toMatchSnapshot();
-    });
-  });
-
-  describe('authenticate', () => {
-    test(`create proof for ${chainRef}`, async () => {
-      const provider = new NearMockSigner(local_provider);
-=======
       )
-      const proof = await authProvider.createLink(did)
+      const proof = await nearAuthProvider.createLink(did)
+      console.log('proof', proof)
       expect(proof).toMatchSnapshot()
     })
   })
@@ -100,52 +50,14 @@ describe('Blockchain: NEAR', () => {
   describe('authenticate', () => {
     test(`create proof for ${chainRef}`, async () => {
       const provider = new NearMockSigner(local_provider)
->>>>>>> 4267d8b31fca38a163bb009198a49e1de22b2a58
       const authProvider = new NearAuthProvider(
         provider,
-        local_provider.getPublicKey().toString(),
+        account,
         chainRef
-<<<<<<< HEAD
-      );
-      const result = await authProvider.authenticate('msg');
-      expect(result).toMatchSnapshot();
-    });
-  });
-
-  describe('authenticate', () => {
-    test(`entropy replication for ${chainRef}`, async () => {
-      const provider = new NearMockSigner(local_provider);
-=======
       )
       const result = await authProvider.authenticate('msg')
       expect(result).toMatchSnapshot()
     })
   })
 
-  describe('authenticate', () => {
-    test(`entropy replication for ${chainRef}`, async () => {
-      const provider = new NearMockSigner(local_provider)
->>>>>>> 4267d8b31fca38a163bb009198a49e1de22b2a58
-      const authProvider = new NearAuthProvider(
-        provider,
-        local_provider.getPublicKey().toString(),
-        chainRef
-<<<<<<< HEAD
-      );
-      const msg = 'hello'
-      const control = await authProvider.authenticate(msg);
-      await expect(authProvider.authenticate(msg)).resolves.toEqual(control);
-      await expect(authProvider.authenticate(msg)).resolves.toEqual(control);
-    });
-  });
-
-});
-=======
-      )
-      const msg = 'hello'
-      expect(await authProvider.authenticate(msg)).toMatchSnapshot()
-      expect(await authProvider.authenticate(msg)).toMatchSnapshot()
-    })
-  })
 })
->>>>>>> 4267d8b31fca38a163bb009198a49e1de22b2a58
--- packages/blockchain-utils-linking/src/near.ts
+++ packages/blockchain-utils-linking/src/near.ts
@@ -4,53 +4,59 @@ import { getConsentMessage, LinkProof } from './util'
 import { hash } from '@stablelib/sha256'
 import * as uint8arrays from 'uint8arrays'
 
-const stringEncode = (str: string): string =>
-  uint8arrays.toString(uint8arrays.fromString(str), 'base64pad')
+const stringEncode = (str: string): Uint8Array => {
+  return uint8arrays.fromString((str), 'base64pad')
+}
 
-// REF: near.org
+const makeUint8 = (str: string): Uint8Array => {
+    const utf8Encode = new TextEncoder()
+    return utf8Encode.encode(str)
+}
+
 export class NearAuthProvider implements AuthProvider {
   readonly isAuthProvider = true
 
-  constructor(
-    private readonly provider: any,
-    // NOTE: address here is: ed25519 public key
-    private readonly address: string,
-    private readonly chainRef: string
-  ) {}
+    constructor(
+      private readonly keyPair: any,
+      private readonly address: string,
+      private readonly chainRef: string) 
+      {}
 
-  async authenticate(message: string): Promise<string> {
-    const encodedMsg = stringEncode(message)
-    const { signature } = await this.provider.sign(encodedMsg)
-    const digest = hash(signature)
-    return `0x${uint8arrays.toString(digest, 'base16')}`
-  }
-
-  async createLink(did: string): Promise<LinkProof> {
-    const { message, timestamp } = getConsentMessage(did)
-    const encodedMsg = stringEncode(message)
-    const { signature, account } = await this.provider.sign(encodedMsg)
-    const caipAccount = new AccountID({
-      address: account,
-      chainId: `near:${this.chainRef}`,
-    })
-    return {
-      version: 2,
-      message,
-      signature,
-      account: caipAccount.toString(),
-      timestamp,
+    async authenticate(message: string): Promise<string>  {
+        const encodedMsg = stringEncode(message)
+        const { signature } = await this.keyPair.sign(encodedMsg)
+        const digest = hash(signature)
+        return `0x${uint8arrays.toString(digest, 'base16')}`
     }
-  }
 
-  async accountId(): Promise<AccountID> {
-    return new AccountID({
-      address: this.address,
-      chainId: `near:${this.chainRef}`,
-    })
-  }
+    async createLink(did: string): Promise<LinkProof> {
+       
+        const { message, timestamp } = getConsentMessage(did, true)
+        const altered = makeUint8(message)
+        const { signature } = await this.keyPair.sign(altered)
+        const sig = uint8arrays.toString(signature, 'base64pad')
+        const account = await this.accountId()
 
-  // NOTE: address here is: ed25519 public key
-  withAddress(address: string): AuthProvider {
-    return new NearAuthProvider(this.provider, address, this.chainRef)
-  }
+        const proof = {
+            version: 2,
+            type: 'near',
+            message,
+            signature: sig,
+            account: account.toString(),
+     //       provider: this.keyPair,
+            timestamp,
+        }
+        return proof
+    }
+
+    async accountId(): Promise<AccountID> {
+        return new AccountID({
+            address: this.address,
+            chainId: `near:${this.chainRef}`,
+        })
+    }
+
+    withAddress(address: string): AuthProvider {
+        return new NearAuthProvider(this.keyPair, address, this.chainRef)
+    }
 }
