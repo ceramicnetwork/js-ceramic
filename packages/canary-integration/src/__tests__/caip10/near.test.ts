a07a3d862696f426da4fde748866eaf14738b880
ukstv <sergey@ukstv.me>
2021-09-22 16:49:22 +0300

    chore: near caip10link integration test
    forgot to copy this one

--- packages/canary-integration/src/__tests__/caip10/near.test.ts
+++ packages/canary-integration/src/__tests__/caip10/near.test.ts
@@ -1,51 +1,53 @@
 import { createCeramic } from '../../create-ceramic'
 import { createIPFS } from '../../create-ipfs'
 import { CeramicApi, IpfsApi } from '@ceramicnetwork/common'
-import { KeyPair } from 'near-api-js'
-import * as uint8arrays from 'uint8arrays'
 import * as linking from '@ceramicnetwork/blockchain-utils-linking'
 import { happyPath, wrongProof } from './caip-flows'
+import * as nearApiJs from 'near-api-js'
 
 const privateKey =
   'ed25519:9hB3onqC56qBSHpHJaE6EyxKPyFxCxzRBkmjuVx6UqXwygvAmFbwnsLuZ2YHsYJqkPTCygVBwXpNzssvWvUySbd'
-const chainRef = 'near-mainnet'
-const localProvider = KeyPair.fromString(privateKey)
-
-class NearMockSigner {
-  constructor(readonly provider: KeyPair) {}
-
-  public async sign(message: string): Promise<{ signature: string; account: string }> {
-    const { signature, publicKey } = await this.provider.sign(uint8arrays.fromString(message))
-    return {
-      signature: uint8arrays.toString(signature, 'base64pad'),
-      account: uint8arrays.toString(publicKey.data, 'base64pad'),
-    }
-  }
+const chainRef = 'testnet'
+const accountName = 'crustykitty.testnet'
+const keyPair = nearApiJs.utils.KeyPair.fromString(privateKey)
+const keyStore = new nearApiJs.keyStores.InMemoryKeyStore()
+keyStore.setKey(chainRef, accountName, keyPair)
+const config = {
+  keyStore, // instance of InMemoryKeyStore
+  networkId: 'testnet',
+  nodeUrl: 'https://rpc.testnet.near.org',
+  walletUrl: 'https://wallet.testnet.near.org',
+  helperUrl: 'https://helper.testnet.near.org',
+  explorerUrl: 'https://explorer.testnet.near.org',
 }
 
 let ceramic: CeramicApi
 let ipfs: IpfsApi
 
+beforeEach(async () => {
+  ceramic = await createCeramic(ipfs)
+}, 10000)
+
+afterEach(async () => {
+  await ceramic.close()
+}, 10000)
+
 beforeAll(async () => {
   ipfs = await createIPFS()
-  ceramic = await createCeramic(ipfs)
-}, 120000)
+}, 10000)
 
 afterAll(async () => {
-  await ceramic.close()
   await ipfs?.stop()
-}, 120000)
+}, 10000)
 
 test('happy path', async () => {
-  const provider = new NearMockSigner(localProvider)
-  const address = uint8arrays.toString(localProvider.getPublicKey().data, 'base64pad')
-  const authProvider = new linking.NearAuthProvider(provider, address, chainRef)
+  const near = await nearApiJs.connect(config)
+  const authProvider = new linking.NearAuthProvider(near, accountName, chainRef)
   await happyPath(ceramic, authProvider)
-}, 120000)
+}, 20000)
 
 test('wrong proof', async () => {
-  const provider = new NearMockSigner(localProvider)
-  const address = uint8arrays.toString(localProvider.getPublicKey().data, 'base64pad')
-  const authProvider = new linking.NearAuthProvider(provider, address, chainRef)
+  const near = await nearApiJs.connect(config)
+  const authProvider = new linking.NearAuthProvider(near, accountName, chainRef)
   await wrongProof(ceramic, authProvider)
-}, 120000)
+}, 20000)
