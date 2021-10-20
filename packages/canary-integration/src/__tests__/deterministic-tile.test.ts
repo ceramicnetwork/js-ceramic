94cf0d689de57451e74bfc19b4815b7dea951874
stephhuynh18 <stephhuynh18@users.noreply.github.com>
2021-10-20 13:28:39 -0700

    Deprecate deterministic tile metadata arg, add deterministic function, add test

---
+++ packages/canary-integration/src/__tests__/deterministic-tile.test.ts
@@ -0,0 +1,37 @@
+import { IpfsApi } from '@ceramicnetwork/common'
+import { createIPFS } from '../create-ipfs'
+import { CeramicApi } from '@ceramicnetwork/common'
+import { createCeramic } from '../create-ceramic'
+import { TileDocument } from '@ceramicnetwork/stream-tile'
+
+let ipfs: IpfsApi
+let ceramic: CeramicApi
+
+
+beforeAll(async () => {
+  ipfs = await createIPFS()
+  ceramic = await createCeramic(ipfs)
+}, 12000)
+
+afterAll(async () => {
+  await ipfs.stop()
+  await ceramic.close()
+})
+
+test("can create and retreive deterministic tile document", async () => {
+  const createdTile = await TileDocument.deterministic(ceramic, {family: 'test123'})
+  await createdTile.update({ foo: 'bar' })
+  
+  const retrievedTile = await TileDocument.deterministic(ceramic, {family: 'test123'}, { anchor: false, publish: false })
+
+  expect(createdTile.id.toString()).toEqual(retrievedTile.id.toString())
+  expect(createdTile.content).toMatchObject(retrievedTile.content)
+})
+
+test("cannot create or retreive deterministic tile document if family or tag not set", async () => {
+  await expect(TileDocument.deterministic(ceramic, { forbidControllerChange: false })).rejects.toThrow(
+    /Family and\/or tags are required/
+  )
+})
+
+
