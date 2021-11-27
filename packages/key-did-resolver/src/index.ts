5e3d6e78f6698b57747168a41882ad475965f966
Brent Shambaugh <brent.shambaugh@gmail.com>
2021-11-26 22:15:01 -0600

    updated index.ts to match current remote plus additional curves

--- packages/key-did-resolver/src/index.ts
+++ packages/key-did-resolver/src/index.ts
@@ -5,7 +5,7 @@ import type {
   DIDResolutionOptions,
   ResolverRegistry,
   ParsedDID,
-  Resolver
+  Resolver,
 } from 'did-resolver'
 
 import * as secp256k1 from './secp256k1'
@@ -17,20 +17,25 @@ const DID_LD_JSON = 'application/did+ld+json'
 const DID_JSON = 'application/did+json'
 // supported drivers
 const prefixToDriverMap: any = {
-  0xE7: secp256k1,
-  0xED: ed25519,
-  0x1200: secp256r1,  
+  0xe7: secp256k1,
+  0xed: ed25519,
+  0x1200: secp256r1,
   0x1201: secp384r1
 }
 
-export default {
-  getResolver: (): ResolverRegistry => ({
-    'key': async (did: string, parsed: ParsedDID, r: Resolver, options: DIDResolutionOptions): Promise<DIDResolutionResult> => {
+export function getResolver(): ResolverRegistry {
+  return {
+    key: async (
+      did: string,
+      parsed: ParsedDID,
+      r: Resolver,
+      options: DIDResolutionOptions
+    ): Promise<DIDResolutionResult> => {
       const contentType = options.accept || DID_JSON
       const response: DIDResolutionResult = {
         didResolutionMetadata: { contentType },
         didDocument: null,
-        didDocumentMetadata: {}
+        didDocumentMetadata: {},
       }
       try {
         const multicodecPubKey: any = multibase.decode(parsed.id)
@@ -38,7 +43,7 @@ export default {
         const pubKeyBytes = multicodecPubKey.slice(varint.decode.bytes)
         const doc = await prefixToDriverMap[keyType].keyToDidDoc(pubKeyBytes, parsed.id)
         if (contentType === DID_LD_JSON) {
-          doc['@context'] = 'https://w3id.org/did/v1',
+          doc['@context'] = 'https://w3id.org/did/v1'
           response.didDocument = doc
         } else if (contentType === DID_JSON) {
           response.didDocument = doc
@@ -51,6 +56,9 @@ export default {
         response.didResolutionMetadata.message = e.toString()
       }
       return response
-    }
-  })
+    },
+  }
 }
+
+export default { getResolver }
+
