e2cd543ace92a1c9644049b18cc70ae92e5b9b61
stephhuynh18 <stephhuynh18@users.noreply.github.com>
2021-12-07 12:43:01 -0800

    Add timeout to fetch json

--- packages/common/src/utils/http-utils.ts
+++ packages/common/src/utils/http-utils.ts
@@ -1,9 +1,12 @@
 import fetch from 'cross-fetch'
+// TODO: remove with esm upgrade
+import AbortController from 'abort-controller'
 
 interface FetchOpts {
   body?: any
   method?: string
   headers?: any
+  timeout?: number
 }
 
 export async function fetchJson(url: string, opts: FetchOpts = {}): Promise<any> {
@@ -13,7 +16,25 @@ export async function fetchJson(url: string, opts: FetchOpts = {}): Promise<any>
       headers: { 'Content-Type': 'application/json' },
     })
   }
+
+  const timeoutLength = opts.timeout || 60000
+  const controller = new AbortController()
+  const timeout = setTimeout(() => {
+    controller.abort()
+  }, timeoutLength)
+  Object.assign(opts, {
+    signal: controller.signal,
+  })
+
   const res = await fetch(url, opts)
+    .catch((err) => {
+      if (err.name == 'AbortError') {
+        throw new Error(`Http request timed out after ${timeoutLength} ms`)
+      }
+      throw err
+    })
+    .finally(() => timeout && clearTimeout(timeout))
+
   if (!res.ok) {
     const text = await res.text()
     throw new Error(`HTTP request to '${url}' failed with status '${res.statusText}': ${text}`)
