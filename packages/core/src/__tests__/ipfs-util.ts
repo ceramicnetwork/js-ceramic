3db4b34ef9f59b4aaf5a8e1ce77ee954728cf753
ukstv <sergey@ukstv.me>
2021-12-16 16:39:25 +0300

    chore(core): Use go-ipfs for tests

--- packages/core/src/__tests__/ipfs-util.ts
+++ packages/core/src/__tests__/ipfs-util.ts
@@ -1,42 +1,57 @@
+import type { IpfsApi } from '@ceramicnetwork/common'
 import * as dagJose from 'dag-jose'
-import { create } from 'ipfs-core'
-import { IpfsApi } from '@ceramicnetwork/common'
-import tmp from 'tmp-promise'
+import { path } from 'go-ipfs'
+import * as Ctl from 'ipfsd-ctl'
+import * as ipfsHttp from 'ipfs-http-client'
+import type { Options } from 'ipfs-core'
 import getPort from 'get-port'
+import mergeOpts from 'merge-options'
+
+const mergeOptions = mergeOpts.bind({ ignoreUndefined: true })
+
+async function ipfsConfig(override: Partial<Options> = {}): Promise<Options> {
+  const swarmPort = await getPort()
+  const apiPort = await getPort()
+  const gatewayPort = await getPort()
+  const defaultConfig: Partial<Options> = {
+    ipld: { codecs: [dagJose] },
+    config: {
+      Pubsub: {
+        Enabled: true,
+      },
+      Addresses: {
+        Swarm: [`/ip4/127.0.0.1/tcp/${swarmPort}`],
+        Gateway: `/ip4/127.0.0.1/tcp/${gatewayPort}`,
+        API: `/ip4/127.0.0.1/tcp/${apiPort}`,
+      },
+      Bootstrap: [],
+    },
+  }
+
+  return mergeOptions(defaultConfig, override) as Options
+}
 
 /**
  * Create an IPFS instance
  * @param overrideConfig - IFPS config for override
  */
-export async function createIPFS(overrideConfig: Record<string, unknown> = {}): Promise<IpfsApi> {
-  const tmpFolder = await tmp.dir({ unsafeCleanup: true })
+/**
+ * Create an IPFS instance
+ * @param overrideConfig - IFPS config for override
+ */
+export async function createIPFS(overrideConfig: Partial<Options> = {}): Promise<IpfsApi> {
+  const ipfsOptions = await ipfsConfig(overrideConfig)
 
-  const port = await getPort()
-  const defaultConfig = {
-    ipld: { codecs: [dagJose] },
-    repo: `${tmpFolder.path}/ipfs${port}/`,
-    config: {
-      Addresses: { Swarm: [`/ip4/127.0.0.1/tcp/${port}`] },
-      Bootstrap: [],
-    },
-  }
-
-  const config = { ...defaultConfig, ...overrideConfig }
-  const instance = await create(config)
-
-  // IPFS does not notify you when it stops.
-  // Here we intercept a call to `ipfs.stop` to clean up IPFS repository folder.
-  // Poor man's hook.
-  return new Proxy(instance, {
-    get(target: any, p: PropertyKey): any {
-      if (p === 'stop') {
-        tmpFolder.cleanup()
-      }
-      return target[p]
-    },
+  const ipfsd = await Ctl.createController({
+    ipfsHttpModule: ipfsHttp,
+    ipfsBin: path(),
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore ipfsd-ctl uses own type, that is _very_ similar to Options from ipfs-core
+    ipfsOptions: ipfsOptions,
+    disposable: true,
   })
+  return ipfsd.api
 }
-
 /**
  * Connect two IPFS instances via `swarm.connect`
  *
@@ -45,16 +60,9 @@ export async function createIPFS(overrideConfig: Record<string, unknown> = {}): 
  */
 export async function swarmConnect(a: IpfsApi, b: IpfsApi) {
   const addressB = (await b.id()).addresses[0]
+  const addressA = (await a.id()).addresses[0]
   await a.swarm.connect(addressB)
-}
-
-/**
- * Instantiate a number of IPFS instances
- * @param n - number of ipfs instances
- * @param overrideConfig - IPFS config for override
- */
-export function fleet(n: number, overrideConfig: Record<string, unknown> = {}): Promise<IpfsApi[]> {
-  return Promise.all(Array.from({ length: n }).map(() => createIPFS(overrideConfig)))
+  await b.swarm.connect(addressA)
 }
 
 /**
@@ -66,10 +74,27 @@ export async function withFleet(
   n: number,
   task: (instances: IpfsApi[]) => Promise<void>
 ): Promise<void> {
-  const instances = await fleet(n)
+  // const instances = await fleet(n)
+  const factory = Ctl.createFactory({
+    ipfsHttpModule: ipfsHttp,
+    ipfsBin: path(),
+    disposable: true,
+  })
+  const controllers = await Promise.all(
+    Array.from({ length: n }).map(async () => {
+      const ipfsOptions = await ipfsConfig()
+
+      return factory.spawn({
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore ipfsd-ctl uses own type, that is _very_ similar to Options from ipfs-core
+        ipfsOptions: ipfsOptions,
+      })
+    })
+  )
+  const instances = controllers.map((c) => c.api)
   try {
     await task(instances)
   } finally {
-    instances.map((instance) => instance.stop())
+    await factory.clean()
   }
 }
