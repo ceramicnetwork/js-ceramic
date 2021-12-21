246a91832de776d2ddb576933c3cff250dff8c67
Spencer T Brody <spencer+github@3box.io>
2021-12-21 14:44:41 -0500

    fix(core): Fix ipfs retries when using ipfs http client

--- packages/core/src/__tests__/dispatcher.test.ts
+++ packages/core/src/__tests__/dispatcher.test.ts
@@ -11,6 +11,10 @@ import { PinStore } from '../store/pin-store'
 import { RunningState } from '../state-management/running-state'
 import { StateManager } from '../state-management/state-manager'
 import cloneDeep from 'lodash.clonedeep'
+import { createIPFS } from './ipfs-util'
+import IpfsHttpClient from 'ipfs-http-client'
+import HttpApi from 'ipfs-http-server'
+import getPort from 'get-port'
 
 const TOPIC = '/ceramic'
 const FAKE_CID = new CID('bafybeig6xv5nwphfmvcnektpnojts33jqcuam7bmye2pb54adnrtccjlsu')
@@ -19,7 +23,7 @@ const FAKE_STREAM_ID = StreamID.fromString(
   'kjzl6cwe1jw147dvq16zluojmraqvwdmbh61dx9e0c59i344lcrsgqfohexp60s'
 )
 
-const ipfs = {
+const mock_ipfs = {
   pubsub: {
     ls: jest.fn(async () => Promise.resolve([])),
     subscribe: jest.fn(),
@@ -36,10 +40,11 @@ const ipfs = {
   id: async () => ({ id: 'ipfsid' }),
 }
 
-describe('Dispatcher', () => {
+describe('Dispatcher with mock ipfs', () => {
   let dispatcher: Dispatcher
   let repository: Repository
   const loggerProvider = new LoggerProvider()
+  const ipfs = mock_ipfs
 
   beforeEach(async () => {
     ipfs.dag.put.mockClear()
@@ -234,5 +239,69 @@ describe('Dispatcher', () => {
     const tips = new Map().set(FAKE_STREAM_ID.toString(), FAKE_CID2)
     await dispatcher.handleMessage({ typ: MsgType.RESPONSE, id: queryID, tips: tips })
     expect(dispatcher.repository.stateManager.update).toBeCalledWith(stream2.id, FAKE_CID2)
+  })
+})
+
+describe('Dispatcher with real ipfs over http', () => {
+  jest.setTimeout(1000 * 60 * 2) // 2 minutes. Need at least 1 min 30 for 3 30 second timeouts with retries
+
+  let dispatcher: Dispatcher
+  let ipfsNode: IpfsApi
+  let ipfsApi: HttpApi
+  let ipfsClient: IpfsApi
+
+  beforeAll(async () => {
+    const ipfsPort = await getPort()
+    const ipfsUrl = `http://127.0.0.1:${ipfsPort}`
+    const ipfsApiAddress = `/ip4/127.0.0.1/tcp/${ipfsPort}`
+    const overrideConfig = { config: { Addresses: { API: [ipfsApiAddress] } } }
+    console.log(`creatings ipfs with api port ${ipfsPort}`)
+    ipfsNode = await createIPFS(overrideConfig)
+    ipfsApi = new HttpApi(ipfsNode)
+    await ipfsApi.start()
+
+    ipfsClient = await IpfsHttpClient.create({ url: ipfsUrl })
+
+    const loggerProvider = new LoggerProvider()
+    const levelPath = await tmp.tmpName()
+    const stateStore = new LevelStateStore(levelPath)
+    stateStore.open('test')
+    const repository = new Repository(100, 100, loggerProvider.getDiagnosticsLogger())
+    const pinStore = {
+      stateStore,
+    } as unknown as PinStore
+    repository.setDeps({ pinStore } as unknown as RepositoryDependencies)
+    dispatcher = new Dispatcher(
+      ipfsClient,
+      TOPIC,
+      repository,
+      loggerProvider.getDiagnosticsLogger(),
+      loggerProvider.makeServiceLogger('pubsub')
+    )
+  })
+
+  afterAll(async () => {
+    await dispatcher.close()
+    await ipfsApi.stop()
+    await ipfsNode.stop()
+  })
+
+  it('basic ipfs http client functionality', async () => {
+    const cid = await dispatcher.storeCommit({ foo: 'bar' })
+
+    const data = await dispatcher.retrieveCommit(cid)
+    expect(data.foo).toEqual('bar')
+  })
+
+  it('retries on timeout', async () => {
+    const ipfsGetSpy = jest.spyOn(ipfsClient.dag, 'get')
+
+    // try to load a CID that ipfs doesn't know about.  It will timeout
+    await expect(dispatcher.retrieveCommit(FAKE_CID)).rejects.toThrow(/Request timed out/)
+
+    // Make sure we tried 3 times to get the cid from ipfs, not just once
+    expect(ipfsGetSpy).toBeCalledTimes(3)
+
+    ipfsGetSpy.mockRestore()
   })
 })
