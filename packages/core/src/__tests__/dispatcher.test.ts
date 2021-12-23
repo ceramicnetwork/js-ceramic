91ef32366608c2c53c61acf1a5370b15985b53b1
Spencer T Brody <spencer+github@3box.io>
2021-12-23 10:55:35 -0500

    don't instanciate state store

--- packages/core/src/__tests__/dispatcher.test.ts
+++ packages/core/src/__tests__/dispatcher.test.ts
@@ -267,11 +267,11 @@ describe('Dispatcher with real ipfs over http', () => {
     const loggerProvider = new LoggerProvider()
     const levelPath = await tmp.tmpName()
     console.log(`starting level state store at path ${levelPath.toString()}`)
-    const stateStore = new LevelStateStore(levelPath)
+    //const stateStore = new LevelStateStore(levelPath)
     //stateStore.open('test')
     const repository = new Repository(100, 100, loggerProvider.getDiagnosticsLogger())
     const pinStore = {
-      stateStore,
+      stateStore: null,
     } as unknown as PinStore
     repository.setDeps({ pinStore } as unknown as RepositoryDependencies)
     // dispatcher = new Dispatcher(
