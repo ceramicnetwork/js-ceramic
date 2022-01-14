c4aad455e8713b618d056af7335b494f064ed7df
Elizabeth Murray <Sterahi@users.noreply.github.com>
2022-01-14 04:21:39 -0500

    Add Deprecation notice to all functions that have been moved to the Glaze CLI (#1985)
    * add depreciation notice for all depreciated commands.
    
    reset package-lock
    
    add depreciation notice for cli help
    
    * depreciation -> deprecation
    
    * deprecate config & transition from raw colors to picocolors.
    
    * reset pkh-did-resolver to not be downgraded

--- packages/cli/src/ceramic-cli-utils.ts
+++ packages/cli/src/ceramic-cli-utils.ts
@@ -1,5 +1,6 @@
 import os from 'os'
 import path from 'path'
+import pc from 'picocolors'
 import { randomBytes } from '@stablelib/random'
 import * as u8a from 'uint8arrays'
 
@@ -191,6 +192,7 @@ export class CeramicCliUtils {
 
       console.log(doc.id)
       console.log(JSON.stringify(doc.content, null, 2))
+      depreciationNotice()
     })
   }
 
@@ -228,6 +230,7 @@ export class CeramicCliUtils {
       await doc.update(parsedContent, metadata)
 
       console.log(JSON.stringify(doc.content, null, 2))
+      depreciationNotice()
     })
   }
 
@@ -239,6 +242,7 @@ export class CeramicCliUtils {
     await CeramicCliUtils._runWithCeramicClient(async (ceramic: CeramicApi) => {
       const stream = await TileDocument.load(ceramic, streamRef)
       console.log(JSON.stringify(stream.content, null, 2))
+      depreciationNotice()
     })
   }
 
@@ -250,6 +254,7 @@ export class CeramicCliUtils {
     await CeramicCliUtils._runWithCeramicClient(async (ceramic: CeramicApi) => {
       const stream = await ceramic.loadStream(streamRef)
       console.log(JSON.stringify(StreamUtils.serializeState(stream.state), null, 2))
+      depreciationNotice()
     })
   }
 
@@ -263,6 +268,7 @@ export class CeramicCliUtils {
     await CeramicCliUtils._runWithCeramicClient(async (ceramic: CeramicApi) => {
       const doc = await TileDocument.load(ceramic, id)
       console.log(JSON.stringify(doc.content, null, 2))
+      depreciationNotice()
       doc.subscribe(() => {
         console.log('--- document changed ---')
         console.log(JSON.stringify(doc.content, null, 2))
@@ -281,6 +287,7 @@ export class CeramicCliUtils {
       const stream = await ceramic.loadStream(id)
       const commits = stream.allCommitIds.map((v) => v.toString())
       console.log(JSON.stringify(commits, null, 2))
+      depreciationNotice()
     })
   }
 
@@ -351,6 +358,7 @@ export class CeramicCliUtils {
     await CeramicCliUtils._runWithCeramicClient(async (ceramic: CeramicApi) => {
       const result = await ceramic.pin.add(id)
       console.log(JSON.stringify(result, null, 2))
+      depreciationNotice()
     })
   }
 
@@ -364,6 +372,7 @@ export class CeramicCliUtils {
     await CeramicCliUtils._runWithCeramicClient(async (ceramic: CeramicApi) => {
       const result = await ceramic.pin.rm(id)
       console.log(JSON.stringify(result, null, 2))
+      depreciationNotice()
     })
   }
 
@@ -445,6 +454,7 @@ export class CeramicCliUtils {
     const cliConfig = await this._loadCliConfig()
 
     console.log(JSON.stringify(cliConfig, null, 2))
+    depreciationNotice()
   }
 
   /**
@@ -467,6 +477,7 @@ export class CeramicCliUtils {
 
     console.log(`Ceramic CLI configuration ${variable} set to ${value}`)
     console.log(JSON.stringify(cliConfig))
+    depreciationNotice()
   }
 
   /**
@@ -481,6 +492,7 @@ export class CeramicCliUtils {
 
     console.log(`Ceramic CLI configuration ${variable} unset`)
     console.log(JSON.stringify(cliConfig, null, 2))
+    depreciationNotice()
   }
 
   /**
@@ -582,4 +594,13 @@ export class CeramicCliUtils {
     }
     return controllers.includes(',') ? controllers.split(',') : [controllers]
   }
+}
+
+const depreciationNotice = () => {
+  console.log(
+    `${pc.red(pc.bold('This command has been deprecated.'))}
+Please use the upgraded Glaze CLI instead. 
+Please test with the new CLI before reporting any problems. 
+${pc.green('npm i -g @glazed/cli')}`
+  )
 }
