a4fe3b0455342da33c8d6fc49c77b31ca8c5758e
ukstv <sergey@ukstv.me>
2022-01-27 13:46:34 -0500

    feat: Add PL-managed list of bootstrap nodes to embedded go-ipfs instance

--- packages/cli/src/build-ipfs-connection.util.ts
+++ packages/cli/src/build-ipfs-connection.util.ts
@@ -51,7 +51,14 @@ async function createGoIPFS(overrideConfig: Partial<ipfsClient.Options> = {}): P
         Gateway: `/ip4/127.0.0.1/tcp/${gatewayPort}`,
         API: `/ip4/127.0.0.1/tcp/${apiPort}`,
       },
-      Bootstrap: [],
+      Bootstrap: [
+        '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+        '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+        '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
+        '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
+        '/ip4/104.131.131.82/udp/4001/quic/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
+      ],
     },
   }
 
