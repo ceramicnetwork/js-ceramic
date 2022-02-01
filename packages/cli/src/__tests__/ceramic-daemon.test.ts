894e6cc7a6cf7117a73bd37bcbd301acecae853c
Spencer T Brody <spencer+github@3box.io>
2022-02-01 10:42:53 -0500

    Revert "wip split test"
    This reverts commit 6c6c2a46c097513b59edf22a9ce7f24875b7d08d.

--- packages/cli/src/__tests__/ceramic-daemon.test.ts
+++ packages/cli/src/__tests__/ceramic-daemon.test.ts
@@ -478,4 +478,87 @@ describe('Ceramic interop: core <> http-client', () => {
       expect(resClient[streamId.toString()].metadata).toEqual(metadata)
     })
   })
+
+  describe('pin api', () => {
+    let docA, docB
+
+    beforeEach(async () => {
+      docB = await TileDocument.create(core, { foo: 'bar' })
+      docA = await TileDocument.create(core, { foo: 'baz' })
+    })
+
+    const pinLs = async (streamId?: StreamID): Promise<Array<any>> => {
+      const pinnedDocsIterator = await client.pin.ls(streamId)
+      const docs = []
+      for await (const doc of pinnedDocsIterator) {
+        docs.push(doc)
+      }
+      return docs
+    }
+
+    it('pin API CRUD test', async () => {
+      // Make sure no docs are pinned to start
+      let pinnedDocs = await pinLs()
+      expect(pinnedDocs).toHaveLength(0)
+
+      // Pin docA
+      await client.pin.add(docA.id)
+
+      // Make sure docA shows up in list of all pinned docs
+      pinnedDocs = await pinLs()
+      expect(pinnedDocs).toEqual([docA.id.toString()])
+
+      // Make sure docA shows as pinned when checking for its specific streamId
+      pinnedDocs = await pinLs(docA.id)
+      expect(pinnedDocs).toEqual([docA.id.toString()])
+
+      // Make sure docB doesn't show up as pinned when checking for its streamId
+      pinnedDocs = await pinLs(docB.id)
+      expect(pinnedDocs).toHaveLength(0)
+
+      // Now pin docB as well, and make sure 'ls' works as expected in all cases
+      await client.pin.add(docB.id)
+
+      pinnedDocs = await pinLs()
+      expect(pinnedDocs).toHaveLength(2)
+      expect(pinnedDocs).toContain(docA.id.toString())
+      expect(pinnedDocs).toContain(docB.id.toString())
+
+      pinnedDocs = await pinLs(docA.id)
+      expect(pinnedDocs).toEqual([docA.id.toString()])
+
+      pinnedDocs = await pinLs(docB.id)
+      expect(pinnedDocs).toEqual([docB.id.toString()])
+
+      // Now unpin docA
+      await client.pin.rm(docA.id)
+
+      pinnedDocs = await pinLs()
+      expect(pinnedDocs).toEqual([docB.id.toString()])
+
+      // Make sure docB still shows as pinned when checking for its specific streamId
+      pinnedDocs = await pinLs(docB.id)
+      expect(pinnedDocs).toEqual([docB.id.toString()])
+
+      // Make sure docA no longer shows up as pinned when checking for its streamId
+      pinnedDocs = await pinLs(docA.id)
+      expect(pinnedDocs).toHaveLength(0)
+    })
+
+    it('force pin', async () => {
+      const pinSpy = jest.spyOn(ipfs.pin, 'add')
+      await client.pin.add(docA.id)
+
+      // 2 CIDs pinned for the one genesis commit (signed envelope + payload)
+      expect(pinSpy).toBeCalledTimes(2)
+
+      // Pin a second time, shouldn't cause any more calls to ipfs.pin.add
+      await client.pin.add(docA.id)
+      expect(pinSpy).toBeCalledTimes(2)
+
+      // Now force re-pin and make sure underlying state and ipfs records get re-pinned
+      await client.pin.add(docA.id, true)
+      expect(pinSpy).toBeCalledTimes(4)
+    })
+  })
 })
