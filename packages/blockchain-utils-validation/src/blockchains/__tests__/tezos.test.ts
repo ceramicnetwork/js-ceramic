0f14dde8847b0ad1d96b2076f574e0007be1a3b3
Sergey Ukustov <sergey@ukstv.me>
2022-02-02 17:42:37 +0300

    chore: Recover another commented out Tezos test

--- packages/blockchain-utils-validation/src/blockchains/__tests__/tezos.test.ts
+++ packages/blockchain-utils-validation/src/blockchains/__tests__/tezos.test.ts
@@ -1,154 +1,156 @@
-// TODO(NET-1108) ESM Mocking
-test.todo('tezos: jest and ESM mocking')
-// import { TezosAuthProvider, TezosProvider } from '@ceramicnetwork/blockchain-utils-linking'
-// import { InMemorySigner } from '@taquito/signer'
-// import { validateLink } from '../../index.js'
-// import { LinkProof } from '@ceramicnetwork/blockchain-utils-linking'
-// import fetch from 'cross-fetch'
-// import { AccountId, ChainId } from 'caip'
-// jest.mock('cross-fetch', () => jest.fn()) // this gets hoisted to the top of the file
-// const mockFetch = fetch as jest.Mock
-// const { Response } = jest.requireActual('cross-fetch')
-//
-// const did = 'did:3:bafysdfwefwe'
-// const privateKey = 'p2sk2obfVMEuPUnadAConLWk7Tf4Dt3n4svSgJwrgpamRqJXvaYcg1'
-//
-// type HttpResponse = Response
-//
-// type IoTestCase = {
-//   testName?: string
-//   error?: any
-//   pubkeyObject?(publicKey: string): HttpResponse
-// }
-//
-// type ProofTestCase = {
-//   message: string
-//   proof: () => LinkProof
-// }
-//
-// let provider: TezosProvider
-// let publicKey: string
-// let validProof: LinkProof
-// let invalidSignatureProof: LinkProof
-// let invalidChainIdProof: LinkProof
-//
-// // cache Date.now() to restore it after all tests
-// const dateNow = Date.now
-//
-// beforeAll(async () => {
-//   // Mock Date.now() to return a constant value
-//   Date.now = () => 666000
-//
-//   // initialize mock tezos provider
-//   const signer = await InMemorySigner.fromSecretKey(privateKey)
-//   provider = {
-//     signer,
-//   }
-//
-//   // create proof for did
-//   publicKey = await provider.signer.publicKey()
-//   const authProvider = new TezosAuthProvider(provider)
-//   validProof = await authProvider.createLink(did)
-//
-//   invalidSignatureProof = Object.assign({}, validProof)
-//   invalidSignatureProof.signature = 'invalid'
-//
-//   invalidChainIdProof = Object.assign({}, validProof)
-//   const accountId = AccountId.parse(invalidChainIdProof.account)
-//   const chainId = new ChainId(accountId.chainId)
-//   invalidChainIdProof.account = new AccountId({
-//     address: accountId.address,
-//     chainId: {
-//       namespace: chainId.namespace,
-//       reference: 'some unsupported chain reference',
-//     },
-//   }).toString()
-// })
-//
-// afterAll(() => {
-//   // restore Date.now()
-//   Date.now = dateNow
-//   jest.clearAllMocks()
-// })
-//
-// describe('Blockchain: Tezos', () => {
-//   describe('validateLink', () => {
-//     // create test cases
-//     const ioTestCases: IoTestCase[] = [
-//       // Validation not possible
-//       // - public key hash never used the blockchain
-//       // - public key can't be found from non-existant wallet (on the blockchain)
-//       // - signature can't be verified
-//       {
-//         testName: 'unable to validate when wallet address has never interacted with the blockchain',
-//         error: new Error('Fetch response error'),
-//       },
-//
-//       // Validation not possible
-//       // - public key is not published
-//       // - signature can not be verified
-//       {
-//         testName: 'unable to validate when wallet address has not been published to the blockchain',
-//         pubkeyObject(): HttpResponse {
-//           return new Response(
-//             JSON.stringify({
-//               pubkey: undefined,
-//             })
-//           )
-//         },
-//       },
-//
-//       // Able to test validation
-//       // - the public key is published/found on the blockchain
-//       // - the signature can be verified
-//       {
-//         pubkeyObject(publicKey?: string): HttpResponse {
-//           return new Response(
-//             JSON.stringify({
-//               pubkey: publicKey,
-//             })
-//           )
-//         },
-//       },
-//     ]
-//
-//     const proofTestCases: ProofTestCase[] = [
-//       // valid proof
-//       {
-//         message: 'valid proof',
-//         proof: () => validProof,
-//       },
-//       // invalid proof
-//       {
-//         message: 'invalid proof',
-//         proof: () => invalidSignatureProof,
-//       },
-//       // invalid chain reference
-//       {
-//         message: 'invalid chain reference',
-//         proof: () => invalidChainIdProof,
-//       },
-//     ]
-//
-//     // run test cases
-//     for (const { testName, pubkeyObject, error } of ioTestCases) {
-//       for (const { message, proof } of proofTestCases) {
-//         test(testName || message, async () => {
-//           void testName
-//           void message
-//           mockFetch.mockReset()
-//           // mock axios response or error
-//           if (pubkeyObject) {
-//             mockFetch.mockImplementationOnce(() => Promise.resolve(pubkeyObject(publicKey)))
-//           }
-//           if (error) {
-//             mockFetch.mockImplementationOnce(() => Promise.reject(error))
-//           }
-//
-//           // wait for test with the proof from the test case
-//           await expect(validateLink(proof())).resolves.toMatchSnapshot()
-//         })
-//       }
-//     }
-//   })
-// })
+import { jest } from '@jest/globals'
+import { TezosAuthProvider, TezosProvider } from '@ceramicnetwork/blockchain-utils-linking'
+import { InMemorySigner } from '@taquito/signer'
+import { LinkProof } from '@ceramicnetwork/blockchain-utils-linking'
+import { AccountId, ChainId } from 'caip'
+
+const did = 'did:3:bafysdfwefwe'
+const privateKey = 'p2sk2obfVMEuPUnadAConLWk7Tf4Dt3n4svSgJwrgpamRqJXvaYcg1'
+
+type HttpResponse = Response
+
+type IoTestCase = {
+  testName?: string
+  error?: any
+  pubkeyObject?(publicKey: string): Promise<HttpResponse>
+}
+
+type ProofTestCase = {
+  message: string
+  proof: () => LinkProof
+}
+
+const ioTestCases: IoTestCase[] = [
+  // Validation not possible
+  // - public key hash never used the blockchain
+  // - public key can't be found from non-existant wallet (on the blockchain)
+  // - signature can't be verified
+  {
+    testName: 'unable to validate when wallet address has never interacted with the blockchain',
+    error: new Error('Fetch response error'),
+  },
+
+  // Validation not possible
+  // - public key is not published
+  // - signature can not be verified
+  {
+    testName: 'unable to validate when wallet address has not been published to the blockchain',
+    async pubkeyObject(): Promise<HttpResponse> {
+      const { Response } = await import('cross-fetch')
+      return new Response(
+        JSON.stringify({
+          pubkey: undefined,
+        })
+      )
+    },
+  },
+
+  // Able to test validation
+  // - the public key is published/found on the blockchain
+  // - the signature can be verified
+  {
+    async pubkeyObject(publicKey?: string): Promise<HttpResponse> {
+      const { Response } = await import('cross-fetch')
+      return new Response(
+        JSON.stringify({
+          pubkey: publicKey,
+        })
+      )
+    },
+  },
+]
+
+const proofTestCases: ProofTestCase[] = [
+  // valid proof
+  {
+    message: 'valid proof',
+    proof: () => validProof,
+  },
+  // invalid proof
+  {
+    message: 'invalid proof',
+    proof: () => invalidSignatureProof,
+  },
+  // invalid chain reference
+  {
+    message: 'invalid chain reference',
+    proof: () => invalidChainIdProof,
+  },
+]
+
+let provider: TezosProvider
+let publicKey: string
+let validProof: LinkProof
+let invalidSignatureProof: LinkProof
+let invalidChainIdProof: LinkProof
+let responseResult: any
+
+// cache Date.now() to restore it after all tests
+const dateNow = Date.now
+
+jest.unstable_mockModule('cross-fetch', () => {
+  const originalModule = jest.requireActual('cross-fetch') as any
+  return {
+    ...originalModule,
+    default: async () => responseResult,
+  }
+})
+
+beforeAll(async () => {
+  // Mock Date.now() to return a constant value
+  Date.now = () => 666000
+
+  // initialize mock tezos provider
+  const signer = await InMemorySigner.fromSecretKey(privateKey)
+  provider = {
+    signer,
+  }
+
+  // create proof for did
+  publicKey = await provider.signer.publicKey()
+  const authProvider = new TezosAuthProvider(provider)
+  validProof = await authProvider.createLink(did)
+
+  invalidSignatureProof = { ...validProof, signature: 'invalid' }
+
+  const accountId = AccountId.parse(validProof.account)
+  const chainId = new ChainId(accountId.chainId)
+  const invalidAccount = new AccountId({
+    address: accountId.address,
+    chainId: {
+      namespace: chainId.namespace,
+      reference: 'some unsupported chain reference',
+    },
+  })
+  invalidChainIdProof = { ...validProof, account: invalidAccount.toString() }
+})
+
+afterAll(() => {
+  // restore Date.now()
+  Date.now = dateNow
+  jest.clearAllMocks()
+})
+
+describe('Blockchain: Tezos', () => {
+  describe('validateLink', () => {
+    // create test cases
+    // run test cases
+    for (const { testName, pubkeyObject, error } of ioTestCases) {
+      for (const { message, proof } of proofTestCases) {
+        test(testName || message, async () => {
+          const { validateLink } = await import('../../index.js')
+          // mockFetch.mockReset()
+          // // mock axios response or error
+          if (pubkeyObject) {
+            responseResult = await pubkeyObject(publicKey)
+          }
+          if (error) {
+            responseResult = Promise.reject(error)
+          }
+          // wait for test with the proof from the test case
+          await expect(validateLink(proof())).resolves.toMatchSnapshot()
+        })
+      }
+    }
+  })
+})
