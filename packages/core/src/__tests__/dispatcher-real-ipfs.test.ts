0eeb84bb2604d59eae7203570411725c90fc8626
Spencer T Brody <spencer+github@3box.io>
2022-02-14 05:27:30 -0500

    chore(core): Interrupt blocked ipfs network i/o at shutdown (#2033)
    * chore(core): Interrupt blocked ipfs network i/o at shutdown
    
    * chore(core): Re-enable disabled multiquery test
    
    * fix test
    
    * minor

--- packages/core/src/__tests__/dispatcher-real-ipfs.test.ts
+++ packages/core/src/__tests__/dispatcher-real-ipfs.test.ts
@@ -17,6 +17,7 @@ describe('Dispatcher with real ipfs over http', () => {
 
   let dispatcher: Dispatcher
   let ipfsClient: IpfsApi
+  let shutdownController: AbortController
 
   beforeAll(async () => {
     ipfsClient = await createIPFS()
@@ -30,12 +31,15 @@ describe('Dispatcher with real ipfs over http', () => {
       stateStore,
     } as unknown as PinStore
     repository.setDeps({ pinStore } as unknown as RepositoryDependencies)
+    shutdownController = new AbortController()
+
     dispatcher = new Dispatcher(
       ipfsClient,
       TOPIC,
       repository,
       loggerProvider.getDiagnosticsLogger(),
       loggerProvider.makeServiceLogger('pubsub'),
+      shutdownController.signal,
       10,
       new TaskQueue(),
       3000 // time out ipfs.dag.get after 3 seconds
@@ -50,6 +54,11 @@ describe('Dispatcher with real ipfs over http', () => {
     await TestUtils.delay(5000)
 
     await ipfsClient.stop()
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+    jest.restoreAllMocks()
   })
 
   it('basic ipfs http client functionality', async () => {
@@ -67,7 +76,17 @@ describe('Dispatcher with real ipfs over http', () => {
 
     // Make sure we tried 3 times to get the cid from ipfs, not just once
     expect(ipfsGetSpy).toBeCalledTimes(3)
-
-    ipfsGetSpy.mockRestore()
   })
+
+  it('interrupts on shutdown', async () => {
+    // This test has a shorter test timeout than the 'retries on timeout' test above
+    // (5 seconds instead of 30).  That means that if the retrieveCommit call actually goes through
+    // 3 retries that each take 3 seconds to time out, the test will take over 9 seconds and will
+    // fail due to test timeout.  If the test succeeds, that means that signaling the
+    // shutdownController successfully interrupted waiting on IPFS.
+
+    const getPromise = dispatcher.retrieveCommit(FAKE_CID)
+    shutdownController.abort()
+    await expect(getPromise).rejects.toThrow(/The user aborted a request/)
+  }, 5000)
 })
