d017d478920aab26901779d096db11eb542b047b
Joel Thorstensson <joel@oed.earth>
2022-03-06 10:56:18 +0000

    feat(stream-tile): support creating Tiles with CACAO

--- packages/canary-integration/src/__tests__/capability.test.ts
+++ packages/canary-integration/src/__tests__/capability.test.ts
@@ -10,12 +10,36 @@ import { randomBytes } from '@stablelib/random'
 import { SiweMessage, Cacao } from 'ceramic-cacao'
 import { createCeramic } from '../create-ceramic.js'
 
-let ipfs: IpfsApi
-let ceramic: CeramicApi
-let wallet: Wallet
-let didKey: DID
+
+const addCapToDid = async (wallet, didKey, resource) => {
+  // Create CACAO with did:key as aud
+  const siweMessage = new SiweMessage({
+    domain: 'service.org',
+    address: wallet.address,
+    chainId: '1',
+    statement: 'I accept the ServiceOrg Terms of Service: https://service.org/tos',
+    uri: didKey.id,
+    version: '1',
+    nonce: '23423423',
+    issuedAt: new Date().toISOString(),
+    resources: [resource],
+  })
+  // Sign CACAO with did:pkh
+  const signature = await wallet.signMessage(siweMessage.toMessage())
+  siweMessage.signature = signature
+  const capability = Cacao.fromSiweMessage(siweMessage)
+  // Create new did:key with capability attached
+  const didKeyWithCapability = didKey.withCapability(capability)
+  await didKeyWithCapability.authenticate()
+  return didKeyWithCapability
+}
 
 describe('CACAO Integration test', () => {
+  let ipfs: IpfsApi
+  let ceramic: CeramicApi
+  let wallet: Wallet
+  let didKey: DID
+
   beforeAll(async () => {
     ipfs = await createIPFS()
     ceramic = await createCeramic(ipfs)
@@ -34,243 +58,199 @@ describe('CACAO Integration test', () => {
     await ceramic.close()
   })
 
-  test('can update with streamId in capability', async () => {
-    // Create a determinstic tiledocument owned by the user
-    const deterministicDocument = await TileDocument.deterministic(ceramic, {
-      deterministic: true,
-      family: 'testCapabilities1',
-      controllers: [`did:pkh:eip155:1:${wallet.address}`],
-    })
+  describe('Resources using StreamId', () => {
+    test('can update with streamId in capability', async () => {
+      // Create a determinstic tiledocument owned by the user
+      const deterministicDocument = await TileDocument.deterministic(ceramic, {
+        deterministic: true,
+        family: 'testCapabilities1',
+        controllers: [`did:pkh:eip155:1:${wallet.address}`],
+      })
+      const streamId = deterministicDocument.id
+      const didKeyWithCapability = await addCapToDid(wallet, didKey, `ceramic://${streamId.toString()}`)
 
-    const streamId = deterministicDocument.id
-
-
-    // Create CACAO with did:key as aud
-    const siweMessage = new SiweMessage({
-      domain: 'service.org',
-      address: wallet.address,
-      chainId: '1',
-      statement: 'I accept the ServiceOrg Terms of Service: https://service.org/tos',
-      uri: didKey.id,
-      version: '1',
-      nonce: '23423423',
-      issuedAt: new Date().toISOString(),
-      resources: [`ceramic://${streamId.toString()}`],
-    })
-    // Sign CACAO with did:pkh
-    const signature = await wallet.signMessage(siweMessage.toMessage())
-    siweMessage.signature = signature
-    const capability = Cacao.fromSiweMessage(siweMessage)
-    // Create new did:key with capability attached
-    const didKeyWithCapability = didKey.withCapability(capability)
-    await didKeyWithCapability.authenticate()
-
-    await deterministicDocument.update({ foo: 'bar' }, null, {
-      asDID: didKeyWithCapability,
-      anchor: false,
-      publish: false,
-    })
-
-    expect(deterministicDocument.content).toEqual({ foo: 'bar' })
-  }, 30000)
-
-  test('does not allow updating if cacao issuer is not document controller', async () => {
-    // Create a determinstic tiledocument owned by the user
-    const deterministicDocument = await TileDocument.deterministic(ceramic, {
-      deterministic: true,
-      family: 'testCapabilities2',
-    })
-
-    const streamId = deterministicDocument.id
-
-    // Create CACAO with did:key as aud
-    const siweMessage = new SiweMessage({
-      domain: 'service.org',
-      address: wallet.address,
-      chainId: '1',
-      statement: 'I accept the ServiceOrg Terms of Service: https://service.org/tos',
-      uri: didKey.id,
-      version: '1',
-      nonce: '23423423',
-      issuedAt: new Date().toISOString(),
-      resources: [`ceramic://${streamId.toString()}`],
-    })
-    // Sign CACAO with did:pkh
-    const signature = await wallet.signMessage(siweMessage.toMessage())
-    siweMessage.signature = signature
-    const capability = Cacao.fromSiweMessage(siweMessage)
-    // Create new did:key with capability attached
-    const didKeyWithCapability = didKey.withCapability(capability)
-    await didKeyWithCapability.authenticate()
-
-    await expect(
-      deterministicDocument.update({ foo: 'baz' }, null, {
+      await deterministicDocument.update({ foo: 'bar' }, null, {
         asDID: didKeyWithCapability,
         anchor: false,
         publish: false,
       })
-    ).rejects.toThrow(/invalid_jws/)
-  }, 30000)
 
-  test('fails to update using capability with invalid resource', async () => {
-    // Create a determinstic tiledocument owned by the user
-    const deterministicDocument = await TileDocument.deterministic(ceramic, {
-      deterministic: true,
-      family: 'testCapabilities3',
-      controllers: [`did:pkh:eip155:1:${wallet.address}`],
-    })
+      expect(deterministicDocument.content).toEqual({ foo: 'bar' })
+    }, 30000)
 
-    // Create bad CACAO with did:key as aud
-    const badSiweMessage = new SiweMessage({
-      domain: 'service.org',
-      address: wallet.address,
-      statement: 'I accept the ServiceOrg Terms of Service: https://service.org/tos',
-      uri: didKey.id,
-      version: '1',
-      nonce: '23423423',
-      issuedAt: new Date().toISOString(),
-      resources: [`ceramic://abcdef`],
-    })
-    // Sign CACAO with did:pkh
-    const badSignature = await wallet.signMessage(badSiweMessage.toMessage())
-    badSiweMessage.signature = badSignature
-    const badCapability = Cacao.fromSiweMessage(badSiweMessage)
-    // Create new did:key with capability attached
-    const badDidKeyWithCapability = didKey.withCapability(badCapability)
-    await badDidKeyWithCapability.authenticate()
-
-    await expect(
-      deterministicDocument.update({ foo: 'baz' }, null, {
-        asDID: badDidKeyWithCapability,
-        anchor: false,
-        publish: false,
+    test('fails to update if cacao issuer is not document controller', async () => {
+      // Create a determinstic tiledocument owned by the user
+      const deterministicDocument = await TileDocument.deterministic(ceramic, {
+        deterministic: true,
+        family: 'testCapabilities2',
       })
-    ).rejects.toThrowError(
-      'Capability does not have appropriate permissions to update this TileDocument'
-    )
-  }, 30000)
+      const streamId = deterministicDocument.id
+      const didKeyWithCapability = await addCapToDid(wallet, didKey, `ceramic://${streamId.toString()}`)
 
-  test('can update stream with family resource', async () => {
-    const family = 'testFamily1'
-    // Create a determinstic tiledocument owned by the user
-    const deterministicDocument = await TileDocument.deterministic(ceramic, {
-      deterministic: true,
-      family,
-      controllers: [`did:pkh:eip155:1:${wallet.address}`],
-    })
+      await expect(
+        deterministicDocument.update({ foo: 'baz' }, null, {
+          asDID: didKeyWithCapability,
+          anchor: false,
+          publish: false,
+        })
+      ).rejects.toThrow(/invalid_jws/)
+    }, 30000)
 
-    const streamId = deterministicDocument.id
+    test('fails to update using capability with invalid resource', async () => {
+      // Create a determinstic tiledocument owned by the user
+      const deterministicDocument = await TileDocument.deterministic(ceramic, {
+        deterministic: true,
+        family: 'testCapabilities3',
+        controllers: [`did:pkh:eip155:1:${wallet.address}`],
+      })
+      const badDidKeyWithCapability = await addCapToDid(wallet, didKey, `ceramic://abcdef`)
 
-    // Create CACAO with did:key as aud
-    const siweMessage = new SiweMessage({
-      domain: 'service.org',
-      address: wallet.address,
-      chainId: '1',
-      statement: 'I accept the ServiceOrg Terms of Service: https://service.org/tos',
-      uri: didKey.id,
-      version: '1',
-      nonce: '23423423',
-      issuedAt: new Date().toISOString(),
-      resources: [`ceramic://*?family=${family}`],
-    })
-    // Sign CACAO with did:pkh
-    const signature = await wallet.signMessage(siweMessage.toMessage())
-    siweMessage.signature = signature
-    const capability = Cacao.fromSiweMessage(siweMessage)
-    // Create new did:key with capability attached
-    const didKeyWithCapability = didKey.withCapability(capability)
-    await didKeyWithCapability.authenticate()
+      await expect(
+        deterministicDocument.update({ foo: 'baz' }, null, {
+          asDID: badDidKeyWithCapability,
+          anchor: false,
+          publish: false,
+        })
+      ).rejects.toThrowError(
+        'Capability does not have appropriate permissions to update this TileDocument'
+      )
+    }, 30000)
+  })
 
-    await deterministicDocument.update({ foo: 'bar' }, null, {
-      asDID: didKeyWithCapability,
-      anchor: false,
-      publish: false,
-    })
+  describe('Resources using family', () => {
+    test('can update stream with family resource', async () => {
+      const family = 'testFamily1'
+      // Create a determinstic tiledocument owned by the user
+      const deterministicDocument = await TileDocument.deterministic(ceramic, {
+        deterministic: true,
+        family,
+        controllers: [`did:pkh:eip155:1:${wallet.address}`],
+      })
+      const streamId = deterministicDocument.id
+      const didKeyWithCapability = await addCapToDid(wallet, didKey, `ceramic://*?family=${family}`)
 
-    expect(deterministicDocument.content).toEqual({ foo: 'bar' })
-  }, 30000)
-
-  test('fails to update using capability with wrong family resource', async () => {
-    const family = 'testFamily1'
-    // Create a determinstic tiledocument owned by the user
-    const deterministicDocument = await TileDocument.deterministic(ceramic, {
-      deterministic: true,
-      family,
-      controllers: [`did:pkh:eip155:1:${wallet.address}`],
-    })
-
-    const streamId = deterministicDocument.id
-
-    // Create CACAO with did:key as aud
-    const siweMessage = new SiweMessage({
-      domain: 'service.org',
-      address: wallet.address,
-      chainId: '1',
-      statement: 'I accept the ServiceOrg Terms of Service: https://service.org/tos',
-      uri: didKey.id,
-      version: '1',
-      nonce: '23423423',
-      issuedAt: new Date().toISOString(),
-      resources: [`ceramic://*?family=${family}-wrong`],
-    })
-    // Sign CACAO with did:pkh
-    const signature = await wallet.signMessage(siweMessage.toMessage())
-    siweMessage.signature = signature
-    const capability = Cacao.fromSiweMessage(siweMessage)
-    // Create new did:key with capability attached
-    const didKeyWithCapability = didKey.withCapability(capability)
-    await didKeyWithCapability.authenticate()
-
-    await expect(
-      deterministicDocument.update({ foo: 'baz' }, null, {
+      await deterministicDocument.update({ foo: 'bar' }, null, {
         asDID: didKeyWithCapability,
         anchor: false,
         publish: false,
       })
-    ).rejects.toThrowError(
-      'Capability does not have appropriate permissions to update this TileDocument'
-    )
-  }, 30000)
 
-  test('fails to update using capability with empty family resource', async () => {
-    const family = 'testFamily1'
-    // Create a determinstic tiledocument owned by the user
-    const deterministicDocument = await TileDocument.deterministic(ceramic, {
-      deterministic: true,
-      family,
-      controllers: [`did:pkh:eip155:1:${wallet.address}`],
-    })
+      expect(deterministicDocument.content).toEqual({ foo: 'bar' })
+    }, 30000)
 
-    const streamId = deterministicDocument.id
+    test('fails to update using capability with wrong family resource', async () => {
+      const family = 'testFamily2'
+      // Create a determinstic tiledocument owned by the user
+      const deterministicDocument = await TileDocument.deterministic(ceramic, {
+        deterministic: true,
+        family,
+        controllers: [`did:pkh:eip155:1:${wallet.address}`],
+      })
+      const streamId = deterministicDocument.id
+      const didKeyWithCapability = await addCapToDid(wallet, didKey, `ceramic://*?family=${family}-wrong`)
 
-    // Create CACAO with did:key as aud
-    const siweMessage = new SiweMessage({
-      domain: 'service.org',
-      address: wallet.address,
-      chainId: '1',
-      statement: 'I accept the ServiceOrg Terms of Service: https://service.org/tos',
-      uri: didKey.id,
-      version: '1',
-      nonce: '23423423',
-      issuedAt: new Date().toISOString(),
-      resources: [`ceramic://*?family=`],
-    })
-    // Sign CACAO with did:pkh
-    const signature = await wallet.signMessage(siweMessage.toMessage())
-    siweMessage.signature = signature
-    const capability = Cacao.fromSiweMessage(siweMessage)
-    // Create new did:key with capability attached
-    const didKeyWithCapability = didKey.withCapability(capability)
-    await didKeyWithCapability.authenticate()
+      await expect(
+        deterministicDocument.update({ foo: 'baz' }, null, {
+          asDID: didKeyWithCapability,
+          anchor: false,
+          publish: false,
+        })
+      ).rejects.toThrowError(
+        'Capability does not have appropriate permissions to update this TileDocument'
+      )
+    }, 30000)
 
-    await expect(
-      deterministicDocument.update({ foo: 'baz' }, null, {
+    test('fails to update using capability with empty family resource', async () => {
+      const family = 'testFamily3'
+      // Create a determinstic tiledocument owned by the user
+      const deterministicDocument = await TileDocument.deterministic(ceramic, {
+        deterministic: true,
+        family,
+        controllers: [`did:pkh:eip155:1:${wallet.address}`],
+      })
+      const streamId = deterministicDocument.id
+      const didKeyWithCapability = await addCapToDid(wallet, didKey, `ceramic://*?family=`)
+
+      await expect(
+        deterministicDocument.update({ foo: 'baz' }, null, {
+          asDID: didKeyWithCapability,
+          anchor: false,
+          publish: false,
+        })
+      ).rejects.toThrowError(
+        'Capability does not have appropriate permissions to update this TileDocument'
+      )
+    }, 30000)
+
+    test('fails to update if cacao issuer is not document controller using family resource', async () => {
+      const family = 'testFamily4'
+      // Create a determinstic tiledocument owned by the user
+      const deterministicDocument = await TileDocument.deterministic(ceramic, {
+        deterministic: true,
+        family,
+      })
+      const streamId = deterministicDocument.id
+      const didKeyWithCapability = await addCapToDid(wallet, didKey, `ceramic://*?family=${family}`)
+
+      await expect(
+        deterministicDocument.update({ foo: 'baz' }, null, {
+          asDID: didKeyWithCapability,
+          anchor: false,
+          publish: false,
+        })
+      ).rejects.toThrow(/invalid_jws/)
+    }, 30000)
+
+    test('can create new stream with family resource', async () => {
+      const family = 'testFamily1'
+      const didKeyWithCapability = await addCapToDid(wallet, didKey, `ceramic://*?family=${family}`)
+
+      const doc = await TileDocument.create(ceramic, { foo: 'bar' }, {
+        family,
+        controllers: [`did:pkh:eip155:1:${wallet.address}`],
+      }, {
         asDID: didKeyWithCapability,
         anchor: false,
         publish: false,
       })
-    ).rejects.toThrowError(
-      'Capability does not have appropriate permissions to update this TileDocument'
-    )
-  }, 30000)
+
+      expect(doc.content).toEqual({ foo: 'bar' })
+      expect(doc.metadata.controllers).toEqual([`did:pkh:eip155:1:${wallet.address}`])
+      expect(doc.metadata.family).toEqual(family)
+    }, 30000)
+
+    test('can not create new stream with wrong family', async () => {
+      const family = 'testFamily1'
+      const didKeyWithCapability = await addCapToDid(wallet, didKey, `ceramic://*?family=${family}`)
+
+      await expect(
+        TileDocument.create(ceramic, { foo: 'bar' }, {
+          family: `${family}-wrong`,
+          controllers: [`did:pkh:eip155:1:${wallet.address}`],
+        }, {
+          asDID: didKeyWithCapability,
+          anchor: false,
+          publish: false,
+        })
+      ).rejects.toThrowError(
+        'Capability does not have appropriate permissions to update this TileDocument'
+      )
+    }, 30000)
+
+    test('can not create new stream without capability', async () => {
+      const family = 'testFamily1'
+      await expect(
+        TileDocument.create(ceramic, { foo: 'bar' }, {
+          family: `${family}`,
+          controllers: [`did:pkh:eip155:1:${wallet.address}`],
+        }, {
+          asDID: didKey,
+          anchor: false,
+          publish: false,
+        })
+      ).rejects.toThrowError(
+        /invalid_jws: not a valid verificationMethod for issuer:/
+      )
+    }, 30000)
+  })
 })
