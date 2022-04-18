1d2b81b960db9dedc5a9c796cd9fc0ba805f66c4
Spencer T Brody <spencer+github@3box.io>
2022-04-18 11:51:11 -0400

    feat(core): Add env variable for configuring stream cache size (#2120)
    (cherry picked from commit e5d72c1e5cba05c4fc372aa31dfeb9ada31fa928)

--- packages/core/src/ceramic.ts
+++ packages/core/src/ceramic.ts
@@ -411,7 +411,11 @@ export class Ceramic implements CeramicApi {
 
     const loadOptsOverride = config.syncOverride ? { sync: config.syncOverride } : {}
 
-    const streamCacheLimit = config.streamCacheLimit ?? DEFAULT_CACHE_LIMIT
+    // Use env var if set as highest priority. If no env var, check config file.
+    // If not set in config file, fall back to hard-coded default.
+    const streamCacheLimit = process.env.CERAMIC_STREAM_CACHE_LIMIT
+      ? parseInt(process.env.CERAMIC_STREAM_CACHE_LIMIT)
+      : config.streamCacheLimit ?? DEFAULT_CACHE_LIMIT
     const concurrentRequestsLimit = config.concurrentRequestsLimit ?? streamCacheLimit
     const maxQueriesPerSecond = process.env.CERAMIC_PUBSUB_QPS_LIMIT
       ? parseInt(process.env.CERAMIC_PUBSUB_QPS_LIMIT)
