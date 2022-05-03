024ab819bf1f5d2f110e336b2fcdd147fb74cad0
Golda Velez <gv@birdbox.local>
2022-05-02 20:34:56 -0700

    non-working first rough draft of shoehorning open telemetry in

--- packages/common/src/stream-state-subject.ts
+++ packages/common/src/stream-state-subject.ts
@@ -1,15 +1,19 @@
 import { BehaviorSubject } from 'rxjs'
 import { StreamState } from './stream.js'
 import { StreamUtils } from './utils/stream-utils.js'
-
+//import { tracer } from './setup-instrumentation.js'
+//const { trace }  = require("@opentelemetry/api");
 /**
  * BehaviourSubject<StreamState> that emits only distinct values.
  */
 export class StreamStateSubject extends BehaviorSubject<StreamState> {
   next(next: StreamState) {
+    const otSpan = tracer.startSpan('streamstate_test');
     const current = this.value
+
     if (!StreamUtils.statesEqual(current, next)) {
       super.next(next)
     }
+    otSpan.end()
   }
 }
