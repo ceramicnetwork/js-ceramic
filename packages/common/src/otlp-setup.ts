f2c885614c845f0426eaea78be85afd33b867dc9
Golda Velez <gv@birdbox.local>
2022-05-12 10:29:13 -0700

    functional approach - may change to class based

--- packages/common/src/otlp-setup.ts
+++ packages/common/src/otlp-setup.ts
@@ -31,10 +31,41 @@ const meter = meterProvider.getMeter('js-ceramic');
 // Tracer for js-ceramic
 const tracer = trace.getTracer(
   'js-ceramic'
-);
+)
 
-//export function MetricMaker(params: Map) {
+export const enum MetricType {
+  COUNTER,
+  GAUGE,
+  HISTOGRAM
+}
+
+// see https://opentelemetry.io/docs/reference/specification/metrics/semantic_conventions/#general-metric-semantic-conventions
+
+// probably this should be a class with inheritance instead
+export function MetricMaker(name:string, metric_type:MetricType, params: any) {
+  const caller = MetricMaker.caller
+  let metric = null
+  let metric_name = `${caller}.${name}`
+  switch (metric_type) {
+    case MetricType.COUNTER: {
+      metric_name = `${metric_name}.counter`
+      metric = meter.createCounter(metric_name, params)
+      break
+    }
+    case MetricType.GAUGE: {
+      metric_name = `${metric_name}.gauge`
+      metric = meter.createObservableGauge(metric_name, params)
+      break
+    }
+
+  }
+
 //  ctr = meter.createUpDownCounter('test')
-//}
+}
 
+export function SpanMaker(params: any) {
+  const caller = SpanMaker.caller
+
+//  ctr = meter.createUpDownCounter('test')
+}
 
