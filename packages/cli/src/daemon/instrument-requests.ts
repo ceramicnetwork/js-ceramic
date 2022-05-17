f4bc56d33f79923384e993c904c7efa82572d98c
Golda Velez <gv@birdbox.local>
2022-05-17 16:07:36 -0700

    add metrics into the http requests

--- packages/cli/src/daemon/instrument-requests.ts
+++ packages/cli/src/daemon/instrument-requests.ts
@@ -1,9 +1,12 @@
+import {Counter, Span} from '@ceramicnetwork/common'
 
-module.exports = function instrumentRequests(req, res, next) {
+export function instrumentRequests(req, res, next) {
 
   const token = req.header("authorization")
-  const agent = req.header("user-agent") 
+  const agent = req.header("user-agent")
   // do some metrics
+  const counter = new Counter('request', {'method': 'GET'})
+  counter.add(1)
   next()
 }
 
