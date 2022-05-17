686aa5d8fd9c55d75e46e7d9e9600da790b4a139
Spencer T Brody <spencer+github@3box.io>
2022-05-17 14:38:24 -0400

    kasjk

--- packages/stream-model-instance-document-handler/src/tile-document-handler.ts
+++ packages/stream-model-instance-document-handler/src/tile-document-handler.ts
@@ -1,7 +1,7 @@
 import jsonpatch from 'fast-json-patch'
 import cloneDeep from 'lodash.clonedeep'
 import type { Cacao } from 'ceramic-cacao'
-import { ModelInstanceDocument } from '@ceramicnetwork/stream-tile'
+import { ModelInstanceDocument } from '@ceramicnetwork/stream-model-instance-document'
 import {
   AnchorStatus,
   CommitData,
@@ -85,7 +85,7 @@ export class ModelInstanceDocumentHandler implements StreamHandler<ModelInstance
     const payload = commitData.commit
     const isSigned = StreamUtils.isSignedCommitData(commitData)
     if (isSigned) {
-      const streamId = await StreamID.fromGenesis('tile', commitData.commit)
+      const streamId = await StreamID.fromGenesis('MID', commitData.commit)
       const { controllers, family } = payload.header
       await this._verifySignature(commitData, context, controllers[0], family, streamId)
     } else if (payload.data) {
