d70c3f6ac0824757c1f019eed13c4bf2c212aeb9
Sergey Ukustov <sergey@ukstv.me>
2022-05-18 17:57:30 +0300

    feat(core): Initial crack at types and index api

---
+++ packages/core/src/indexing/types.ts
@@ -0,0 +1,58 @@
+import type { StreamID } from '@ceramicnetwork/streamid'
+import type { DID } from 'dids'
+
+export interface IndexStreamArgs {
+  streamID: StreamID
+  model: StreamID
+  controller: DID
+  lastAnchor: Date | null
+  content: Record<string, any>
+  relationFields?: Map<string, StreamID | DID>
+}
+
+export enum Ordering {
+  CHRONOLOGICAL = 'chronological', // last_anchored_at DESC NULLS FIRST, created_at DESC
+  INSERTION = 'insertion', // created_at DESC = when an entry was added to the index
+}
+
+export interface BaseQuery {
+  model: StreamID | string
+  account?: string
+  order?: Ordering // default: CHRONOLOGICAL_DESC
+}
+
+export interface ForwardPagination {
+  first: number
+  after?: string
+}
+
+export interface BackwardPagination {
+  last: number
+  before?: string
+}
+
+export type Pagination = ForwardPagination | BackwardPagination
+
+export interface Page<T> {
+  entries: Array<T>
+  pageInfo: PageInfo
+}
+
+export type PageInfo = {
+  hasNextPage: boolean
+  hasPreviousPage: boolean
+  startCursor: string
+  endCursor: string
+}
+
+// Per database
+export interface DatabaseIndexAPI {
+  // This method inserts the stream if it is not present in the index, or updates
+  // the 'content' if the stream already exists in the index.
+  // 'relationFields' won't be included in the MVP, but will be added later
+  // as part of adding support for relations to data models.
+  indexStream(args: IndexStreamArgs): Promise<void>
+
+  // Regardless of the database, we are going to do pagination.
+  page(query: BaseQuery & Pagination): Promise<Page<StreamID>>
+}
