6b4753718ecfded7bb94b7a84ecd40384168b958
Spencer T Brody <spencer+github@3box.io>
2022-05-25 17:31:58 -0400

    Update test to use http-client

--- packages/canary-integration/src/__tests__/model.test.ts
+++ packages/canary-integration/src/__tests__/model.test.ts
@@ -1,4 +1,6 @@
 import { jest } from '@jest/globals'
+import cloneDeep from 'lodash.clonedeep'
+import getPort from 'get-port'
 import { AnchorStatus, CommitType, IpfsApi } from '@ceramicnetwork/common'
 import { createIPFS } from '@ceramicnetwork/ipfs-daemon'
 import { Model, ModelAccountRelation } from '@ceramicnetwork/stream-model'
@@ -6,24 +8,36 @@ import { createCeramic } from '../create-ceramic.js'
 import { anchorUpdate } from '@ceramicnetwork/core/lib/state-management/__tests__/anchor-update'
 import { Ceramic } from '@ceramicnetwork/core'
 import { StreamID } from '@ceramicnetwork/streamid'
-import cloneDeep from 'lodash.clonedeep'
+import { CeramicDaemon, DaemonConfig } from '@ceramicnetwork/cli'
+import { CeramicClient } from '@ceramicnetwork/http-client'
 
 const PLACEHOLDER_CONTENT = { name: 'myModel' }
 const FINAL_CONTENT = { name: 'myModel', schema: {}, accountRelation: ModelAccountRelation.LIST }
 
-describe('Model core API tests', () => {
+describe('Model API http-client tests', () => {
   jest.setTimeout(1000 * 30)
 
   let ipfs: IpfsApi
-  let ceramic: Ceramic
+  let core: Ceramic
+  let daemon: CeramicDaemon
+  let ceramic: CeramicClient
 
   beforeAll(async () => {
     ipfs = await createIPFS()
-    ceramic = await createCeramic(ipfs)
+    core = await createCeramic(ipfs)
+
+    const port = await getPort()
+    const apiUrl = 'http://localhost:' + port
+    daemon = new CeramicDaemon(core, DaemonConfig.fromObject({ 'http-api': { port } }))
+    await daemon.listen()
+    ceramic = new CeramicClient(apiUrl)
+    ceramic.setDID(core.did)
   }, 12000)
 
   afterAll(async () => {
     await ceramic.close()
+    await daemon.close()
+    await core.close()
     await ipfs.stop()
   })
 
@@ -67,7 +81,9 @@ describe('Model core API tests', () => {
     const model = await Model.create(ceramic, FINAL_CONTENT)
     expect(model.state.anchorStatus).toEqual(AnchorStatus.PENDING)
 
-    await anchorUpdate(ceramic, model)
+    await anchorUpdate(core, model)
+    await model.sync()
+
     expect(model.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
     expect(model.state.log.length).toEqual(2)
     expect(model.state.log[0].type).toEqual(CommitType.GENESIS)
@@ -81,7 +97,9 @@ describe('Model core API tests', () => {
     await model.replacePlaceholder(FINAL_CONTENT)
     expect(model.state.anchorStatus).toEqual(AnchorStatus.PENDING)
 
-    await anchorUpdate(ceramic, model)
+    await anchorUpdate(core, model)
+    await model.sync()
+
     expect(model.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
     expect(model.state.log.length).toEqual(3)
     expect(model.state.log[0].type).toEqual(CommitType.GENESIS)
@@ -139,7 +157,8 @@ describe('Model core API tests', () => {
   test('Can load a complete stream', async () => {
     const model = await Model.createPlaceholder(ceramic, PLACEHOLDER_CONTENT)
     await model.replacePlaceholder(FINAL_CONTENT)
-    await anchorUpdate(ceramic, model)
+    await anchorUpdate(core, model)
+    await model.sync()
 
     const loaded = await Model.load(ceramic, model.id)
 
