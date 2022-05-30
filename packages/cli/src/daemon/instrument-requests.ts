a0b8216b9e9bb5738fe54585cb9e964af25d2e17
Golda Velez <gv@birdbox.local>
2022-05-30 15:20:38 -0700

    reduce files to minimal functions and add test

--- packages/cli/src/daemon/instrument-requests.ts
+++ packages/cli/src/daemon/instrument-requests.ts
@@ -9,18 +9,5 @@ export function instrumentRequest(req, res, next) {
   Count(REQUEST_METRIC, 1, {'method': req.method, 'endpoint':req.url, 'agent':agent})
   next()
 }
-/*
 
-This would require https://www.npmjs.com/package/on-finished
-
-export function instrumentResponse(req, res, next) {
-
-  const token = req.header("authorization")
-  const agent = req.header("user-agent")
-  // do some metrics
-  // res.statusCode
-  Count(RESPONSE_METRIC, 1, {'method': req.method, 'endpoint':req.url, 'agent':agent})
-  next()
-}
- */
 
