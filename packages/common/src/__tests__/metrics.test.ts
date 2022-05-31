dbfe7944fbb75f52cd571dc4a0e6da9db8744711
Golda Velez <gv@birdbox.local>
2022-05-31 10:29:17 -0700

    100% test coverage for metrics-setup

--- packages/common/src/__tests__/metrics.test.ts
+++ packages/common/src/__tests__/metrics.test.ts
@@ -1,18 +1,16 @@
-import {Count, REQUEST_METRIC} from '../../../common'
+import {Count, Record, REQUEST_METRIC} from '../../../common'
 import { StreamState } from '../stream'
 
 const updates: StreamState[] = []
 
 describe('simple test of metrics', () => {
-  afterEach(() => {
-    delete process.env.OTEL_EXPORTER_PROMETHEUS_HOST;
-    delete process.env.OTEL_EXPORTER_PROMETHEUS_PORT;
-  });
   test('create valid metric', async () => {
     Count(REQUEST_METRIC, 1, {'anyparam': null, 'otherparam':'atring', 'intparam':2})
+    Record(REQUEST_METRIC, 1, {'anyparam': null, 'otherparam':'atring', 'intparam':2})
   })
   test('create_invalid_metric', async () => {
     expect(function() {Count("Invalid metric", 2)}).toThrow(/Error: metric names must be defined in VALID_METRIC_NAMES/)
+    expect(function() {Record("Invalid metric", 2)}).toThrow(/Error: metric names must be defined in VALID_METRIC_NAMES/)
   })
 
 })
