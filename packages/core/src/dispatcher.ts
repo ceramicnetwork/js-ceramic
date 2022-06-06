9966b6d6f1c1e6614f03d8447e05ab42f3b1972f
0xJovonni <4885245+Jovonni@users.noreply.github.com>
2022-06-06 09:47:52 -0400

    updated comment

--- packages/core/src/dispatcher.ts
+++ packages/core/src/dispatcher.ts
@@ -309,7 +309,7 @@ export class Dispatcher {
    */
   async _handleUpdateMessage(message: UpdateMessage): Promise<void> {
     // TODO Add validation the message adheres to the proper format.
-    // TODO model isn't used in update yet, will be in later versions
+    // TODO(NET-1527) model isn't used in update yet, will be in later versions
     const { stream: streamId, tip, model } = message
     // TODO: add cache of cids here so that we don't emit event
     // multiple times if we get the message from more than one peer.
