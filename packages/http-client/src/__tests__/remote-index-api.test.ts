10c150eae7e81cbf332f8ebe4844957b7ee27b0a
Sergey Ukustov <sergey@ukstv.me>
2022-06-06 19:26:50 +0300

    feat: Test RemoteIndexAPI

---
+++ packages/http-client/src/__tests__/remote-index-api.test.ts
@@ -0,0 +1,51 @@
+import { jest } from '@jest/globals'
+import { RemoteIndexApi } from '../remote-index-api.js'
+import { CommitType, fetchJson, StreamState, TestUtils } from '@ceramicnetwork/common'
+import { StreamID } from '@ceramicnetwork/streamid'
+
+const FAUX_ENDPOINT = new URL('https://example.com')
+const MODEL = new StreamID(1, TestUtils.randomCID())
+
+const EMPTY_RESPONSE = {
+  entries: [],
+  pageInfo: {},
+}
+const FAUX_STREAM_STATE = {
+  type: 0,
+  log: [
+    {
+      type: CommitType.GENESIS,
+      cid: TestUtils.randomCID(),
+    },
+  ],
+} as unknown as StreamState
+
+test('model in query', async () => {
+  const fauxFetch = jest.fn(async () => EMPTY_RESPONSE) as typeof fetchJson
+  const indexApi = new RemoteIndexApi(FAUX_ENDPOINT, fauxFetch)
+  const result = await indexApi.queryIndex({ model: MODEL, first: 5 })
+  expect(result).toEqual(EMPTY_RESPONSE)
+  expect(fauxFetch).toBeCalledWith(new URL(`https://example.com/collection?model=${MODEL}&first=5`))
+})
+
+test('model, account in query', async () => {
+  const fauxFetch = jest.fn(async () => EMPTY_RESPONSE) as typeof fetchJson
+  const indexApi = new RemoteIndexApi(FAUX_ENDPOINT, fauxFetch)
+  const result = await indexApi.queryIndex({ model: MODEL, account: 'did:key:foo', first: 5 })
+  expect(result).toEqual(EMPTY_RESPONSE)
+  expect(fauxFetch).toBeCalledWith(
+    new URL(
+      `https://example.com/collection?model=${MODEL}&account=${encodeURIComponent(
+        'did:key:foo'
+      )}&first=5`
+    )
+  )
+})
+
+test('serialize stream stte', async () => {
+  const response = { ...EMPTY_RESPONSE, entries: [FAUX_STREAM_STATE] }
+  const fauxFetch = jest.fn(async () => response) as typeof fetchJson
+  const indexApi = new RemoteIndexApi(FAUX_ENDPOINT, fauxFetch)
+  const result = await indexApi.queryIndex({ model: MODEL, account: 'did:key:foo', first: 5 })
+  expect(result.entries[0]).toEqual(FAUX_STREAM_STATE)
+})
--- packages/http-client/src/remote-index-api.ts
+++ packages/http-client/src/remote-index-api.ts
@@ -1,9 +1,29 @@
 import type { BaseQuery, IndexApi, Page, Pagination, StreamState } from '@ceramicnetwork/common'
+import { StreamUtils, fetchJson } from '@ceramicnetwork/common'
 
+/**
+ * IndexAPI implementation on top of HTTP endpoint.
+ */
 export class RemoteIndexApi implements IndexApi {
-  constructor(private readonly _apiUrl: URL) {}
+  private readonly _collectionURL: URL
 
+  constructor(apiUrl: URL, private readonly fetchJsonFn: typeof fetchJson = fetchJson) {
+    this._collectionURL = new URL('./collection', apiUrl)
+  }
+
+  /**
+   * Issue a query to `/collection` endpoint.
+   */
   async queryIndex(query: BaseQuery & Pagination): Promise<Page<StreamState>> {
-    throw new Error(`NotImplemented: RemoteIndexApi::queryIndex`)
+    const queryURL = new URL(this._collectionURL)
+    for (const key in query) {
+      queryURL.searchParams.set(key, query[key])
+    }
+    const response = await this.fetchJsonFn(queryURL)
+    const entries = response.entries.map(StreamUtils.deserializeState)
+    return {
+      entries: entries,
+      pageInfo: response.pageInfo,
+    }
   }
 }
