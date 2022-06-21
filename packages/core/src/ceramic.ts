f535abbd5c1c4357a53cfbc887cb94b082fc016b
Sergey Ukustov <sergey@ukstv.me>
2022-06-21 17:26:09 +0300

    feat: warn if runs SQLite in production

--- packages/core/src/ceramic.ts
+++ packages/core/src/ceramic.ts
@@ -46,9 +46,9 @@ import { EthereumAnchorValidator } from './anchor/ethereum/ethereum-anchor-valid
 import * as fs from 'fs'
 import os from 'os'
 import * as path from 'path'
-import { buildIndexing } from './indexing/build-indexing.js'
 import type { DatabaseIndexApi } from './indexing/database-index-api.js'
 import { LocalIndexApi } from './indexing/local-index-api.js'
+import { makeIndexApi } from './initialization/make-index-api'
 
 const DEFAULT_CACHE_LIMIT = 500 // number of streams stored in the cache
 const DEFAULT_QPS_LIMIT = 10 // Max number of pubsub query messages that can be published per second without rate limiting
@@ -143,7 +143,7 @@ export interface CeramicParameters {
  * Protocol options that are derived from the specified Ceramic network name (e.g. "mainnet", "testnet-clay", etc)
  */
 interface CeramicNetworkOptions {
-  name: string // Must be one of the supported network names
+  name: Networks // Must be one of the supported network names
   pubsubTopic: string // The topic that will be used for broadcasting protocol messages
 }
 
@@ -375,15 +375,8 @@ export class Ceramic implements CeramicApi {
     const loggerProvider = config.loggerProvider ?? new LoggerProvider()
     const logger = loggerProvider.getDiagnosticsLogger()
     const pubsubLogger = loggerProvider.makeServiceLogger('pubsub')
-    let indexingApi: DatabaseIndexApi | undefined = undefined
     const networkOptions = Ceramic._generateNetworkOptions(config)
-    if (config.indexing) {
-      indexingApi = buildIndexing(config.indexing)
-    } else {
-      logger.warn(
-        `Indexing is not configured. Please, add the indexing settings to your config file`
-      )
-    }
+    const indexingApi = makeIndexApi(config.indexing, networkOptions.name, logger)
 
     let anchorService = null
     if (!config.gateway) {
---
+++ packages/core/src/initialization/make-index-api.ts
@@ -0,0 +1,42 @@
+import type { DiagnosticsLogger } from '@ceramicnetwork/common'
+import { Networks } from '@ceramicnetwork/common'
+import { buildIndexing, IndexingConfig } from '../indexing/build-indexing.js'
+import { SqliteIndexApi } from '../indexing/sqlite/sqlite-index-api.js'
+import { DatabaseIndexApi } from '../indexing/database-index-api.js'
+
+/**
+ * Returns true if (1) runs on mainnet, (2) uses SQLite, (3) have models to index.
+ */
+function isNonProduction(
+  indexApi: DatabaseIndexApi,
+  network: Networks,
+  modelsToIndex: any
+): boolean {
+  return (
+    network === Networks.MAINNET && // runs on mainnet
+    indexApi instanceof SqliteIndexApi && // uses SQLite
+    Array.isArray(modelsToIndex) && // have models to index
+    modelsToIndex.length > 0
+  )
+}
+
+/**
+ * Make IndexAPI instance. Call `buildIndexing` inside.
+ */
+export function makeIndexApi(
+  indexingConfig: IndexingConfig | undefined,
+  network: Networks,
+  logger: DiagnosticsLogger
+): DatabaseIndexApi | undefined {
+  if (!indexingConfig) {
+    logger.warn(`Indexing is not configured. Please, add the indexing settings to your config file`)
+    return undefined
+  }
+  const indexApi = buildIndexing(indexingConfig)
+  if (isNonProduction(indexApi, network, indexingConfig.models)) {
+    logger.warn(
+      'For production deployments we recommend using Postgres not SQLite as it will scale better'
+    )
+  }
+  return indexApi
+}
