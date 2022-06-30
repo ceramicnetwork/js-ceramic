a85355c3d70ac6a3a5977b3d662f22e0d3113a6e
Sergey Ukustov <sergey@ukstv.me>
2022-06-30 16:39:10 +0300

    feat: Throw if allowQueriesBeforeHistoricalSync is false

--- packages/cli/src/ceramic-daemon.ts
+++ packages/cli/src/ceramic-daemon.ts
@@ -473,6 +473,10 @@ export class CeramicDaemon {
   }
 
   async getCollection(req: Request, res: Response): Promise<void> {
+    if (!this.opts.indexing.allowQueriesBeforeHistoricalSync) {
+      res.statusCode = 503
+      throw new Error(`Index for historical data is not available`)
+    }
     const httpQuery = parseQueryObject(req.query)
     const query = collectionQuery(httpQuery)
     const indexResponse = await this.ceramic.index.queryIndex(query)
