9df3d70bacfdfd139bfe4c72b93af9e9074bc343
Spencer T Brody <spencer+github@3box.io>
2022-07-06 16:22:32 -0400

    chore(core): Update comment (#2303)

--- packages/core/src/indexing/as-table-name.util.ts
+++ packages/core/src/indexing/as-table-name.util.ts
@@ -1,7 +1,7 @@
 import type { StreamID } from '@ceramicnetwork/streamid'
 
 /**
- * Convert StreamID to table name `mid_%`
+ * Convert StreamID to table name for the indexing database.
  */
 export function asTableName(model: StreamID | string): string {
   return model.toString()
