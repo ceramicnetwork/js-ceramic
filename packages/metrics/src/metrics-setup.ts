19ad72a7f6e7180d46112e90db41a05cd75bb61e
Golda Velez <golda@3box.io>
2022-07-21 16:08:49 +0200

    fix: use enum for controlled vocabs

--- packages/metrics/src/metrics-setup.ts
+++ packages/metrics/src/metrics-setup.ts
@@ -7,12 +7,12 @@ import { Resource } from '@opentelemetry/resources'
 // the parameters will be used to distinguish the endpoint, type of call etc
 // be flexible about what we record, as the dependency in the grafana does not see this code
 
-export const METRIC_NAMES = {
-  HTTP_REQUEST: 'http_request',
-  HTTP_RESPONSE: 'http_response',
-  PUBSUB_RECEIVED: 'pubsub_received',
-  PUBSUB_PUBLISHED: 'pubsub_published',
-  STREAM_PINNED: 'stream_pinned',
+export enum METRIC_NAMES {
+  HTTP_REQUEST = 'http_request',
+  HTTP_RESPONSE = 'http_response',
+  PUBSUB_RECEIVED = 'pubsub_received',
+  PUBSUB_PUBLISHED = 'pubsub_published',
+  STREAM_PINNED = 'stream_pinned',
 }
 
 export const UNKNOWN_CALLER = 'Unknown'
