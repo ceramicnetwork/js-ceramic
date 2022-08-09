84af36bf8986e8ab10a9d57a517be068579bae0d
stephhuynh18 <stephhuynh18@users.noreply.github.com>
2022-08-09 14:14:11 -0700

    Add tests and validation for anchor source

--- packages/core/src/anchor/ethereum/ethereum-anchor-validator.ts
+++ packages/core/src/anchor/ethereum/ethereum-anchor-validator.ts
@@ -43,7 +43,15 @@ const BLOCK_THRESHHOLDS = {
   'eip155:1': 1000000000, //mainnet
   'eip155:3': 1000000000, //ropsten
   'eip155:5': 1000000000, //goerli
+  'eip155:100': 1000000000, //gnosis
   'eip155:1337': 1, //ganache
+}
+const ANCHOR_AUTHORS = {
+  'eip155:1': '0xD3f84Cf6Be3DD0EB16dC89c972f7a27B441A39f2', //mainnet
+  'eip155:3': '0xD3f84Cf6Be3DD0EB16dC89c972f7a27B441A39f2', //ropsten
+  'eip155:5': '0xD3f84Cf6Be3DD0EB16dC89c972f7a27B441A39f2', //goerli
+  'eip155:100': '0xD3f84Cf6Be3DD0EB16dC89c972f7a27B441A39f2', //gnosis
+  'eip155:1337': '0xD3f84Cf6Be3DD0EB16dC89c972f7a27B441A39f2', //ganache
 }
 
 /*
@@ -235,7 +243,17 @@ export class EthereumAnchorValidator implements AnchorValidator {
         } must include the version field. AnchorProof blockNumber: ${anchorProof.blockNumber}`
       )
     }
-    //TODO (NET-1657): Add check to validateAnchorInclusion for ensuring contract addresses match the official contract address
+
+    if (
+      anchorProof.version === 1 &&
+      validationResult.txResponse.to !== ANCHOR_AUTHORS[this._chainId]
+    ) {
+      throw new Error(
+        `Anchor was created using address ${
+          validationResult.txResponse.to
+        }. This is not the official anchoring contract address ${ANCHOR_AUTHORS[this._chainId]}`
+      )
+    }
   }
 
   /**
