299fd4e33ed7b654b0422be862b2cec5953e645d
Spencer T Brody <spencer+github@3box.io>
2022-08-12 14:54:02 -0400

    Test that demonstrates stream instance independence behaviors between core and client

--- packages/cli/src/__tests__/ceramic-multi-daemon.test.ts
+++ packages/cli/src/__tests__/ceramic-multi-daemon.test.ts
@@ -152,4 +152,30 @@ describe('Ceramic interop between multiple daemons and http clients', () => {
     await doc2.sync({ sync: SyncOptions.PREFER_CACHE })
     expect(doc2.content).toEqual(updatedContent)
   })
+
+  it('stream instances are independent in core', async () => {
+    const initialContent = { state: 'initial' }
+    const updatedContent = { state: 'updated' }
+    const doc1A = await TileDocument.create(core1, initialContent, null, { anchor: false })
+
+    const doc2 = await TileDocument.load(core2, doc1A.id)
+    await doc2.update(updatedContent, null, {publish:false, anchor:false})
+
+    const doc1B = await TileDocument.load(core1, doc1A.id, {sync: SyncOptions.SYNC_ALWAYS})
+    expect(doc1B.content).toEqual(updatedContent)
+    expect(doc1A.content).toEqual(initialContent)
+  })
+
+  it('stream instances are independent in http client', async () => {
+    const initialContent = { state: 'initial' }
+    const updatedContent = { state: 'updated' }
+    const doc1A = await TileDocument.create(client1, initialContent, null, { anchor: false })
+
+    const doc2 = await TileDocument.load(client2, doc1A.id)
+    await doc2.update(updatedContent, null, {publish:false, anchor:false})
+
+    const doc1B = await TileDocument.load(client1, doc1A.id, {sync: SyncOptions.SYNC_ALWAYS})
+    expect(doc1B.content).toEqual(updatedContent)
+    expect(doc1A.content).toEqual(initialContent)
+  })
 })
