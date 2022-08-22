866141b06563465be1891bddb0803961eb604efd
Joel Thorstensson <joel@oed.earth>
2022-08-22 08:56:10 +0100

    fix(stream-tests): model test

--- packages/stream-tests/src/__tests__/model.test.ts
+++ packages/stream-tests/src/__tests__/model.test.ts
@@ -57,18 +57,6 @@ describe('Model API http-client tests', () => {
     expect(model.state.anchorStatus).toEqual(AnchorStatus.PENDING)
   })
 
-  test('Create and update placeholder', async () => {
-    const model = await Model.createPlaceholder(ceramic, PLACEHOLDER_CONTENT)
-    expect(JSON.stringify(model.content)).toEqual(JSON.stringify(PLACEHOLDER_CONTENT))
-
-    await model.replacePlaceholder(FINAL_CONTENT)
-
-    expect(JSON.stringify(model.content)).toEqual(JSON.stringify(FINAL_CONTENT))
-    expect(model.state.log.length).toEqual(2)
-    expect(model.state.log[0].type).toEqual(CommitType.GENESIS)
-    expect(model.state.log[1].type).toEqual(CommitType.SIGNED)
-  })
-
   test('Anchor genesis', async () => {
     const model = await Model.create(ceramic, FINAL_CONTENT)
     expect(model.state.anchorStatus).toEqual(AnchorStatus.PENDING)
@@ -80,23 +68,6 @@ describe('Model API http-client tests', () => {
     expect(model.state.log.length).toEqual(2)
     expect(model.state.log[0].type).toEqual(CommitType.GENESIS)
     expect(model.state.log[1].type).toEqual(CommitType.ANCHOR)
-    expect(JSON.stringify(model.content)).toEqual(JSON.stringify(FINAL_CONTENT))
-  })
-
-  test('Anchor after updating placeholder', async () => {
-    const model = await Model.createPlaceholder(ceramic, PLACEHOLDER_CONTENT)
-    expect(model.state.anchorStatus).toEqual(AnchorStatus.NOT_REQUESTED)
-    await model.replacePlaceholder(FINAL_CONTENT)
-    expect(model.state.anchorStatus).toEqual(AnchorStatus.PENDING)
-
-    await TestUtils.anchorUpdate(core, model)
-    await model.sync()
-
-    expect(model.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
-    expect(model.state.log.length).toEqual(3)
-    expect(model.state.log[0].type).toEqual(CommitType.GENESIS)
-    expect(model.state.log[1].type).toEqual(CommitType.SIGNED)
-    expect(model.state.log[2].type).toEqual(CommitType.ANCHOR)
     expect(JSON.stringify(model.content)).toEqual(JSON.stringify(FINAL_CONTENT))
   })
 
@@ -113,56 +84,15 @@ describe('Model API http-client tests', () => {
     )
   })
 
-  test('Cannot update placeholder and leave it incomplete', async () => {
-    const model = await Model.createPlaceholder(ceramic, PLACEHOLDER_CONTENT)
-
-    const updatedContent = { ...PLACEHOLDER_CONTENT, accountRelation: ModelAccountRelation.LIST }
-    await expect(model.replacePlaceholder(updatedContent)).rejects.toThrow(
-      /missing a 'schema' field/
-    )
-  })
-
-  test('Cannot update a finalized model', async () => {
-    const model = await Model.createPlaceholder(ceramic, PLACEHOLDER_CONTENT)
-    await model.replacePlaceholder(FINAL_CONTENT)
-
-    const updatedContent = { ...FINAL_CONTENT, description: 'Adding a description' }
-    await expect(model.replacePlaceholder(updatedContent)).rejects.toThrow(
-      /Cannot update a finalized Model/
-    )
-  })
-
-  test('Existing fields can be updated when updating a placeholder', async () => {
-    const model = await Model.createPlaceholder(ceramic, PLACEHOLDER_CONTENT)
-    const finalContent = {
-      ...FINAL_CONTENT,
-      name: 'newName',
-      description: 'Also add a description',
-    }
-
-    await model.replacePlaceholder(finalContent)
-
-    expect(JSON.stringify(model.content)).toEqual(JSON.stringify(finalContent))
-  })
-
-  test('Cannot load a placeholder', async () => {
-    const model = await Model.createPlaceholder(ceramic, PLACEHOLDER_CONTENT)
-
-    await expect(Model.load(ceramic, model.id)).rejects.toThrow(
-      /Incomplete placeholder Models cannot be loaded/
-    )
-  })
-
   test('Can load a complete stream', async () => {
-    const model = await Model.createPlaceholder(ceramic, PLACEHOLDER_CONTENT)
-    await model.replacePlaceholder(FINAL_CONTENT)
+    const model = await Model.create(ceramic, FINAL_CONTENT)
     await TestUtils.anchorUpdate(core, model)
     await model.sync()
 
     const loaded = await Model.load(ceramic, model.id)
 
     expect(loaded.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
-    expect(loaded.state.log.length).toEqual(3)
+    expect(loaded.state.log.length).toEqual(2)
     expect(JSON.stringify(loaded.state)).toEqual(JSON.stringify(model.state))
   })
 })
@@ -206,39 +136,14 @@ describe('Model API multi-node tests', () => {
     expect(JSON.stringify(loadedState)).toEqual(JSON.stringify(modelState))
   })
 
-  test('load placeholder over network fails', async () => {
-    const model = await Model.createPlaceholder(ceramic0, PLACEHOLDER_CONTENT)
-
-    await expect(Model.load(ceramic1, model.id)).rejects.toThrow(
-      /Incomplete placeholder Models cannot be loaded/
-    )
-  })
-
-  test('load updated model', async () => {
-    const model = await Model.createPlaceholder(ceramic0, PLACEHOLDER_CONTENT)
-    await model.replacePlaceholder(FINAL_CONTENT)
-
-    const loaded = await Model.load(ceramic1, model.id)
-
-    const modelState = model.state
-    const loadedState = loaded.state
-    expect(modelState.anchorStatus).toEqual(AnchorStatus.PENDING)
-    expect(loadedState.anchorStatus).toEqual(AnchorStatus.NOT_REQUESTED)
-    delete modelState.anchorStatus
-    delete loadedState.anchorStatus
-    expect(loadedState.log.length).toEqual(2)
-    expect(JSON.stringify(loadedState)).toEqual(JSON.stringify(modelState))
-  })
-
-  test('load updated and anchored model', async () => {
-    const model = await Model.createPlaceholder(ceramic0, PLACEHOLDER_CONTENT)
-    await model.replacePlaceholder(FINAL_CONTENT)
+  test('load anchored model', async () => {
+    const model = await Model.create(ceramic0, FINAL_CONTENT)
     await TestUtils.anchorUpdate(ceramic0, model)
 
     const loaded = await Model.load(ceramic1, model.id)
 
     expect(loaded.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
-    expect(loaded.state.log.length).toEqual(3)
+    expect(loaded.state.log.length).toEqual(2)
     expect(JSON.stringify(loaded.state)).toEqual(JSON.stringify(model.state))
   })
 })
