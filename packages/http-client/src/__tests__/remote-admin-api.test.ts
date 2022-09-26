9172de0be98d3ed217de9349c479fe89f8dc64cb
Artur Wdowiarski <arturwdowiarski@me.com>
2022-09-26 14:19:18 +0200

    Added authorization headers to remote admin api calls

--- packages/http-client/src/__tests__/remote-admin-api.test.ts
+++ packages/http-client/src/__tests__/remote-admin-api.test.ts
@@ -1,7 +1,11 @@
+import { DID } from 'dids'
 import { StreamID } from '@ceramicnetwork/streamid'
-import { fetchJson, TestUtils } from '@ceramicnetwork/common'
+import { Context, fetchJson, TestUtils } from '@ceramicnetwork/common'
 import { RemoteAdminApi } from '../remote-admin-api.js'
 import { jest } from '@jest/globals'
+import { Ed25519Provider } from 'key-did-provider-ed25519'
+import KeyResolver from 'key-did-resolver'
+import { randomBytes } from '@stablelib/random'
 
 const FAUX_ENDPOINT = new URL('https://example.com')
 const MODEL = new StreamID(1, TestUtils.randomCID())
@@ -12,22 +16,41 @@ const GET_RESPONSE = {
   models: [MODEL.toString()]
 }
 
+let FAUX_CONTEXT: Context
+
+beforeAll(async () => {
+  const seed = randomBytes(32)
+  const provider = new Ed25519Provider(seed)
+  const did = new DID({ provider, resolver: KeyResolver.getResolver() })
+  await did.authenticate()
+  FAUX_CONTEXT = { did: did } as Context
+})
+
 test('getIndexedModels()', async () => {
-  const adminApi = new RemoteAdminApi(FAUX_ENDPOINT)
+  const adminApi = new RemoteAdminApi(FAUX_ENDPOINT, FAUX_CONTEXT)
+  // @ts-ignore
+  adminApi.buildAuthorizationHeader = (): string => {
+    return '<FAKE AUTH HEADER>'
+  }
   const fauxFetch = jest.fn(async () => GET_RESPONSE) as typeof fetchJson
   (adminApi as any)._fetchJson = fauxFetch
   await adminApi.getIndexedModels()
-  expect(fauxFetch).toBeCalledWith(new URL(`https://example.com/admin/models`))
+  expect(fauxFetch).toBeCalledWith(new URL(`https://example.com/admin/models`), {"headers": {"Authorization:": "Basic <FAKE AUTH HEADER>"}})
 })
 
 test('addModelsToIndex()', async () => {
-  const adminApi = new RemoteAdminApi(FAUX_ENDPOINT)
+  const adminApi = new RemoteAdminApi(FAUX_ENDPOINT, FAUX_CONTEXT)
+  // @ts-ignore
+  adminApi.buildAuthorizationHeader = (): string => {
+    return '<FAKE AUTH HEADER>'
+  }
   const fauxFetch = jest.fn(async () => SUCCESS_RESPONSE) as typeof fetchJson
   (adminApi as any)._fetchJson = fauxFetch
   await adminApi.addModelsToIndex([MODEL])
   expect(fauxFetch).toBeCalledWith(
     new URL(`https://example.com/admin/models`),
     {
+      headers: {"Authorization:": "Basic <FAKE AUTH HEADER>" },
       method: 'post',
       body: { models: [MODEL.toString()] },
     }
@@ -35,13 +58,18 @@ test('addModelsToIndex()', async () => {
 })
 
 test('removeModelsFromIndex()', async () => {
-  const adminApi = new RemoteAdminApi(FAUX_ENDPOINT)
+  const adminApi = new RemoteAdminApi(FAUX_ENDPOINT, FAUX_CONTEXT)
+  // @ts-ignore
+  adminApi.buildAuthorizationHeader = (): string => {
+    return '<FAKE AUTH HEADER>'
+  }
   const fauxFetch = jest.fn(async () => SUCCESS_RESPONSE) as typeof fetchJson
   (adminApi as any)._fetchJson = fauxFetch
   await adminApi.removeModelsFromIndex([MODEL])
   expect(fauxFetch).toBeCalledWith(
     new URL(`https://example.com/admin/models`),
     {
+      headers: { "Authorization:": "Basic <FAKE AUTH HEADER>" },
       method: 'delete',
       body: { models: [MODEL.toString()] },
     }
@@ -49,13 +77,18 @@ test('removeModelsFromIndex()', async () => {
 })
 
 test('replaceModelsInIndex()', async () => {
-  const adminApi = new RemoteAdminApi(FAUX_ENDPOINT)
+  const adminApi = new RemoteAdminApi(FAUX_ENDPOINT, FAUX_CONTEXT)
+  // @ts-ignore
+  adminApi.buildAuthorizationHeader = (): string => {
+    return '<FAKE AUTH HEADER>'
+  }
   const fauxFetch = jest.fn(async () => SUCCESS_RESPONSE) as typeof fetchJson
   (adminApi as any)._fetchJson = fauxFetch
   await adminApi.replaceModelsInIndex([MODEL])
   expect(fauxFetch).toBeCalledWith(
     new URL(`https://example.com/admin/models`),
     {
+      headers: { "Authorization:": "Basic <FAKE AUTH HEADER>" },
       method: 'put',
       body: { models: [MODEL.toString()] },
     }
--- packages/http-client/src/remote-admin-api.ts
+++ packages/http-client/src/remote-admin-api.ts
@@ -1,4 +1,4 @@
-import { AdminApi, fetchJson, Stream, StreamState, StreamUtils, Context } from '@ceramicnetwork/common'
+import { AdminApi, fetchJson, Context } from '@ceramicnetwork/common'
 import { StreamID } from '@ceramicnetwork/streamid'
 
 /**
@@ -7,33 +7,49 @@ import { StreamID } from '@ceramicnetwork/streamid'
 export class RemoteAdminApi implements AdminApi {
   // Stored as a member to make it easier to inject a mock in unit tests
   private readonly _fetchJson: typeof fetchJson = fetchJson
+
   readonly baseURL = './admin/models'
 
   constructor(
-    private readonly _apiUrl: URL
+    private readonly _apiUrl: URL,
+    private readonly _context: Context
   ) {}
 
   private getUrl(): URL {
     return new URL(this.baseURL, this._apiUrl)
   }
 
+  private async buildAuthorizationHeader(modelsIDs?: Array<StreamID>): Promise<string> {
+    const body = modelsIDs ? modelsIDs.map(streamID => streamID.toString()) : undefined
+    const jws = await this._context.did.createJWS({
+      timestamp: Date.now(),
+      requestPath: this._apiUrl.pathname,
+      requestBody: body
+    })
+    return `${jws.signatures[0].protected}.${jws.payload}.${jws.signatures[0].signature}`
+  }
+
   async addModelsToIndex(modelsIDs: Array<StreamID>): Promise<void> {
     await this._fetchJson(this.getUrl(), {
+      headers: { 'Authorization:': `Basic ${await this.buildAuthorizationHeader(modelsIDs)}` },
       method: 'post',
       body: { models: modelsIDs.map(modelID => modelID.toString()) },
     })
   }
 
   async getIndexedModels(): Promise<Array<StreamID>> {
-    
-    const response= await this._fetchJson(this.getUrl())
+    const response= await this._fetchJson(this.getUrl(), {
+      headers: { 'Authorization:': `Basic ${await this.buildAuthorizationHeader()}` },
+    })
     return response.models.map((modelStreamIDString: string) => {
       return StreamID.fromString(modelStreamIDString)
     })
+
   }
 
   async removeModelsFromIndex(modelsIDs: Array<StreamID>): Promise<void> {
     await this._fetchJson(this.getUrl(), {
+      headers: { 'Authorization:': `Basic ${await this.buildAuthorizationHeader(modelsIDs)}` },
       method: 'delete',
       body: { models: modelsIDs.map(modelID => modelID.toString()) },
     })
@@ -41,6 +57,7 @@ export class RemoteAdminApi implements AdminApi {
 
   async replaceModelsInIndex(modelsIDs: Array<StreamID>): Promise<void> {
     await this._fetchJson(this.getUrl(), {
+      headers: { 'Authorization:': `Basic ${await this.buildAuthorizationHeader(modelsIDs)}` },
       method: 'put',
       body: { models: modelsIDs.map(modelID => modelID.toString()) },
     })
