3f7f74a8b70df3b7bb23f88592148ef35521c099
Sergey Ukustov <sergey@ukstv.me>
2022-09-28 10:13:42 +0300

    chore: countURL

--- packages/http-client/src/remote-index-api.ts
+++ packages/http-client/src/remote-index-api.ts
@@ -15,13 +15,15 @@ export class RemoteIndexApi implements IndexApi {
   // Stored as a member to make it easier to inject a mock in unit tests
   private readonly _fetchJson: typeof fetchJson = fetchJson
   private readonly _collectionURL: URL
+  private readonly _countURL: URL
 
   constructor(apiUrl: URL) {
-    this._collectionURL = new URL('./collection/', apiUrl)
+    this._collectionURL = new URL('./collection', apiUrl)
+    this._countURL = new URL('./collection/count', apiUrl)
   }
 
   async count(query: BaseQuery): Promise<number> {
-    const queryURL = serializeObjectToSearchParams(new URL('./count', this._collectionURL), query)
+    const queryURL = serializeObjectToSearchParams(this._countURL, query)
     const response = await this._fetchJson(queryURL)
     return response.count
   }
@@ -30,7 +32,7 @@ export class RemoteIndexApi implements IndexApi {
    * Issue a query to `/collection` endpoint.
    */
   async query(query: PaginationQuery): Promise<Page<StreamState | null>> {
-    const queryURL = serializeObjectToSearchParams(new URL(this._collectionURL), query)
+    const queryURL = serializeObjectToSearchParams(this._collectionURL, query)
 
     const response = await this._fetchJson(queryURL)
     const edges = response.edges.map((e) => {
