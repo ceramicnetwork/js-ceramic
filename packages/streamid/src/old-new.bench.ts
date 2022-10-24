a3fcd65cca9cf81b33e639924b3832fed32f0da5
Sergey Ukustov <sergey@ukstv.me>
2022-10-24 16:33:46 +0300

    chore: Benchmark old-new implementations

---
+++ packages/streamid/src/old-new.bench.ts
@@ -0,0 +1,38 @@
+import benchmark from 'benchmark'
+import { TestUtils } from '@ceramicnetwork/common'
+import * as present from './index.js'
+import * as old from '../../../node_modules/@ceramicnetwork/streamid/lib/index.js'
+
+const suite = new benchmark.Suite()
+
+function formatNumber(number: number) {
+  return String(number).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+suite
+  .add('old.StreamId.fromString', () => {
+    const streamId = new old.StreamID(1, TestUtils.randomCID())
+    old.StreamID.fromString(streamId.toString())
+  })
+  .add('present.StreamId.fromString', () => {
+    const streamId = new present.StreamID(1, TestUtils.randomCID())
+    present.StreamID.fromString(streamId.toString())
+  })
+  .add('old.StreamId.fromBytes', () => {
+    const streamId = new old.StreamID(1, TestUtils.randomCID())
+    old.StreamID.fromBytes(streamId.bytes)
+  })
+  .add('present.StreamId.fromBytes', () => {
+    const streamId = new present.StreamID(1, TestUtils.randomCID())
+    present.StreamID.fromBytes(streamId.bytes)
+  })
+  .on('cycle', (event: any) => {
+    const name = event.target.name.padEnd('emit  '.length)
+    const hz = formatNumber(event.target.hz.toFixed(0)).padStart(9)
+    process.stdout.write(`${name}${hz} ops/sec\n`)
+  })
+  .on('error', (event: any) => {
+    process.stderr.write(event.target.error.toString() + '\n')
+    process.exit(1)
+  })
+  .run()
