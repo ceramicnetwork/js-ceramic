49e8b6edc638610ad0762b69bd0998cda353cd88
Spencer T Brody <spencer+github@3box.io>
2022-10-26 10:10:10 -0400

    use helper function

--- packages/stream-handler-common/src/anchor-commit-utils.ts
+++ packages/stream-handler-common/src/anchor-commit-utils.ts
@@ -7,6 +7,26 @@ import {
 } from '@ceramicnetwork/common'
 import cloneDeep from 'lodash.clonedeep'
 
+/**
+ * Mutates the input StreamState object to apply anchor timestamps to all commits that they anchor.
+ * We do this for more than just the most recent anchor commit to fix-up the state objects
+ * stored in the state store for streams that were created and pinned before this logic was in
+ * place.  In the future, we may consider only propagating the timestamp from the anchor commit
+ * being applied to the commits that it anchors, up until the previous anchor commit, and then
+ * stopping, so we don't have to iterate over the whole log.
+ */
+function applyAnchorTimestampsToLog(state: StreamState): void {
+  let timestamp
+  for (let i = state.log.length - 1; i >= 0; i--) {
+    const logEntry = state.log[i]
+    if (logEntry.type == CommitType.ANCHOR) {
+      timestamp = logEntry.timestamp
+    } else {
+      logEntry.timestamp = timestamp
+    }
+  }
+}
+
 export async function applyAnchorCommit(
   commitData: CommitData,
   state: StreamState
@@ -23,21 +43,7 @@ export async function applyAnchorCommit(
     timestamp: commitData.proof.blockTimestamp,
   })
 
-  // Apply anchor timestamps to all commits that they anchor
-  // We do this for more than just the most recent anchor commit to fix-up the state objects
-  // stored in the state store for streams that were created and pinned before this logic was in
-  // place.  In the future, we may consider only propagating the timestamp from the anchor commit
-  // being applied to the commits that it anchors, up until the previous anchor commit, and then
-  // stopping, so we don't have to iterate over the whole log.
-  let timestamp
-  for (let i = state.log.length - 1; i >= 0; i--) {
-    const logEntry = state.log[i]
-    if (logEntry.type == CommitType.ANCHOR) {
-      timestamp = logEntry.timestamp
-    } else {
-      logEntry.timestamp = timestamp
-    }
-  }
+  applyAnchorTimestampsToLog(state)
 
   if (state.next?.content) {
     state.content = state.next.content
