c90a856b6578e3b0d6265c8e86187e3fa06b0bbf
Artur Wdowiarski <arturwdowiarski@me.com>
2022-11-02 16:15:08 +0000

    Paralellised table operations in sqlite database api

--- packages/core/src/indexing/sqlite/init-tables.ts
+++ packages/core/src/indexing/sqlite/init-tables.ts
@@ -35,8 +35,8 @@ export async function listMidTables(dbConnection: Knex): Promise<Array<string>> 
   return result.map((r) => r.name)
 }
 
-export async function listConfigTables(): Promise<Array<ConfigTable>> {
-  // TODO (CDB-1852): extend with ceramic_auth
+export function listConfigTables(): Array<ConfigTable> {
+  // TODO (CDB-1852): extend with ceramic_auth; If it will need to be async, see if it can be parallelised within initConfigTables(...)
   return [{ tableName: INDEXED_MODEL_CONFIG_TABLE_NAME, validSchema: CONFIG_TABLE_MODEL_INDEX_STRUCTURE }]
 }
 
@@ -54,17 +54,19 @@ function relationsDefinitionsToColumnInfo(relations?: ModelRelationsDefinition):
  */
 export async function initConfigTables(dataSource: Knex, logger: DiagnosticsLogger) {
   const configTables = await listConfigTables()
-  for (const table of configTables) {
-    const exists = await dataSource.schema.hasTable(table.tableName)
-    if (!exists) {
-      logger.imp(`Creating Compose DB config table: ${table.tableName}`)
-      await createConfigTable(dataSource, table.tableName)
-    }
+  await Promise.all(configTables.map( table => { return initConfigTable(table, dataSource, logger) } ))
+}
+
+async function initConfigTable(table: ConfigTable, dataSource: Knex, logger: DiagnosticsLogger) {
+  const exists = await dataSource.schema.hasTable(table.tableName)
+  if (!exists) {
+    logger.imp(`Creating Compose DB config table: ${table.tableName}`)
+    await createConfigTable(dataSource, table.tableName)
   }
 }
 
 /**
- * Create mid tables and corresponding indexes.
+ * Create mid tables and corresponding indexes.n
  */
 export async function initMidTables(
   dbConnection: Knex,
@@ -72,15 +74,19 @@ export async function initMidTables(
   logger: DiagnosticsLogger
 ) {
   const existingTables = await listMidTables(dbConnection)
-  for (const modelIndexArgs of modelsToIndex) {
-    const tableName = asTableName(modelIndexArgs.model)
-    if (existingTables.includes(tableName)) {
-      continue
-    }
-    logger.imp(`Creating Compose DB Indexing table for model: ${tableName}`)
-    const relationColumns = relationsDefinitionsToColumnInfo(modelIndexArgs.relations)
-    await createModelTable(dbConnection, tableName, relationColumns)
+  await Promise.all(modelsToIndex.map( modelIndexArgs => {
+    return initMidTable(modelIndexArgs, existingTables, dbConnection, logger)
+  }))
+}
+
+async function initMidTable(modelIndexArgs: IndexModelArgs, existingTables: Array<string>, dbConnection: Knex, logger: DiagnosticsLogger) {
+  const tableName = asTableName(modelIndexArgs.model)
+  if (existingTables.includes(tableName)) {
+    return
   }
+  logger.imp(`Creating Compose DB Indexing table for model: ${tableName}`)
+  const relationColumns = relationsDefinitionsToColumnInfo(modelIndexArgs.relations)
+  await createModelTable(dbConnection, tableName, relationColumns)
 }
 
 /**
@@ -88,18 +94,22 @@ export async function initMidTables(
  * @param dataSource
  * @param modelsToIndex
  */
-async function _verifyConfigTables(dataSource: Knex, modelsToIndex: Array<IndexModelArgs>) {
-  const configTables = await listConfigTables()
-  for (const table of configTables) {
-    const columns = await dataSource.table(table.tableName).columnInfo()
-    const validSchema = JSON.stringify(table.validSchema)
+async function _verifyConfigTables(dataSource: Knex) {
+  const configTables = listConfigTables()
+  await Promise.all(configTables.map( configTable => {
+    return _verifyConfigTable(configTable, dataSource)
+  }))
+}
 
-    if (validSchema != JSON.stringify(columns)) {
-      throw new Error(
-        `Schema verification failed for config table: ${table.tableName}. Please make sure node has been setup correctly.`
-      )
-      process.exit(-1)
-    }
+async function _verifyConfigTable(table: ConfigTable, dataSource: Knex) {
+  const columns = await dataSource.table(table.tableName).columnInfo()
+  const validSchema = JSON.stringify(table.validSchema)
+
+  if (validSchema != JSON.stringify(columns)) {
+    throw new Error(
+      `Schema verification failed for config table: ${table.tableName}. Please make sure node has been setup correctly.`
+    )
+    process.exit(-1)
   }
 }
 
@@ -109,31 +119,34 @@ async function _verifyConfigTables(dataSource: Knex, modelsToIndex: Array<IndexM
  * @param modelsToIndex
  */
 async function _verifyMidTables(dataSource: Knex, modelsToIndex: Array<IndexModelArgs>) {
-  const tables = await listMidTables(dataSource)
+  const tableNames = await listMidTables(dataSource)
+  await Promise.all(tableNames.map( tableName => {
+    return _verifyMidTable(tableName, dataSource, modelsToIndex)
+  }))
+}
 
-  for (const tableName of tables) {
-    const modelIndexArgs = modelsToIndex.find((model) => tableName == asTableName(model.model))
-    if (!modelIndexArgs) {
-      // TODO: CDB-1869 - This means that there's is a table for a model that is no longer indexed. Should this table have been deleted?
-      continue
-    }
+async function _verifyMidTable(tableName: string, dataSource: Knex, modelsToIndex: Array<IndexModelArgs>) {
+  const modelIndexArgs = modelsToIndex.find((model) => tableName == asTableName(model.model))
+  if (!modelIndexArgs) {
+    // TODO: CDB-1869 - This means that there's is a table for a model that is no longer indexed. Should this table have been deleted?
+    return
+  }
 
-    // Clone the COMMON_TABLE_STRUCTURE object that has the fields expected for all tables so we can
-    // extend it with the model-specific fields expected
-    const expectedTableStructure = Object.assign({}, COMMON_TABLE_STRUCTURE)
-    if (modelIndexArgs.relations) {
-      for (const relation of Object.keys(modelIndexArgs.relations)) {
-        expectedTableStructure[relation] = RELATION_COLUMN_STRUCTURE
-      }
+  // Clone the COMMON_TABLE_STRUCTURE object that has the fields expected for all tables so we can
+  // extend it with the model-specific fields expected
+  const expectedTableStructure = Object.assign({}, COMMON_TABLE_STRUCTURE)
+  if (modelIndexArgs.relations) {
+    for (const relation of Object.keys(modelIndexArgs.relations)) {
+      expectedTableStructure[relation] = RELATION_COLUMN_STRUCTURE
     }
-    const validSchema = JSON.stringify(expectedTableStructure)
+  }
+  const validSchema = JSON.stringify(expectedTableStructure)
 
-    const columns = await dataSource.table(tableName).columnInfo()
-    if (validSchema != JSON.stringify(columns)) {
-      throw new Error(
-        `Schema verification failed for index: ${tableName}. Please make sure latest migrations have been applied.`
-      )
-    }
+  const columns = await dataSource.table(tableName).columnInfo()
+  if (validSchema != JSON.stringify(columns)) {
+    throw new Error(
+      `Schema verification failed for index: ${tableName}. Please make sure latest migrations have been applied.`
+    )
   }
 }
 
@@ -145,7 +158,7 @@ async function _verifyMidTables(dataSource: Knex, modelsToIndex: Array<IndexMode
 // TODO (NET-1635): unify logic between postgres & sqlite
 export async function verifyTables(dataSource: Knex, modelsToIndex: Array<IndexModelArgs>) {
   await Promise.all([
-    _verifyConfigTables(dataSource, modelsToIndex),
+    _verifyConfigTables(dataSource),
     _verifyMidTables(dataSource, modelsToIndex)
   ])
 }
