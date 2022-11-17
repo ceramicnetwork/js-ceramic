99d9787eb5a8cc654959d0d574818bf42811fd5f
Golda Velez <gvelez17@gmail.com>
2022-11-16 19:40:19 -0700

    update the dependency in core from metrics to observability (#2562)
    * update the dependency in core from metrics to observability
    
    * leave metrics package in place for now
    
    * updated metric name

--- packages/core/src/pubsub/pubsub-message.ts
+++ packages/core/src/pubsub/pubsub-message.ts
@@ -1,7 +1,7 @@
 import { StreamID } from '@ceramicnetwork/streamid'
 import { CID } from 'multiformats/cid'
 import { UnreachableCaseError, toCID } from '@ceramicnetwork/common'
-import { Metrics, METRIC_NAMES } from '@ceramicnetwork/metrics'
+import { ServiceMetrics as Metrics } from '@ceramicnetwork/observability'
 import * as dagCBOR from '@ipld/dag-cbor'
 import { create as createDigest } from 'multiformats/hashes/digest'
 import * as sha256 from '@stablelib/sha256'
@@ -48,6 +48,9 @@ export type PubsubMessage = UpdateMessage | QueryMessage | ResponseMessage | Kee
 const textEncoder = new TextEncoder()
 const textDecoder = new TextDecoder('utf-8')
 
+const PUBSUB_PUBLISHED = 'pubsub_published'
+const PUBSUB_RECEIVED = 'pubsub_received'
+
 function messageHash(message: any): string {
   // DAG-CBOR encoding
   const encoded = dagCBOR.encode(message)
@@ -72,7 +75,7 @@ export function buildQueryMessage(streamId: StreamID): QueryMessage {
 }
 
 export function serialize(message: PubsubMessage): Uint8Array {
-  Metrics.count(METRIC_NAMES.PUBSUB_PUBLISHED, 1, { typ: message.typ }) // really attempted to publish...
+  Metrics.count(PUBSUB_PUBLISHED, 1, { typ: message.typ }) // really attempted to publish...
   switch (message.typ) {
     case MsgType.QUERY: {
       return textEncoder.encode(
@@ -121,7 +124,7 @@ export function deserialize(message: any): PubsubMessage {
   const parsed = JSON.parse(asString)
 
   const typ = parsed.typ as MsgType
-  Metrics.count(METRIC_NAMES.PUBSUB_RECEIVED, 1, { typ: typ })
+  Metrics.count(PUBSUB_RECEIVED, 1, { typ: typ })
   switch (typ) {
     case MsgType.UPDATE: {
       // TODO don't take streamid from 'doc' once we no longer interop with nodes older than v1.0.0
