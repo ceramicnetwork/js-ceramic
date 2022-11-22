a0170a27512a51109cb9c077b3cdb6e5d913c2a6
Golda Velez <golda@3box.io>
2022-11-22 04:03:44 -0700

    also in instrument requests

--- packages/cli/src/daemon/instrument-requests.ts
+++ packages/cli/src/daemon/instrument-requests.ts
@@ -1,11 +1,13 @@
-import {Metrics, METRIC_NAMES} from '@ceramicnetwork/metrics'
+import {ServiceMetrics as Metrics} from '@ceramicnetwork/observability'
 
 export function instrumentRequests(req, res, next) {
 
   const token = req.header("authorization")
   const agent = req.header("user-agent")
 
-  Metrics.count(METRIC_NAMES.HTTP_REQUEST, 1,
+  const HTTP_REQUEST = 'http_request'
+
+  Metrics.count(HTTP_REQUEST, 1,
        {'method': req.method, 'endpoint':req.url, 'agent':agent, 'package':'cli.daemon'})
   next()
 }
