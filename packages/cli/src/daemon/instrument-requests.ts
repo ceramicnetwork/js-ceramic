d3b211ca71d33062457683203b74f527f59147d7
Golda Velez <golda@3box.io>
2022-12-06 15:49:03 -0700

    linter errors (including unrelated ones)

--- packages/cli/src/daemon/instrument-requests.ts
+++ packages/cli/src/daemon/instrument-requests.ts
@@ -2,12 +2,15 @@ import {ServiceMetrics as Metrics} from '@ceramicnetwork/observability'
 
 export function instrumentRequests(req, res, next) {
 
-  const token = req.header("authorization")
+  // When we move to API tokens, we will need to gather and map this token
+  // to identify the partner
+  // const token = req.header("authorization")
+  
   const agent = req.header("user-agent")
 
   const HTTP_REQUEST = 'http_request'
 
-  const clean_endpoint_re = /(\/streams\/)[^\/\?]+(\?.+)?/
+  const clean_endpoint_re = /(\/streams\/)[^/?]+(\?.+)?/
 
   const endpoint = req.url.replace(clean_endpoint_re, "$1...$2")
 
