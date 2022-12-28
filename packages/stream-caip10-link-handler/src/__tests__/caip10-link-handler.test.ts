e809665078cafaf8be80f1ade2e2920bdca340bb
Spencer T Brody <spencer+github@3box.io>
2022-12-28 11:06:54 -0500

    chore(core): Remove all references to AnchorProof.blockTimestamp (or blockNumber) (#2623)
    Co-authored-by: Joel Thorstensson <oed@3box.io>

--- packages/stream-caip10-link-handler/src/__tests__/caip10-link-handler.test.ts
+++ packages/stream-caip10-link-handler/src/__tests__/caip10-link-handler.test.ts
@@ -280,8 +280,6 @@ describe('Caip10LinkHandler', () => {
       r2: { proof: FAKE_CID_4, id: FAKE_CID_1, prev: FAKE_CID_2 },
       r2proof: {
         value: {
-          blockNumber: 123456,
-          blockTimestamp: 1608116723,
           chainId: 'fakechain:123',
         },
       },
@@ -305,8 +303,6 @@ describe('Caip10LinkHandler', () => {
       r4: { proof: FAKE_CID_7, id: FAKE_CID_1, prev: FAKE_CID_5 },
       r4proof: {
         value: {
-          blockNumber: 123456,
-          blockTimestamp: 1608116727,
           chainId: 'fakechain:123',
         },
       },
@@ -323,7 +319,7 @@ describe('Caip10LinkHandler', () => {
       type: CommitType.ANCHOR,
       commit: commits.r2,
       proof: commits.r2proof.value,
-      timestamp: commits.r2proof.value.blockTimestamp,
+      timestamp: 1608116723,
     }
     state = await handler.applyCommit(anchorCommitData_1, context, state)
     expect(state.content).toEqual('did:3:testdid1')
