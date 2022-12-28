61418d651553225d624f0d51d68841ef5e68b8d5
Spencer T Brody <spencer+github@3box.io>
2022-12-28 10:21:41 -0500

    Update packages/core/src/conflict-resolution.ts
    Co-authored-by: Joel Thorstensson <oed@3box.io>

--- packages/core/src/conflict-resolution.ts
+++ packages/core/src/conflict-resolution.ts
@@ -417,8 +417,8 @@ export class ConflictResolution {
     }
 
     const stateLog = HistoryLog.fromState(this.dispatcher, initialState)
-    const log = await fetchLog(this.dispatcher, tip, stateLog).then((log) =>
-      verifyAnchorAndApplyTimestamps(this.logger, this.dispatcher, this.anchorValidator, log)
+    const logWithoutTimestamps = await fetchLog(this.dispatcher, tip, stateLog)
+    const log = await verifyAnchorAndApplyTimestamps(this.logger, this.dispatcher, this.anchorValidator, logWithoutTimestamps)
     )
 
     if (log.length) {
