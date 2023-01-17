7d759c787b21874d5b2a390d38539e2ba9c717d2
Stephanie Huynh <stephhuynh18@users.noreply.github.com>
2023-01-17 09:23:10 -0800

    feat(core): create rebuild anchor worker (#2646)

--- packages/core/src/sync/interfaces.ts
+++ packages/core/src/sync/interfaces.ts
@@ -11,11 +11,19 @@ export interface ISyncApi {
   shutdown(): Promise<void>
 }
 
+// handles a commit found during a sync
+// should be similar to: https://github.com/ceramicnetwork/js-ceramic/blob/6ae6e121b33132225f256762825e1439fd84f23a/packages/core/src/state-management/state-manager.ts#L210
+export type HandleCommit = (streamId: StreamID, commit: CID, model?: StreamID) => Promise<void>
+
 export interface IpfsService {
   retrieveFromIPFS(cid: CID | string, path?: string): Promise<any>
   retrieveCommit(cid: CID | string, streamId: StreamID): Promise<any>
   storeCommit(data: any, streamId?: StreamID): Promise<CID>
   storeRecord(record: Record<string, unknown>): Promise<CID>
+}
+
+export interface IndexApi {
+  shouldIndexStream(streamId: StreamID): Promise<boolean>
 }
 
 export interface TreeMetadata {
