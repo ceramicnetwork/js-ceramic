dd3436593d45f2c02a60a5ec7e61a53e56c2a0c4
Paul Le Cam <paul@3box.io>
2023-01-27 14:01:40 +0000

    Fix missing Model imports

--- packages/stream-model-instance-handler/src/__tests__/model-instance-document-handler.test.ts
+++ packages/stream-model-instance-handler/src/__tests__/model-instance-document-handler.test.ts
@@ -11,7 +11,7 @@ import * as sha256 from '@stablelib/sha256'
 import cloneDeep from 'lodash.clonedeep'
 import jsonpatch from 'fast-json-patch'
 import { ModelInstanceDocument } from '@ceramicnetwork/stream-model-instance'
-import { ModelDefinition } from '@ceramicnetwork/stream-model'
+import { Model, ModelDefinition } from '@ceramicnetwork/stream-model'
 import {
   CeramicApi,
   CommitType,
