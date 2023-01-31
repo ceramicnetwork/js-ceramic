45cc8d83236cec48844980c1229ca4f82e99089b
Nathaniel Cook <nvcook42@gmail.com>
2023-01-31 08:07:35 -0700

    wip: update scripts

--- tools/test.sh
+++ tools/test.sh
@@ -61,7 +61,38 @@ then
     echo -n "Stream ID to pin: "
     read streamid
     echo
+
     ./bin/ceramic.js pin add $streamid
+
+    init=$(./bin/ceramic.js state $streamid | jq -r .log[0].cid)
+    update=$(./bin/ceramic.js state $streamid | jq -r .log[1].cid)
+
+    echo "$init $update"
+
+    echo -n "What should be anchored (none/init/update)?: "
+    read anchor
+    echo
+    if [[ $anchor == "none" ]]
+    then
+        exit 0
+    fi
+    if [[ $anchor == "init" ]]
+    then
+        cid=$init
+    fi
+    if [[ $anchor == "update" ]]
+    then
+        cid=$update
+    fi
+
+    echo "anchoring cid $cid"
+    curl -X POST \
+        -H 'Content-Type: application/json'\
+        https://cas-qa.3boxlabs.com/api/v0/requests \
+        --data-binary "{\"cid\": \"$cid\", \"streamId\":\"$streamid\"}"
+    echo
+
+    wait_for_anchor
     exit 0
 fi
 
