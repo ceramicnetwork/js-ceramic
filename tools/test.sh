9dfcfa7ba294494df3a8346cef69ca0ff0f5d72d
Nathaniel Cook <nvcook42@gmail.com>
2023-02-01 16:15:25 -0700

    fix: anchor multiple commits in test.sh

--- tools/test.sh
+++ tools/test.sh
@@ -4,11 +4,37 @@ dir=$(cd $(dirname $BASH_SOURCE) && pwd)
 echo $dir
 cd $dir/../packages/cli/
 
-#cas_url='https://cas-qa.3boxlabs.com/api/v0/requests'
-cas_url='https://cas-dev.3boxlabs.com/api/v0/requests'
+cas_url='https://cas-qa.3boxlabs.com/api/v0/requests'
+#cas_url='https://cas-dev.3boxlabs.com/api/v0/requests'
 mode=$1
 shift
 
+
+pick_anchors() {
+    init=$1
+    update=$2
+
+    echo -n "What should be anchored (none/init/update/all)?: "
+    read anchor
+    echo
+    case $anchor in
+    none)
+        cids=()
+      ;;
+    init)
+        cids=($init)
+        ;;
+    update)
+        cids=($update)
+        ;;
+    all)
+        cids=($init $update)
+        ;;
+    *)
+        pick_anchors
+        ;;
+    esac
+}
 wait_for_anchor() {
     streamid=$1
     anchorStatus='PENDING'
@@ -19,6 +45,7 @@ wait_for_anchor() {
         echo "status $anchorStatus"
     done
 }
+
 request_anchor() {
     streamid=$1
     cid=$2
@@ -30,85 +57,55 @@ request_anchor() {
     echo
 }
 
-if [[ "$mode" == "create" ]]
-then
+do_anchors() {
+    streamid=$1
+
+    ./bin/ceramic.js update $streamid --content '{ "Title": "My updated document" }' --no-anchor > /dev/null
+
+    init=$(./bin/ceramic.js state $streamid | jq -r .log[0].cid)
+    update=$(./bin/ceramic.js state $streamid | jq -r .log[1].cid)
+
+    echo "init: $init update: $update"
+
+    pick_anchors $init $update
+
+    for cid in "${cids[@]}"
+    do
+        request_anchor $streamid $cid
+    done
+    if (( ${#cids[@]} != 0 ))
+    then
+        wait_for_anchor $streamid
+    fi
+}
+
+case $mode in
+    create)
     streamid=$(./bin/ceramic.js create tile --content '{"title":"My document"}' --only-genesis | grep StreamID | sed 's/StreamID(\(.*\))/\1/')
 
     echo "Created stream $streamid"
 
-    ./bin/ceramic.js update $streamid --content '{ "Title": "My updated document" }' --no-anchor
-
-    init=$(./bin/ceramic.js state $streamid | jq -r .log[0].cid)
-    update=$(./bin/ceramic.js state $streamid | jq -r .log[1].cid)
-
-    echo "$init $update"
-
-    echo -n "What should be anchored (none/init/update)?: "
-    read anchor
-    echo
-    if [[ $anchor == "none" ]]
-    then
-        exit 0
-    fi
-    if [[ $anchor == "init" ]]
-    then
-        cid=$init
-    fi
-    if [[ $anchor == "update" ]]
-    then
-        cid=$update
-    fi
-
-    request_anchor $streamid $cid
-    wait_for_anchor $streamid
-
-    exit 0
-fi
-if [[ "$mode" == "pin" ]]
-then
-
+    do_anchors $streamid
+    ;;
+pin)
     echo -n "Stream ID to pin: "
     read streamid
     echo
 
-    ./bin/ceramic.js pin add $streamid
+    ./bin/ceramic.js pin add $streamid > /dev/null
 
-    init=$(./bin/ceramic.js state $streamid | jq -r .log[0].cid)
-    update=$(./bin/ceramic.js state $streamid | jq -r .log[1].cid)
+    echo "Pinned stream $streamid"
 
-    echo "$init $update"
-
-    echo -n "What should be anchored (none/init/update)?: "
-    read anchor
-    echo
-    if [[ $anchor == "none" ]]
-    then
-        exit 0
-    fi
-    if [[ $anchor == "init" ]]
-    then
-        cid=$init
-    fi
-    if [[ $anchor == "update" ]]
-    then
-        cid=$update
-    fi
-
-    request_anchor $streamid $cid
-    wait_for_anchor $streamid
-
-    exit 0
-fi
-if [[ "$mode" == "non-cid" ]]
-then
-
-    #cid=bagcqcera6hrv3zpbvfkyt5u5s5vvpt4nabg24lzdoxqkzfysrg2ajz3qo2zq
+    do_anchors $streamid
+    ;;
+anchor)
     streamid=$1
     cid=$2
 
     request_anchor $streamid $cid
-    exit 0
-fi
+    ;;
+*)
+    echo "Specify mode as one of: create,pin,anchor"
+    exit 1
+esac
 
-echo "Must specify mode"
-exit 1
