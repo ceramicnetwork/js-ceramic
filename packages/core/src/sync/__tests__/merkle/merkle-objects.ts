5c6bff7ba80bbf93b0717e59beb0c8931d53148a
Sergey Ukustov <sergey@ukstv.me>
2023-02-15 15:41:38 +0300

    chore: Drop old IpfsLeafCompare

--- packages/core/src/sync/__tests__/merkle/merkle-objects.ts
+++ packages/core/src/sync/__tests__/merkle/merkle-objects.ts
@@ -1,11 +1,10 @@
 import type { CID } from 'multiformats/cid'
-import type { DiagnosticsLogger, AbortOptions } from '@ceramicnetwork/common'
+import type { AbortOptions } from '@ceramicnetwork/common'
 import { type ICandidate, Node } from '@ceramicnetwork/anchor-utils'
 import type {
   MetadataFunction,
   CIDHolder,
   MergeFunction,
-  CompareFunction,
   TreeMetadata,
 } from '@ceramicnetwork/anchor-utils'
 
@@ -33,48 +32,6 @@ export class IpfsMerge implements MergeFunction<CIDHolder, TreeMetadata> {
 
     const mergedCid = await this.ipfsService.storeRecord(merged)
     return new Node<CIDHolder>({ cid: mergedCid }, left, right)
-  }
-}
-
-/**
- * Implements IPFS merge CIDs
- */
-export class IpfsLeafCompare implements CompareFunction<ICandidate> {
-  constructor(private readonly logger: DiagnosticsLogger) {
-    this.compare = this.compare.bind(this)
-  }
-
-  compare(left: Node<ICandidate>, right: Node<ICandidate>): number {
-    try {
-      // Sort by model first
-      const leftModel = left.data.metadata.model?.toString()
-      const rightModel = right.data.metadata.model?.toString()
-      if (leftModel !== rightModel) {
-        if (leftModel != null) {
-          return rightModel == null
-            ? -1 // null last
-            : leftModel.localeCompare(rightModel)
-        }
-        return 1 // null last
-      }
-
-      // Sort by controller
-      // If either value is an object for whatever reason it will
-      // be sorted last because "[" < "d" ("[object Object]" vs "did:...")
-      const leftController = String(left.data.metadata.controllers[0])
-      const rightController = String(right.data.metadata.controllers[0])
-      if (leftController !== rightController) {
-        return leftController.localeCompare(rightController)
-      }
-
-      // Sort by stream ID
-      return left.data.streamId.toString().localeCompare(right.data.streamId.toString())
-    } catch (err) {
-      this.logger.err(
-        `Error while comparing stream ${left.data.streamId.toString()} to stream ${right.data.streamId.toString()}. Error: ${err}`
-      )
-      throw err
-    }
   }
 }
 
