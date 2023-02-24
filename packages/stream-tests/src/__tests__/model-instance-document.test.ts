255ad56c14e2c1b233d760cc9d88e064876aae8f
Spencer T Brody <spencer+github@3box.io>
2023-02-24 15:19:35 -0700

    fix MID test

--- packages/stream-tests/src/__tests__/model-instance-document.test.ts
+++ packages/stream-tests/src/__tests__/model-instance-document.test.ts
@@ -342,8 +342,10 @@ describe('ModelInstanceDocument API http-client tests', () => {
     )
     const doc = await ModelInstanceDocument.create(ceramic, CONTENT0, { model: nonIndexedModel.id })
     await expect(TestUtils.isPinned(ceramic, doc.id)).resolves.toBeTruthy()
-    await doc.replace(CONTENT1, { pin: false })
-    await expect(TestUtils.isPinned(ceramic, doc.id)).resolves.toBeFalsy()
+    await expect(doc.replace(CONTENT1, { pin: false })).rejects.toThrow(
+      /Cannot pin or unpin streams through the CRUD APIs/
+    )
+    await expect(TestUtils.isPinned(ceramic, doc.id)).resolves.toBeTruthy()
   })
 
   test(`Pinning a ModelInstanceDocument pins its Model`, async () => {
