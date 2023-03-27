7db29e3580c664f6c642170c1a18747a89544ae0
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2023-03-27 19:57:06 -0400

    fix: optionally use env var for git commit hash

--- packages/cli/tools/commitHash.cjs
+++ packages/cli/tools/commitHash.cjs
@@ -1,2 +1,9 @@
-const _commitHash = require('child_process').execSync('git rev-parse HEAD').toString().trim()
-require('fs').writeFileSync('src/commitHash.ts', `export const commitHash = '${_commitHash}'`)
+function getCommitHash() {
+    if (process.env.GIT_COMMIT_HASH) {
+        return process.env.GIT_COMMIT_HASH
+    } else {
+        return require('child_process').execSync('git rev-parse HEAD').toString().trim()
+    }
+}
+
+require('fs').writeFileSync('src/commitHash.ts', `export const commitHash = '${getCommitHash()}'`)
