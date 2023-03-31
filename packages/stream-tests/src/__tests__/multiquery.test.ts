cfde334ca67afdc68f48671b8f7514a7472ca967
gzalz <ericdaniel.gonzalez@gmail.com>
2023-03-31 13:16:34 -0600

    restore previous tests

--- packages/stream-tests/src/__tests__/multiquery.test.ts
+++ packages/stream-tests/src/__tests__/multiquery.test.ts
@@ -80,119 +80,119 @@ describe('multiquery API http-client tests', () => {
     expect(Math.abs(expectedTimestamp - givenTimestamp)).toBeLessThan(5)
   }
 
-  // it('loads the same stream at multiple points in time using atTime', async () => {
-  //   // test data for the atTime feature
-  //   streamFStates.push(streamF.state)
-  //   // timestamp before the first anchor commit
-  //   streamFTimestamps.push(Math.floor(Date.now() / 1000))
-  //   await TestUtils.delay(1000)
-  //   await streamF.update({ ...streamF.content, update: 'new stuff' })
-  //   await TestUtils.anchorUpdate(core, streamF)
-  //   await TestUtils.delay(1000)
-  //   // timestamp between the first and the second anchor commit
-  //   streamFTimestamps.push(Math.floor(Date.now() / 1000))
-  //   streamFStates.push(streamF.state)
-  //   await TestUtils.delay(1000)
-  //   await streamF.update({ ...streamF.content, update: 'newer stuff' })
-  //   await TestUtils.anchorUpdate(core, streamF)
-  //   await TestUtils.delay(1000)
-  //   // timestamp after the second anchor commit
-  //   streamFTimestamps.push(Math.floor(Date.now() / 1000))
-  //   streamFStates.push(streamF.state)
-  //
-  //   const queries = [
-  //     {
-  //       streamId: streamF.id,
-  //       atTime: streamFTimestamps[0],
-  //     },
-  //     {
-  //       streamId: streamF.id,
-  //       atTime: streamFTimestamps[1],
-  //     },
-  //     {
-  //       streamId: streamF.id,
-  //       atTime: streamFTimestamps[2],
-  //     },
-  //     {
-  //       streamId: streamF.id,
-  //     },
-  //   ]
-  //   const streams = await ceramic.multiQuery(queries)
-  //
-  //   expect(Object.keys(streams).length).toEqual(4)
-  //   const states = Object.values(streams).map((stream) => stream.state)
-  //   // annoying thing, was pending when snapshotted but will
-  //   // obviously not be when loaded at a specific commit
-  //   streamFStates[0].anchorStatus = 0
-  //
-  //   // first stream state didn't have an anchor timestamp when it was added to the streamFStates
-  //   // array, but it does get a timestamp after being anchored
-  //   // Assert that the timestamp it got from being anchored is within 10 seconds of when it was created
-  //   expect(Math.abs(states[0].log[0].timestamp - streamFTimestamps[0])).toBeLessThan(5)
-  //   delete states[0].log[0].timestamp
-  //
-  //   expect(states[0]).toEqual(streamFStates[0])
-  //   expect(states[1]).toEqual(streamFStates[1])
-  //   expect(states[2]).toEqual(streamFStates[2])
-  //   expect(states[3]).toEqual(streamF.state)
-  // }, 60000)
-  //
-  // it('loads the same stream at multiple points in time using opts.atTime', async () => {
-  //   // test data for the atTime feature
-  //   streamFStates.push(streamF.state)
-  //   // timestamp before the first anchor commit
-  //   streamFTimestamps.push(Math.floor(Date.now() / 1000))
-  //   await TestUtils.delay(1000)
-  //   await streamF.update({ ...streamF.content, update: 'new stuff' })
-  //   await TestUtils.anchorUpdate(core, streamF)
-  //   await TestUtils.delay(1000)
-  //   // timestamp between the first and the second anchor commit
-  //   streamFTimestamps.push(Math.floor(Date.now() / 1000))
-  //   streamFStates.push(streamF.state)
-  //   await TestUtils.delay(1000)
-  //   await streamF.update({ ...streamF.content, update: 'newer stuff' })
-  //   await TestUtils.anchorUpdate(core, streamF)
-  //   await TestUtils.delay(1000)
-  //   // timestamp after the second anchor commit
-  //   streamFTimestamps.push(Math.floor(Date.now() / 1000))
-  //   streamFStates.push(streamF.state)
-  //
-  //   const queries = [
-  //     {
-  //       streamId: streamF.id,
-  //       opts: {atTime: streamFTimestamps[0]},
-  //     },
-  //     {
-  //       streamId: streamF.id,
-  //       opts: {atTime: streamFTimestamps[1]},
-  //     },
-  //     {
-  //       streamId: streamF.id,
-  //       opts: {atTime: streamFTimestamps[2]},
-  //     },
-  //     {
-  //       streamId: streamF.id,
-  //     },
-  //   ]
-  //   const streams = await ceramic.multiQuery(queries)
-  //
-  //   expect(Object.keys(streams).length).toEqual(4)
-  //   const states = Object.values(streams).map((stream) => stream.state)
-  //   // annoying thing, was pending when snapshotted but will
-  //   // obviously not be when loaded at a specific commit
-  //   streamFStates[0].anchorStatus = 0
-  //
-  //   // first stream state didn't have an anchor timestamp when it was added to the streamFStates
-  //   // array, but it does get a timestamp after being anchored
-  //   // Assert that the timestamp it got from being anchored is within 10 seconds of when it was created
-  //   expect(Math.abs(states[0].log[0].timestamp - streamFTimestamps[0])).toBeLessThan(5)
-  //   delete states[0].log[0].timestamp
-  //
-  //   expect(states[0]).toEqual(streamFStates[0])
-  //   expect(states[1]).toEqual(streamFStates[1])
-  //   expect(states[2]).toEqual(streamFStates[2])
-  //   expect(states[3]).toEqual(streamF.state)
-  // }, 60000)
+  it('loads the same stream at multiple points in time using atTime', async () => {
+    // test data for the atTime feature
+    streamFStates.push(streamF.state)
+    // timestamp before the first anchor commit
+    streamFTimestamps.push(Math.floor(Date.now() / 1000))
+    await TestUtils.delay(1000)
+    await streamF.update({ ...streamF.content, update: 'new stuff' })
+    await TestUtils.anchorUpdate(core, streamF)
+    await TestUtils.delay(1000)
+    // timestamp between the first and the second anchor commit
+    streamFTimestamps.push(Math.floor(Date.now() / 1000))
+    streamFStates.push(streamF.state)
+    await TestUtils.delay(1000)
+    await streamF.update({ ...streamF.content, update: 'newer stuff' })
+    await TestUtils.anchorUpdate(core, streamF)
+    await TestUtils.delay(1000)
+    // timestamp after the second anchor commit
+    streamFTimestamps.push(Math.floor(Date.now() / 1000))
+    streamFStates.push(streamF.state)
+
+    const queries = [
+      {
+        streamId: streamF.id,
+        atTime: streamFTimestamps[0],
+      },
+      {
+        streamId: streamF.id,
+        atTime: streamFTimestamps[1],
+      },
+      {
+        streamId: streamF.id,
+        atTime: streamFTimestamps[2],
+      },
+      {
+        streamId: streamF.id,
+      },
+    ]
+    const streams = await ceramic.multiQuery(queries)
+
+    expect(Object.keys(streams).length).toEqual(4)
+    const states = Object.values(streams).map((stream) => stream.state)
+    // annoying thing, was pending when snapshotted but will
+    // obviously not be when loaded at a specific commit
+    streamFStates[0].anchorStatus = 0
+
+    // first stream state didn't have an anchor timestamp when it was added to the streamFStates
+    // array, but it does get a timestamp after being anchored
+    // Assert that the timestamp it got from being anchored is within 10 seconds of when it was created
+    expect(Math.abs(states[0].log[0].timestamp - streamFTimestamps[0])).toBeLessThan(5)
+    delete states[0].log[0].timestamp
+
+    expect(states[0]).toEqual(streamFStates[0])
+    expect(states[1]).toEqual(streamFStates[1])
+    expect(states[2]).toEqual(streamFStates[2])
+    expect(states[3]).toEqual(streamF.state)
+  }, 60000)
+
+  it('loads the same stream at multiple points in time using opts.atTime', async () => {
+    // test data for the atTime feature
+    streamFStates.push(streamF.state)
+    // timestamp before the first anchor commit
+    streamFTimestamps.push(Math.floor(Date.now() / 1000))
+    await TestUtils.delay(1000)
+    await streamF.update({ ...streamF.content, update: 'new stuff' })
+    await TestUtils.anchorUpdate(core, streamF)
+    await TestUtils.delay(1000)
+    // timestamp between the first and the second anchor commit
+    streamFTimestamps.push(Math.floor(Date.now() / 1000))
+    streamFStates.push(streamF.state)
+    await TestUtils.delay(1000)
+    await streamF.update({ ...streamF.content, update: 'newer stuff' })
+    await TestUtils.anchorUpdate(core, streamF)
+    await TestUtils.delay(1000)
+    // timestamp after the second anchor commit
+    streamFTimestamps.push(Math.floor(Date.now() / 1000))
+    streamFStates.push(streamF.state)
+
+    const queries = [
+      {
+        streamId: streamF.id,
+        opts: {atTime: streamFTimestamps[0]},
+      },
+      {
+        streamId: streamF.id,
+        opts: {atTime: streamFTimestamps[1]},
+      },
+      {
+        streamId: streamF.id,
+        opts: {atTime: streamFTimestamps[2]},
+      },
+      {
+        streamId: streamF.id,
+      },
+    ]
+    const streams = await ceramic.multiQuery(queries)
+
+    expect(Object.keys(streams).length).toEqual(4)
+    const states = Object.values(streams).map((stream) => stream.state)
+    // annoying thing, was pending when snapshotted but will
+    // obviously not be when loaded at a specific commit
+    streamFStates[0].anchorStatus = 0
+
+    // first stream state didn't have an anchor timestamp when it was added to the streamFStates
+    // array, but it does get a timestamp after being anchored
+    // Assert that the timestamp it got from being anchored is within 10 seconds of when it was created
+    expect(Math.abs(states[0].log[0].timestamp - streamFTimestamps[0])).toBeLessThan(5)
+    delete states[0].log[0].timestamp
+
+    expect(states[0]).toEqual(streamFStates[0])
+    expect(states[1]).toEqual(streamFStates[1])
+    expect(states[2]).toEqual(streamFStates[2])
+    expect(states[3]).toEqual(streamF.state)
+  }, 60000)
 
 
   it('serailizes syncopts correctly', async () => {
