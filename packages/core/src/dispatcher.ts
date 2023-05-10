becf57ab8670a37c91226f4b602fdf16941dfdb5
Sergey Ukustov <sergey@ukstv.me>
2023-05-10 15:35:19 +0300

    test: Adjust block tests

--- packages/core/src/dispatcher.ts
+++ packages/core/src/dispatcher.ts
@@ -211,8 +211,7 @@ export class Dispatcher {
    */
   async retrieveCommit(cid: CID | string, streamId: StreamID): Promise<any> {
     try {
-      const data = await this._getFromIpfs(cid)
-      return data
+      return await this._getFromIpfs(cid)
     } catch (e) {
       this._logger.err(
         `Error while loading commit CID ${cid.toString()} from IPFS for stream ${streamId.toString()}: ${e}`
