142d17e09cfcd09f513d6b67f0c8c36a4a3c0110
Sergey Ukustov <sergey@ukstv.me>
2023-06-18 16:00:37 +0300

    chore(blockchain-utils-validation): noble ed25519

--- packages/blockchain-utils-validation/src/blockchains/solana.ts
+++ packages/blockchain-utils-validation/src/blockchains/solana.ts
@@ -1,17 +1,8 @@
 import { BlockchainHandler } from '../blockchain-handler.js'
 import { LinkProof } from '@ceramicnetwork/blockchain-utils-linking'
+import { ed25519 } from '@noble/curves/ed25519'
 import * as uint8arrays from 'uint8arrays'
-import { verify } from '@stablelib/ed25519'
 import { normalizeAccountId } from '@ceramicnetwork/common'
-
-const verifySignature = async (
-  pubKey: Uint8Array,
-  message: string,
-  signature: Uint8Array
-): Promise<boolean> => {
-  const verified = verify(pubKey, uint8arrays.fromString(message), signature)
-  return verified
-}
 
 const namespace = 'solana'
 
@@ -20,8 +11,8 @@ export async function validateLink(proof: LinkProof): Promise<LinkProof | null> 
   const pubKey = uint8arrays.fromString(account.address, 'base58btc')
   const msg = proof.message
   const sig = uint8arrays.fromString(proof.signature, 'base64')
-  const is_sig_valid = await verifySignature(pubKey, msg, sig)
-  return is_sig_valid ? proof : null
+  const isValid = ed25519.verify(sig, uint8arrays.fromString(msg), pubKey)
+  return isValid ? proof : null
 }
 
 export const handler: BlockchainHandler = {
