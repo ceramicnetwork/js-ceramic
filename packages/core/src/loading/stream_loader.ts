1a0b9ee238648420986b283f569f5efd102ba84f
Spencer T Brody <spencer+github@3box.io>
2023-08-25 16:18:48 -0400

    comments

--- packages/core/src/loading/stream_loader.ts
+++ packages/core/src/loading/stream_loader.ts
@@ -11,6 +11,12 @@ import { StreamID } from '@ceramicnetwork/streamid'
 import { Dispatcher } from '../dispatcher.js'
 import { verifyAnchorAndApplyTimestamps } from '../conflict-resolution.js'
 
+/**
+ * Class to contain all the logic for loading a stream, including fetching the relevant commit
+ * data from the p2p network and applying the commits to get a StreamState.  It notably however does
+ * not manage the persistence of the state information, nor updating the cache or indexing. It is
+ * purely stateless.
+ */
 export class StreamLoader {
   constructor(
     readonly logger: DiagnosticsLogger,
@@ -21,6 +27,11 @@ export class StreamLoader {
     readonly stateManipulator: StateManipulator
   ) {}
 
+  /**
+   * Completely loads the current state of a Stream from the p2p network just from the StreamID.
+   * @param streamID
+   * @param syncTimeoutSecs
+   */
   async loadStream(streamID: StreamID, syncTimeoutSecs: number): Promise<StreamState> {
     const tip = await this.tipFetcher.findTip(streamID, syncTimeoutSecs)
     const logWithoutTimestamps = await this.logSyncer.syncFullLog(streamID, tip)
@@ -36,6 +47,12 @@ export class StreamLoader {
     })
   }
 
+  /**
+   * Loads the current state of a Stream from the p2p network given a currently known, possibly
+   * stale state of that Stream.
+   * @param state
+   * @param syncTimeoutSecs
+   */
   async syncStream(state: StreamState, syncTimeoutSecs: number): Promise<StreamState> {
     const streamID = StreamUtils.streamIdFromState(state)
     const tip = await this.tipFetcher.findTip(streamID, syncTimeoutSecs)
