07c3efbd85b4a82a6590c27c06327a05cb3f0224
Sergey Ukustov <sergey@ukstv.me>
2023-09-28 19:27:42 +0300

    RemoteCAS.create

--- packages/core/src/anchor/anchor-request-car-file-reader.ts
+++ packages/core/src/anchor/anchor-request-car-file-reader.ts
@@ -1,23 +1,28 @@
 import { CAR } from 'cartonne'
 import { StreamID } from '@ceramicnetwork/streamid'
+import { Memoize } from 'mapmoize'
 import type { CID } from 'multiformats/cid'
 
 export class AnchorRequestCarFileReader {
   constructor(readonly carFile: CAR) {}
 
+  @Memoize()
   private get root(): Record<string, any> {
     const rootCid = this.carFile.roots[0]
     return this.carFile.get(rootCid)
   }
 
+  @Memoize()
   get timestamp(): Date {
     return new Date(this.root.timestamp)
   }
 
+  @Memoize()
   get streamId(): StreamID {
     return StreamID.fromBytes(this.root.streamId)
   }
 
+  @Memoize()
   get tip(): CID {
     return this.root.tip
   }
