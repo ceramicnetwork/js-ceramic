d867cca30af659298a4cdf675b0192f44cf82bdc
Spencer T Brody <spencer+github@3box.io>
2023-12-08 17:12:47 -0500

    Add additional test coverage

--- packages/core/src/state-management/__tests__/anchor-resuming.test.ts
+++ packages/core/src/state-management/__tests__/anchor-resuming.test.ts
@@ -1,10 +1,28 @@
 import { expect, jest } from '@jest/globals'
 import { createCeramic } from '../../__tests__/create-ceramic.js'
-import { AnchorStatus, IpfsApi, SyncOptions, TestUtils } from '@ceramicnetwork/common'
+import {
+  AnchorStatus,
+  GenesisCommit,
+  IpfsApi,
+  SyncOptions,
+  TestUtils,
+} from '@ceramicnetwork/common'
 import { TileDocument } from '@ceramicnetwork/stream-tile'
 import tmp from 'tmp-promise'
 import { createIPFS } from '@ceramicnetwork/ipfs-daemon'
 import all from 'it-all'
+import { AnchorRequestStore } from '../../store/anchor-request-store.js'
+
+/**
+ * Returns a list of all StreamIDs stored in the AnchorRequestStore.
+ */
+async function getPendingAnchorStreamIDs(
+  anchorRequestStore: AnchorRequestStore
+): Promise<Array<string>> {
+  return (await all(anchorRequestStore.list()))
+    .reduce((acc, array) => acc.concat(array), [])
+    .map((result) => result.key.toString())
+}
 
 jest.setTimeout(10000)
 
@@ -44,9 +62,7 @@ test('resume anchors from AnchorRequestStore', async () => {
     })
   )
 
-  const loaded = (await all(ceramic.repository.anchorRequestStore.list()))
-    .reduce((acc, array) => acc.concat(array), [])
-    .map((result) => result.key.toString())
+  const loaded = await getPendingAnchorStreamIDs(ceramic.repository.anchorRequestStore)
   // LevelDB Store stores keys ordered lexicographically
   expect(streamIds.map((streamId) => streamId.toString()).sort()).toEqual(loaded)
 
@@ -83,11 +99,107 @@ test('resume anchors from AnchorRequestStore', async () => {
       })
     })
   )
+
   await TestUtils.waitForConditionOrTimeout(async () => {
     return newRunningStates$.every((runningState$) => {
       return runningState$.value.anchorStatus === AnchorStatus.ANCHORED
     })
   }, 10000)
 
+  newRunningStates$.forEach((state$) =>
+    expect(state$.value.anchorStatus).toEqual(AnchorStatus.ANCHORED)
+  )
+
+  // Wait for async cleanup
+  await TestUtils.waitForConditionOrTimeout(async () => {
+    const remaining = await getPendingAnchorStreamIDs(newCeramic.repository.anchorRequestStore)
+    return remaining.length == 0
+  })
+
+  // There should be nothing left in the AnchorRequestStore at this point
+  await expect(
+    getPendingAnchorStreamIDs(newCeramic.repository.anchorRequestStore)
+  ).resolves.toHaveLength(0)
+
   await newCeramic.close()
+})
+
+test('Cleans up entries from store for already anchored tips', async () => {
+  const ceramic = await createCeramic(ipfs, {
+    stateStoreDirectory: stateStoreDirectoryName,
+  })
+
+  const stream = await TileDocument.create(ceramic, { step: 0 })
+  await TestUtils.anchorUpdate(ceramic, stream)
+  await stream.update({ step: 1 })
+  await TestUtils.anchorUpdate(ceramic, stream)
+  await stream.update({ step: 2 })
+  await TestUtils.anchorUpdate(ceramic, stream)
+  expect(stream.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
+  expect(stream.state.log).toHaveLength(6)
+
+  // Wait for AnchorRequestStore to be cleared out
+  await TestUtils.waitForConditionOrTimeout(async () => {
+    const remaining = await getPendingAnchorStreamIDs(ceramic.repository.anchorRequestStore)
+    return remaining.length == 0
+  })
+  await expect(
+    getPendingAnchorStreamIDs(ceramic.repository.anchorRequestStore)
+  ).resolves.toHaveLength(0)
+
+  const removeFromStoreSpy = jest.spyOn(ceramic.repository.anchorRequestStore, 'remove')
+  // Make removing from the store a no-op temporarily
+  removeFromStoreSpy.mockImplementation(() => Promise.resolve())
+
+  // Now make it seem like the node got the anchor commit via pubsub before getting it via polling
+  // and so the request is still in the AnchorRequestStore despite already being the currently
+  // anchored tip.
+  await ceramic.repository.anchorRequestStore.save(stream.id, {
+    cid: stream.state.log[4].cid,
+    timestamp: Date.now(),
+    genesis: {} as GenesisCommit,
+  })
+  await expect(
+    getPendingAnchorStreamIDs(ceramic.repository.anchorRequestStore)
+  ).resolves.toHaveLength(1)
+
+  // Now allow removes from the store to go through again.
+  removeFromStoreSpy.mockRestore()
+
+  // The node should detect that the entry is already anchored and clean it up.
+  await TestUtils.waitForConditionOrTimeout(async () => {
+    const remaining = await getPendingAnchorStreamIDs(ceramic.repository.anchorRequestStore)
+    return remaining.length == 0
+  })
+
+  await expect(
+    getPendingAnchorStreamIDs(ceramic.repository.anchorRequestStore)
+  ).resolves.toHaveLength(0)
+
+  // Now repeat the same test but this time make it so the AnchorRequestStore has a request for
+  // a CID in the middle of the stream log
+  removeFromStoreSpy.mockImplementation(() => Promise.resolve())
+  await ceramic.repository.anchorRequestStore.save(stream.id, {
+    cid: stream.state.log[2].cid,
+    timestamp: Date.now(),
+    genesis: {} as GenesisCommit,
+  })
+  await expect(
+    getPendingAnchorStreamIDs(ceramic.repository.anchorRequestStore)
+  ).resolves.toHaveLength(1)
+
+  // Allow removes from the store to go through again.
+  removeFromStoreSpy.mockRestore()
+
+  // The node should detect that the entry is already anchored and clean it up.
+  await TestUtils.waitForConditionOrTimeout(async () => {
+    const remaining = await getPendingAnchorStreamIDs(ceramic.repository.anchorRequestStore)
+    return remaining.length == 0
+  })
+
+  await expect(
+    getPendingAnchorStreamIDs(ceramic.repository.anchorRequestStore)
+  ).resolves.toHaveLength(0)
+
+  await ceramic.close()
 })
