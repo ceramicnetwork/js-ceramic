bde6bbcb534868c8b1aa9c6aa4e658cd91c1a082
Sergey Ukustov <sergey@ukstv.me>
2024-01-26 18:15:41 +0300

    wip

--- packages/core/src/state-management/repository.ts
+++ packages/core/src/state-management/repository.ts
@@ -971,6 +971,7 @@ export class Repository {
       tip: state$.tip,
       lastAnchor: lastAnchor,
       firstAnchor: firstAnchor,
+      shouldIndex: state$.value.metadata.shouldIndex,
     }
 
     await this.index.indexStream(streamContent)
