d638d8e9d781264047817429475f4cbfd44b4434
JulissaDantes <julissadcj@gmail.com>
2024-01-30 08:37:36 -0400

    feat: add enum

--- packages/core/src/feed.ts
+++ packages/core/src/feed.ts
@@ -3,11 +3,22 @@ import { Subject, type Observable } from 'rxjs'
 import { CommitID } from '@ceramicnetwork/streamid'
 import { StreamUtils } from '@ceramicnetwork/common'
 
+/**
+ * Describes all event types
+ */
+export enum EventType {
+  CREATION = 0,
+  PIN = 1,
+  ANCHOR = 2,//TODO should we have several of this? does this even triggers the feed?
+  UPDATE = 3,
+}
+
 export class FeedDocument {
   constructor(
     readonly commitId: CommitID,
     readonly content: any,
-    readonly metadata: StreamMetadata
+    readonly metadata: StreamMetadata,
+    readonly eventType: EventType
   ) {}
 
   static fromStreamState(streamState: StreamState): FeedDocument {
@@ -15,6 +26,7 @@ export class FeedDocument {
       commitId: StreamUtils.commitIdFromStreamState(streamState),
       content: streamState.next ? streamState.next.content : streamState.content,
       metadata: streamState.next ? streamState.next.metadata : streamState.metadata,
+      eventType: 0//TODO set correct type
     }
   }
 }
