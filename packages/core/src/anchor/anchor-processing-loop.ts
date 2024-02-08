7039d26a8782686a54c08e959ed2ef4cfaca28d9
Golda Velez <gvelez17@gmail.com>
2024-02-07 19:02:43 -0700

    use interval publishing metrics for anchor polling loop delay (#3142)
    * update dependency and correct variable names
    
    * prettier
    
    * add an additional observation for number processed, and semantic name
    
    * Update packages/core/src/anchor/anchor-processing-loop.ts
    
    Co-authored-by: Spencer T Brody <spencer+github@3box.io>
    
    * de-lint
    
    ---------
    
    Co-authored-by: Spencer T Brody <spencer+github@3box.io>

--- packages/core/src/anchor/anchor-processing-loop.ts
+++ packages/core/src/anchor/anchor-processing-loop.ts
@@ -6,6 +6,9 @@ import type { AnchorLoopHandler } from './anchor-service.js'
 import type { DiagnosticsLogger } from '@ceramicnetwork/common'
 import type { NamedTaskQueue } from '../state-management/named-task-queue.js'
 import type { StreamID } from '@ceramicnetwork/streamid'
+import { TimeableMetric, SinceField } from '@ceramicnetwork/observability'
+
+const METRICS_REPORTING_INTERVAL_MS = 10000 // 10 second reporting interval
 
 /**
  * Get anchor request entries from AnchorRequestStore one by one. For each entry, get CAS response,
@@ -20,6 +23,7 @@ export class AnchorProcessingLoop {
    * Linearizes requests to AnchorRequestStore by stream id. Shared with the AnchorService.
    */
   readonly #anchorStoreQueue: NamedTaskQueue
+  readonly #anchorPollingMetrics: TimeableMetric
 
   constructor(
     batchSize: number,
@@ -30,6 +34,11 @@ export class AnchorProcessingLoop {
     anchorStoreQueue: NamedTaskQueue
   ) {
     this.#anchorStoreQueue = anchorStoreQueue
+    this.#anchorPollingMetrics = new TimeableMetric(
+      SinceField.TIMESTAMP,
+      'anchorRequestAge',
+      METRICS_REPORTING_INTERVAL_MS
+    )
     this.#loop = new ProcessingLoop(logger, store.infiniteList(batchSize), async (streamId) => {
       try {
         logger.verbose(
@@ -46,6 +55,11 @@ export class AnchorProcessingLoop {
           `Anchor event with status ${event.status} for commit CID ${entry.cid} of Stream ${streamId} handled successfully`
         )
         if (isTerminal) {
+          // Record the latency of how long this particular anchor request existed in the
+          // AnchorRequestStore.  The "record" function will compare the current time to the
+          // "timestamp" field from "entry", which was set as the current time when the request was
+          // first written into the AnchorRequestStore.
+          this.#anchorPollingMetrics.record(entry)
           // Remove iff tip stored equals to the tip we processed
           // Sort of Compare-and-Swap.
           await this.#anchorStoreQueue.run(streamId.toString(), async () => {
@@ -71,6 +85,7 @@ export class AnchorProcessingLoop {
    * Start looping.
    */
   start(): void {
+    this.#anchorPollingMetrics.startPublishingStats()
     this.#loop.start()
   }
 
@@ -78,6 +93,7 @@ export class AnchorProcessingLoop {
    * Stop looping.
    */
   async stop(): Promise<void> {
+    this.#anchorPollingMetrics.stopPublishingStats()
     return this.#loop.stop()
   }
 }
