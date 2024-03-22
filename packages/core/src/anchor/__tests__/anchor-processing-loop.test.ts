9f5fabf4e846af7ab38f359be786c7112f14593f
Spencer T Brody <spencer+github@3box.io>
2024-03-22 13:42:53 -0400

    fix: Fix race condition in anchor-processing-loop.test.ts

--- packages/core/src/anchor/__tests__/anchor-processing-loop.test.ts
+++ packages/core/src/anchor/__tests__/anchor-processing-loop.test.ts
@@ -153,8 +153,12 @@ test('Dont process the same entry multiple times concurrently', async () => {
     do {
       // Need this sleep or else the Node runtime might never switch back to the "process" function.
       await CommonTestUtils.delay(1)
-      for (const e of entries) yield e
-    } while (true)
+      for (const e of entries) {
+        if (!processed.has(e)) {
+          yield e
+        }
+      }
+    } while (!isDone.isResolved)
   }
 
   const process = jest.fn(async (entry) => {
