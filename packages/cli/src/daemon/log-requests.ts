ffe141c6de45870956f577f0b612de663cdace0c
Spencer T Brody <spencer+github@3box.io>
2024-04-23 17:12:16 -0400

    feat: log http POST requests body in http-access log

--- packages/cli/src/daemon/log-requests.ts
+++ packages/cli/src/daemon/log-requests.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express'
 import morgan from 'morgan'
 
 const ACCESS_LOG_FMT =
-  'ip=:remote-addr ts=:date[iso] method=:method original_url=:original-url base_url=:base-url path=:path:params http_version=:http-version req_header:req[header] status=:status content_length=:res[content-length] content_type=":res[content-type]" ref=:referrer user_agent=":user-agent" elapsed_ms=:total-time[3] error_message=":error-message" error_code=:error-code'
+  'ip=:remote-addr ts=:date[iso] method=:method original_url=:original-url base_url=:base-url path=:path:params :body http_version=:http-version req_header:req[header] status=:status content_length=:res[content-length] content_type=":res[content-type]" ref=:referrer user_agent=":user-agent" elapsed_ms=:total-time[3] error_message=":error-message" error_code=:error-code'
 
 export function logRequests(loggerProvider: LoggerProvider): any[] {
   morgan.token<Request, Response>('error-message', (req, res: Response) => {
@@ -32,6 +32,26 @@ export function logRequests(loggerProvider: LoggerProvider): any[] {
       }
     }
     return ' params=-'
+  })
+  morgan.token<Request, Response>('body', (req) => {
+    if (req.body) {
+      const keys = Object.keys(req.body)
+      if (keys.length > 0) {
+        const body = keys.reduce((prev, curr) => {
+          const value = req.body[curr]
+          const valKeys = Object.keys(value)
+          if (valKeys.length > 0) {
+            // value is an object
+            return prev + ` body.${curr}=${JSON.stringify(value)}`
+          } else {
+            // value is a scalar
+            return prev + ` body.${curr}=${value}`
+          }
+        }, '')
+        return body
+      }
+    }
+    return ' body=-'
   })
 
   const logger = loggerProvider.makeServiceLogger('http-access')
