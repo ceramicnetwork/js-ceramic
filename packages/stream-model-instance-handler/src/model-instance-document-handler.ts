0925627bae2314d11cb4e86d0626a9dffa521c01
Spencer T Brody <spencer+github@3box.io>
2024-05-10 10:41:00 -0400

    fix: Enforce no unique field in header for set account relation MIDs

--- packages/stream-model-instance-handler/src/model-instance-document-handler.ts
+++ packages/stream-model-instance-handler/src/model-instance-document-handler.ts
@@ -351,18 +351,33 @@ export class ModelInstanceDocumentHandler implements StreamHandler<ModelInstance
    * @param header - the header to validate
    */
   async _validateHeader(model: Model, header: ModelInstanceDocumentHeader): Promise<void> {
-    if (model.content.accountRelation.type === 'single') {
-      if (header.unique) {
-        throw new Error(
-          `ModelInstanceDocuments for models with SINGLE accountRelations must be created deterministically`
-        )
-      }
-    } else {
-      if (!header.unique) {
-        throw new Error(
-          `Deterministic ModelInstanceDocuments are only allowed on models that have the SINGLE accountRelation`
-        )
-      }
+    switch (model.content.accountRelation.type) {
+      case 'single':
+        if (header.unique) {
+          throw new Error(
+            `ModelInstanceDocuments for models with SINGLE accountRelations must be created deterministically`
+          )
+        }
+        break
+      case 'set':
+        if (!header.unique) {
+          throw new Error(
+            `ModelInstanceDocuments for models with SET accountRelations must be created with a unique field containing data from the fields providing the set semantics`
+          )
+        }
+        break
+      case 'list':
+        if (!header.unique) {
+          throw new Error(
+            `ModelInstanceDocuments for models with LIST accountRelations must be created with a unique field`
+          )
+        }
+        break
+      // default:
+      //   throw new UnreachableCaseError(
+      //     model.content.accountRelation.type,
+      //     `Unsupported account relation ${model.content.accountRelation.type} found in Model ${model.content.name}`
+      //   )
     }
   }
 
