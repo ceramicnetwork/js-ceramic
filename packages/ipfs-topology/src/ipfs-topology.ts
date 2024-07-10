d28eeed06e21041289376b23bda864eee60fe441
Nathaniel Cook <nvcook42@gmail.com>
2024-07-10 14:11:19 -0600

    feat: add new mainnet and tnet bootstrap peers (#3254)
    * feat: add new mainnet and tnet bootstrap peers
    
    * fix: remove kubo based devunstable bootstrap nodes
    
    * fix: keep old list in kubo mode

--- packages/ipfs-topology/src/ipfs-topology.ts
+++ packages/ipfs-topology/src/ipfs-topology.ts
@@ -1,4 +1,4 @@
-import { Networks } from '@ceramicnetwork/common'
+import { EnvironmentUtils, Networks } from '@ceramicnetwork/common'
 import type { DiagnosticsLogger, IpfsApi } from '@ceramicnetwork/common'
 import { Multiaddr, multiaddr } from '@multiformats/multiaddr'
 
@@ -6,47 +6,81 @@ import { Multiaddr, multiaddr } from '@multiformats/multiaddr'
  * Hardcoded list of bootstrap peers run by 3BoxLabs for each major network.
  */
 const BOOTSTRAP_LIST = (ceramicNetwork: Networks): Array<Multiaddr> | null => {
-  switch (ceramicNetwork) {
-    case Networks.MAINNET:
-      return [
-        multiaddr(
-          '/dns4/go-ipfs-ceramic-private-mainnet-external.3boxlabs.com/tcp/4011/ws/p2p/QmXALVsXZwPWTUbsT8G6VVzzgTJaAWRUD7FWL5f7d5ubAL'
-        ),
-        multiaddr(
-          '/dns4/go-ipfs-ceramic-private-cas-mainnet-external.3boxlabs.com/tcp/4011/ws/p2p/QmUvEKXuorR7YksrVgA7yKGbfjWHuCRisw2cH9iqRVM9P8'
-        ),
-      ]
-    case Networks.TESTNET_CLAY:
-      return [
-        multiaddr(
-          '/dns4/go-ipfs-ceramic-public-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmWiY3CbNawZjWnHXx3p3DXsg21pZYTj4CRY1iwMkhP8r3'
-        ),
-        multiaddr(
-          '/dns4/go-ipfs-ceramic-private-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmQotCKxiMWt935TyCBFTN23jaivxwrZ3uD58wNxeg5npi'
-        ),
-        multiaddr(
-          '/dns4/go-ipfs-ceramic-private-cas-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmbeBTzSccH8xYottaYeyVX8QsKyox1ExfRx7T1iBqRyCd'
-        ),
-      ]
-    case Networks.DEV_UNSTABLE:
-      return [
-        multiaddr(
-          '/dns4/ipfs-ceramic-public-qa-swarm.3boxlabs.com/tcp/4010/p2p/QmPP3RdaSWDkhcxZReGo591FWanLw9ucvgmUZhtSLt9t6D'
-        ),
-        multiaddr(
-          '/dns4/ipfs-ceramic-private-qa-swarm.3boxlabs.com/tcp/4010/p2p/12D3KooWAQvp6Wnqho9ririWvbmtodnNQX9GECQEps2gb6kwBf2h'
-        ),
-        multiaddr(
-          '/dns4/ipfs-ceramic-private-cas-qa-swarm.3boxlabs.com/tcp/4010/p2p/QmRvJ4HX4N6H26NgtqjoJEUyaDyDRUhGESP1aoyCJE1X1b'
-        ),
-      ]
-    case Networks.LOCAL:
-    case Networks.INMEMORY:
-      return null
-    default: {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const preventCompilingUnhandledCase: never = ceramicNetwork
-      return null
+  if (!EnvironmentUtils.useRustCeramic()) {
+    // Go/Kubo based lists
+    switch (ceramicNetwork) {
+      case Networks.MAINNET:
+        return [
+          multiaddr(
+            '/dns4/go-ipfs-ceramic-private-mainnet-external.3boxlabs.com/tcp/4011/ws/p2p/QmXALVsXZwPWTUbsT8G6VVzzgTJaAWRUD7FWL5f7d5ubAL'
+          ),
+          multiaddr(
+            '/dns4/go-ipfs-ceramic-private-cas-mainnet-external.3boxlabs.com/tcp/4011/ws/p2p/QmUvEKXuorR7YksrVgA7yKGbfjWHuCRisw2cH9iqRVM9P8'
+          ),
+        ]
+      case Networks.TESTNET_CLAY:
+        return [
+          multiaddr(
+            '/dns4/go-ipfs-ceramic-public-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmWiY3CbNawZjWnHXx3p3DXsg21pZYTj4CRY1iwMkhP8r3'
+          ),
+          multiaddr(
+            '/dns4/go-ipfs-ceramic-private-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmQotCKxiMWt935TyCBFTN23jaivxwrZ3uD58wNxeg5npi'
+          ),
+          multiaddr(
+            '/dns4/go-ipfs-ceramic-private-cas-clay-external.3boxlabs.com/tcp/4011/ws/p2p/QmbeBTzSccH8xYottaYeyVX8QsKyox1ExfRx7T1iBqRyCd'
+          ),
+        ]
+      case Networks.DEV_UNSTABLE:
+        return [
+          multiaddr(
+            '/dns4/ipfs-ceramic-public-qa-swarm.3boxlabs.com/tcp/4010/p2p/QmPP3RdaSWDkhcxZReGo591FWanLw9ucvgmUZhtSLt9t6D'
+          ),
+          multiaddr(
+            '/dns4/ipfs-ceramic-private-qa-swarm.3boxlabs.com/tcp/4010/p2p/12D3KooWAQvp6Wnqho9ririWvbmtodnNQX9GECQEps2gb6kwBf2h'
+          ),
+          multiaddr(
+            '/dns4/ipfs-ceramic-private-cas-qa-swarm.3boxlabs.com/tcp/4010/p2p/QmRvJ4HX4N6H26NgtqjoJEUyaDyDRUhGESP1aoyCJE1X1b'
+          ),
+        ]
+      case Networks.LOCAL:
+      case Networks.INMEMORY:
+        return null
+      default: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const preventCompilingUnhandledCase: never = ceramicNetwork
+        return null
+      }
+    }
+  } else {
+    // Rust/Ceramic-one based lists
+    switch (ceramicNetwork) {
+      case Networks.MAINNET:
+        return [
+          multiaddr(
+            '/dns4/bootstrap-mainnet-rust-ceramic-1.3box.io/tcp/4101/p2p/12D3KooWJC1yR4KiCnocV9kuAEwtsMNh7Xmu2vzqpBvk2o3MrYd6',
+          ),
+          multiaddr(
+            '/dns4/bootstrap-mainnet-rust-ceramic-2.3box.io/tcp/4101/p2p/12D3KooWCuS388c1im7KkmdrpsLMziihF8mbcv2w6HPCp4Qmww6m',
+          ),
+        ]
+      case Networks.TESTNET_CLAY:
+        return [
+          multiaddr(
+            '/dns4/bootstrap-tnet-rust-ceramic-1.3box.io/tcp/4101/p2p/12D3KooWMqCFj5bnwuNi6D6KLhYiK4C8Eh9xSUKv2E6Jozs4nWEE',
+          ),
+          multiaddr(
+            '/dns4/bootstrap-tnet-rust-ceramic-2.3box.io/tcp/4101/p2p/12D3KooWPFGbRHWfDaWt5MFFeqAHBBq3v5BqeJ4X7pmn2V1t6uNs',
+          ),
+        ]
+      case Networks.DEV_UNSTABLE:
+      case Networks.LOCAL:
+      case Networks.INMEMORY:
+        return null
+      default: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const preventCompilingUnhandledCase: never = ceramicNetwork
+        return null
+      }
     }
   }
 }
@@ -64,7 +98,7 @@ export class IpfsTopology {
     readonly ceramicNetwork: string,
     readonly logger: DiagnosticsLogger,
     readonly period: number = DEFAULT_BOOTSTRAP_CONNECTION_PERIOD
-  ) {}
+  ) { }
 
   async forceConnection(): Promise<void> {
     const bootstrapList: Multiaddr[] = BOOTSTRAP_LIST(this.ceramicNetwork as Networks) || []
