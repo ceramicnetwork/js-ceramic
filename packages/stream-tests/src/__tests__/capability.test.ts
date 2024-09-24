517d212e6a73784eed985a9a0b1cbcd5c42407d8
Spencer T Brody <spencer+github@3box.io>
2024-09-24 17:19:52 -0400

    fix test

--- packages/stream-tests/src/__tests__/capability.test.ts
+++ packages/stream-tests/src/__tests__/capability.test.ts
@@ -967,7 +967,7 @@ describe('CACAO Integration test', () => {
         // Cannot repair stream even with SyncOptions.SYNC_ALWAYS if it is the genesis commit that
         // messed up.
         await expect(doc.sync()).rejects.toThrow(/CACAO expired/)
-        await expect(doc.sync({ sync: SyncOptions.SYNC_ALWAYS })).rejects.toThrow(/CACAO expired/)
+        await expect(doc.sync({ sync: SyncOptions.SYNC_ALWAYS })).rejects.toThrow(/CACAO.*expired/)
         expect(doc.content).toEqual(CONTENT0)
       },
       1000 * 60
